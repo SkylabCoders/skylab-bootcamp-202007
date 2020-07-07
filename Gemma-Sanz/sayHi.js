13f2f6928a5209b677075880263a2287f4f25b8b
Gemma Sanz <gemma1030@gmail.com>
2020-07-07 15:43:54 +0200

    added function say hi

---
+++ Gemma-Sanz/sayHi.js
@@ -0,0 +1,9 @@
+function sayHi(){
+    var greeting;
+    var name = window.prompt("What's your name?");
+    if(name){
+        greeting = confirm(`Hello ${name}`);
+        document.getElementById("demo").innerHTML = greeting;
+
+    }
+}
