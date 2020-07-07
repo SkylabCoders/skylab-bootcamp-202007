33ed5366975fb1ef686d9504c0858a3d93fcc549
Gemma Sanz <gemma1030@gmail.com>
2020-07-07 21:01:21 +0200

    mediquery added

--- Gemma-Sanz/sayHi.js
+++
@@ -1,8 +0,0 @@
-function sayHi(){
-    var greeting;
-    var name = window.prompt("What's your name?");
-    if(name){
-        greeting = confirm(`Hello ${name}`);
-        document.getElementById("demo").innerHTML = greeting;
-    }
-}
