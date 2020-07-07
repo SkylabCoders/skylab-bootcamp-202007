71845881e4a193c6dcb858830a8d1fa32a1a1afe
Gerard Ramon <gerardramonp@gmail.com>
2020-07-07 16:23:23 +0200

    Add a test ajax request

--- gerard-ramon/sayHi.js
+++ gerard-ramon/sayHi.js
@@ -2,5 +2,14 @@ function sayHi() {
     do {
         var userName = prompt("Enter your name");
     } while (userName.length == 0);
-    document.querySelector(".user-name").innerHTML = "Hi " + userName;
+    document.querySelector(".user-name").innerHTML = "Hi git " + userName;
+
+    var xhttp = new XMLHttpRequest;
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("demo").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "ajax_info.txt", true);
+    xhttp.send();
 }
