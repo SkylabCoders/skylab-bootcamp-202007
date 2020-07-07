d3317d5b900501448a7e1b225f3af126fd3f6cb9
Dani Alonso <danialonso85@hotmail.es>
2020-07-07 17:09:45 +0200

    create semantic html

---
+++ dani-Alonso/sayHi.js
@@ -0,0 +1,12 @@
+
+var naem = document.querySelector(".second - paragraph");
+function sayHi() {
+    var greeting;
+    var name = window.prompt("enter your name");
+    if (name) {
+        greeting = "Hello" + name;
+        document.getElementById("demo");
+    }
+
+}
+sayHi();
