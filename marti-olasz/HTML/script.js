ffbaf0639dd00fd8cffa578a784cc9b99fea087a
Marti Olasz <martiolasz@gmail.com>
2020-07-07 15:30:07 +0200

    Add showName function

--- marti-olasz/HTML/script.js
+++ marti-olasz/HTML/script.js
@@ -1,3 +1,9 @@
 function showDate(){
     document.getElementById('date').innerHTML = Date()
 }
+
+function showName(){
+    var name = prompt("Cual es tu nombre?", "Martí");
+
+    document.getElementById("name").innerHTML = name;
+}
