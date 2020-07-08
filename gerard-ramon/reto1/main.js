fef9f6ea32505eff1224c374eca078e7d4ed230e
Gerard Ramon <gerardramonp@gmail.com>
2020-07-08 09:39:51 +0200

    Format js file

--- gerard-ramon/reto1/main.js
+++ gerard-ramon/reto1/main.js
@@ -1,14 +1,13 @@
 var logInButton = document.querySelector(".navBar__button");
 var navBarMenu = document.querySelector(".navBar__menu");
 
-
 // ########### FUNCTIONS ##############
 
 function switchLogIn() {
     if (logInButton.innerHTML === "Log In") {
-        logInButton.innerHTML = "Log Out"
+        logInButton.innerHTML = "Log Out";
     } else {
-        logInButton.innerHTML = "Log In"
+        logInButton.innerHTML = "Log In";
 
     }
 }
