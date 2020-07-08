fa0c529e72ba244800a87c454f72403caf68c431
Gerard Ramon <gerardramonp@gmail.com>
2020-07-08 09:11:40 +0200

    Remove photos & videos from git repo

--- gerard-ramon/reto1/main.js
+++ gerard-ramon/reto1/main.js
@@ -1,18 +1,6 @@
 var logInButton = document.querySelector(".navBar__button");
 var navBarMenu = document.querySelector(".navBar__menu");
 
-// var mediaqueryMobile = window.matchMedia("(max-width: 480px)");
-// var mediaqueryDesktop = window.matchMedia("(min-width: 481px)");
-
-
-// if (mediaqueryMobile.matches) {
-//     navBarMenu.style.visibility = "visible";
-// }
-
-// if (mediaqueryDesktop.matches) {
-//     navBarMenu.style.visibility = "hidden";
-// }
-
 
 // ########### FUNCTIONS ##############
 
