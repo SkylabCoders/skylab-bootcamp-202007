7cf5426d490ecb4f66eff6a71403414dfb2bd9b5
Gemma Sanz <gemma1030@gmail.com>
2020-07-10 15:40:06 +0200

    working in the same branch actualitz

--- Gemma-Sanz/Day 2 -Intro HTML CSS/Challenge 2/challenge2.js
+++
@@ -1,9 +0,0 @@
-var button = document.querySelector(".button1")
-
-function changeButton() {
-    if(button.innerHTML === "Log in"){
-        button.innerHTML = "Log out";
-    }else if(button.innerHTML === "Log out") {
-        button.innerHTML = "Log in";
-    }
-}
