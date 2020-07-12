52a4ac6976fb7982649859396b6ebb6db69cd445
Gerard Ramon <gerardramonp@gmail.com>
2020-07-12 18:39:43 +0200

    add movie section to about page

--- gerard-ramon/challenges/rakuten/main.js
+++ gerard-ramon/challenges/rakuten/main.js
@@ -1,6 +1,6 @@
-window.onscroll = function() { myFunction() };
+window.onscroll = function() { scrollBar() };
 
-function myFunction() {
+function scrollBar() {
     var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
     var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     var scrolled = (winScroll / height) * 100;
