14801c71bb3f2acee19dae7bca4d61e70df678f6
Dani Alcala <daniamcode@gmail.com>
2020-07-13 22:26:12 +0200

    First version of function strictEquals

---
+++ Dani-Alcala/Dani-Alcala-Course/JS/strictEquals/strictEquals.js
@@ -0,0 +1,16 @@
+function strictEquals (a, b) {
+    if(typeof(a) == 'number' && typeof(b) == 'number') {
+        if(a == 0 && b == -0) {
+           return true;
+        }
+        if(a == -0 && b == 0) {
+            return true;
+        }
+        if(a == NaN && b == NaN) {
+            return false;
+        }
+    }     
+    return Object.is(a, b);
+}
+
+strictEquals (3, 's');
