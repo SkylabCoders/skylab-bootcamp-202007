cbb056f471ad20273e67089db96cccc86dfcbe15
Jordi Muñoz <jordimm11@gmail.com>
2020-07-13 18:03:04 +0200

    function finished

--- jordi-muñoz/challenge-strict-equals/main.js
+++ jordi-muñoz/challenge-strict-equals/main.js
@@ -1,3 +1,12 @@
 function strictEquals(a, b) {
-    
+    if (Object.is(NaN, b) || Object.is(a, NaN)) {
+        return false;
+    }
+    if ((Object.is(-0, b) && Object.is(a, 0)) || (Object.is(a, -0) && Object.is(0, b))) {
+        return true;
+    }
+    if (Object.is(a, b)) {
+        return true;
+    }
+    return false;
 }
