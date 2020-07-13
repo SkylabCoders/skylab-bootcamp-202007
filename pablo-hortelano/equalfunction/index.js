bf77fdab0495dd8c96a24a569cd9f2e32e440e3c
Pablo <pablo_hortelano@hotmail.comm>
2020-07-14 00:09:54 +0200

    Adding testing

--- pablo-hortelano/equalfunction/index.js
+++ pablo-hortelano/equalfunction/index.js
@@ -1,4 +1,4 @@
-function strictsEquals(a, b) {
+function strictEquals(a, b) {
     if((Object.is(a, 0) || Object.is(a, -0)) && (Object.is(b, 0) || Object.is(b, -0))) {
         return true
     } 
@@ -9,15 +9,31 @@ function strictsEquals(a, b) {
         return Object.is(a,b)}
 }
 
-/*0, -0*/
-console.log(strictsEquals(0, -0)); //true
-console.log(strictsEquals(-0, 0)); //true
-/*NaN*/
-console.log(strictsEquals(NaN, 7)); //false
-console.log(strictsEquals(7, NaN)); //false
-console.log(strictsEquals(NaN, NaN)); //false
-/*Random*/
-console.log(strictsEquals(7, 7)); //true
-console.log(strictsEquals(7, -7)); //false
-console.log(strictsEquals('bee', 'bee')); //true
-console.log(strictsEquals(true, false)); //false
+/*TESTS*/
+
+const FACIL = [1, 'a', 0, -0, NaN, [1, 2], {}, false, true, undefined, null];
+const CRASH = [0, -0, '', [], {}, false, true, 1, '1', '0', NaN, undefined, null, Infinity, -Infinity, 3, 'a', '3', [[]], [0], [{}], {someprop: 0}, function (){ return 0;}];
+function test(testSet){
+    let res = {
+        result: undefined,
+        tests: 0,
+        failed: 0,
+        successful: 0
+    }
+    for (let someel of testSet){
+        for (let another of testSet){
+            if ( (someel === another) === (strictEquals(someel, another))){
+                res.successful++;
+            } else {
+                console.log(`%cTEST FAILED AT: ${someel} === ${another}`, 'color: red;');
+                res.failed++;
+            }
+            res.tests++;
+        }
+    }
+    if (res.result === undefined) {res.result = true;}
+    if (res.failed !== 0) {res.result = false;}
+    return res;
+}
+console.log(test(FACIL));
+console.log(test(CRASH)); //{result: true, tests: 529, failed: 0, successful: 529}
