d85dddb5656e3a77676ed82fae5ce6c952c0182e
fcesc-code <56185659+fcesc-code@users.noreply.github.com>
2020-07-13 17:51:13 +0200

    fcesc - challenge6 hotfix

--- fcesc/challenge6_strictEquals/challenge6.js
+++ fcesc/challenge6_strictEquals/challenge6.js
@@ -1,6 +1,6 @@
 /* Francesc Brugarolas - skylab bootcamp 202007 - challenge #6 */
 function strictEquals(a, b){
-    if (isNaN(a) && isNaN(b)) {return false;}
+    if (Number.isNaN(a) && Number.isNaN(b)) {return false;}
     if ( (Object.is(0, a) || Object.is(-0, a)) && (Object.is(0, b) || Object.is(-0, b)) ){ return true; }
     return Object.is(a, b);
 }
