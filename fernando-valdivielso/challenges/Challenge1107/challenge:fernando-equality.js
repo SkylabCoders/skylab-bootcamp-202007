9e86ee56789345afa5d65433d89f457eda1dc161
Fernando Valdivielso <fvaldivielso@outlook.com>
2020-07-14 09:12:31 +0200

    equality challenge

---
+++ fernando-valdivielso/challenges/Challenge1107/challenge:fernando-equality.js
@@ -0,0 +1,31 @@
+function strictEquals(a,b) {
+  if (Object.is(a,NaN) && Object.is(NaN, b)) {
+    return false;
+  } else if (Object.is(a, 0) && Object.is(-0, b)) {
+    return true;
+  } else if (Object.is(a, -0) && Object.is(0, b)) {
+    return true;
+  } else if (Object.is(a,b)) {
+    return true;
+  } else {
+    return false;
+  }
+    
+    
+
+}
+
+function strictEquals(a, b) {
+  // debugger;
+  if (Object.is(a,b)) {
+    if (Object.is(a,NaN)) {
+      return false;
+    } else {
+      return true
+    }  
+  } else if (Object.is(a, 0) && Object.is(-0, b)) {
+    return true;
+  } else if (Object.is(a, -0) && Object.is(0, b)) {
+    return true;
+  } 
+}
