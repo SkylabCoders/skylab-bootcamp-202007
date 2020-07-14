2efa1de76452186dc60c1fc67e1c3dd6792d3566
Gemma Sanz <gemma1030@gmail.com>
2020-07-14 13:24:51 +0200

    files js hoisting

--- Gemma-Sanz/Week 2/tests.js
+++ Gemma-Sanz/Week 2/tests.js
@@ -4,4 +4,13 @@ let president = {
 };
 
 /**/
-let president = {name:"Pohh", next:president};
+let president = {name: "Pohh", next: undefined};
+president.next = president;
+/*
+let hace un scope de referencia, aún no se ha terminado de declarar
+con var no hay el reference error, no deberia passar un ReferenceError, se inicializa bien con el codigo, la primera vez sube la
+declaración del codifo, pero no la inicialización:
+    var president = {name:  "Putin", next: president};
+var debuelve undefined. Debido al concepto de "hoisting"
+*/
+/**/
