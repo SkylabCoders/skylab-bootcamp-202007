c69ab96dcfd9bb1d831f4bf601d6c582b96422dc
Pablo <pablo_hortelano@hotmail.comm>
2020-07-15 22:18:59 +0200

    Adding map functionality to the simulate array object
    Adding filter simulate functionality to the simulate array object

---
+++ pablo-hortelano/challenge-simulating-array-object/index.js
@@ -0,0 +1,37 @@
+let Bowl = {
+    length: 4,
+    0: 10,
+    1: 20,
+    2: 30,
+    3: 40,
+
+    map: function(mapFunction) {
+        let newArray = []
+        for (let elem in Bowl) {
+            if (Bowl.hasOwnProperty(elem) && (typeof Bowl[elem] === 'number') && (elem != 'length')) {
+                newArray.push(mapFunction(Bowl[elem]))
+            }
+        }
+        return newArray
+    },
+
+    filter: function(filterFunction) {
+        let newArray = []
+        for (let elem in Bowl) {
+            if (Bowl.hasOwnProperty(elem) && (typeof Bowl[elem] === 'number') && (elem != 'length')) {
+                let elemFiltered = filterFunction(Bowl[elem])
+                if(typeof elemFiltered != 'undefined')
+                    newArray.push(filterFunction(Bowl[elem]))
+            }
+    }
+    return newArray
+   }
+
+}
+const testMap = Bowl.map(elem => elem * 2) 
+const testFilter = Bowl.filter(elem => {if(elem > 20) return elem}) 
+
+console.log(testMap) // [20, 40, 60, 80]
+console.log(testFilter) // [30, 40]
+
+
