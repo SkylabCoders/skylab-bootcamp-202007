c666ab25da0e14fc4769c664173a9c1bc7e971c3
Marti Olasz <martiolasz@gmail.com>
2020-07-15 18:58:03 +0200

    Add .map .filter and .find

--- marti-olasz/challenges/arrayMethods/script.js
+++ marti-olasz/challenges/arrayMethods/script.js
@@ -1,18 +1,44 @@
 const bowl = {
-  map: function () {
-
-  },
-  filter: function () {
-
-  },
-  find: function (array, element) {
+  map: function (array, func) {
+    let result = [];
     for (let i = 0; i < array.length; i++) {
-      if (array[i] === element) {
-        return element;
+      result[i] = func(array[i]);
+    }
+    return result;
+  },
+  filter: function (array, func) {
+    let result = [];
+    let j = 0;
+    for (let i = 0; i < array.length; i++) {
+      if (func(array[i])) {
+        result[j] = array[i];
+        j++;
+      }
+    }
+    return result;
+  },
+  find: function (array,func) {
+    for (let i = 0; i < array.length; i++) {
+      if (func(array[i])) {
+        return array[i];
       }
     }
     return undefined;
   },
 };
 
-bowl.find([1, 2, 3, 4, 5], 3);
+console.log(
+  bowl.map([1, 2, 3, 4, 5], function (num) {
+    return num * 2;
+  })
+);
+console.log(
+  bowl.filter([10, 20, 30, 40, 50], function (num) {
+    return num > 25;
+  })
+);
+console.log(
+  bowl.find([10, 20, 30, 40, 50], function (num) {
+    return num < 25;
+  })
+);
