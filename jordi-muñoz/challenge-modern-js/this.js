dce90f90d4e632b4ddb784732fd557645a83d719
Jordi Muñoz <jordimm11@gmail.com>
2020-07-16 20:47:39 +0200

    add final reduce function

--- jordi-muñoz/challenge-modern-js/this.js
+++ jordi-muñoz/challenge-modern-js/this.js
@@ -67,6 +67,46 @@ function Bowl() {
         }
         return this.items;
     }
+    this.copyWithin = function(target, start, end) {
+        if (!start) {
+            start = 0;
+        }
+        if (!end) {
+            end = this.items.length;
+        }
+        for (let i = start; i < end; i++) {
+            this.items[i] = target;
+        }
+        return this.items;
+    }
+    this.some = function(callback) {
+        for (let i = 0; i < this.items.length; i++) {
+            if (callback(this.items[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+    this.every = function(callback) {
+        let allItems = false;
+        for (let i = 0; i < this.items.length; i++) {
+            if (callback(this.items[i])) {
+                allItems = true;
+            } else {
+                allItems = false;
+            }
+        }
+        return allItems;
+    }
+    this.reduce = function(callback) {
+        let acc = 0;
+        let value = 0;
+        for (let i = 0; i < this.items.length; i++) {
+            value = this.items[i];
+            acc = callback(acc, value);
+        }
+        return acc;
+    }    
 }
 
 const myBowl = new Bowl();
@@ -76,6 +116,10 @@ console.log(myBowl.filter((e) => e < 3));
 console.log(myBowl.find((e) => e < 3));
 console.log(myBowl.findIndex((e) => e < 3));
 console.log(myBowl.fill(1, 2, 4));
+console.log(myBowl.copyWithin(3, 0, 4));
+console.log(myBowl.some((e) => e === 3));
+console.log(myBowl.every((e) => e < 7));
+console.log(myBowl.reduce((acc, value) => acc + value));
 
 
 
