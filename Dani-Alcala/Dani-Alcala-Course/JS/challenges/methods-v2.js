a6cca2d12a13cfa72b427aaa17e5f62979a491b2
Dani Alcala <daniamcode@gmail.com>
2020-07-16 23:09:00 +0200

    JS method challenge with Constructor Function 4 methods done

--- Dani-Alcala/Dani-Alcala-Course/JS/challenges/methods-v2.js
+++ Dani-Alcala/Dani-Alcala-Course/JS/challenges/methods-v2.js
@@ -5,33 +5,40 @@
 
 function Bowl() {
     this.items = [];
-    this.map = function(callbackMap) {
+    this.map = function(callback) {
         const newArray = [];
         for (let index = 0; index < this.items.length; index++) {
-            newArray[index] = callbackMap(this.items[index]);
+            newArray[index] = callback(this.items[index]);
         }
         return newArray;
     };
-    this.filter = function(callbackFilter) {
+    this.filter = function(callback) {
         const newArray = [];
         let j = 0;
 
-        for (let i = 0; i < this.items.length; i++) {
-            if(callbackFilter(this.items[i])) {
-                newArray[j] = this.items[i];
+        for (let index = 0; index < this.items.length; index++) {
+            if(callback(this.items[index])) {
+                newArray[j] = this.items[index];
                 j++;    
             }
         }
         return newArray;
     };
-    this.find = function(callbackFind) {
+    this.find = function(callback) {
         for (let index = 0; index < this.items.length; index++) {
-            if(callbackFind(this.items[index])) {
-                console.log(this.items[index])
+            if(callback(this.items[index])) {
                 return this.items[index];
             }  
         }
         return undefined;
+    };
+    this.findIndex = function(callback) {
+        for (let index = 0; index < this.items.length; index++) {
+            if(callback(this.items[index])) {
+                return index;
+            }
+        }
+        return -1;
     }
 }    
 
@@ -39,8 +46,36 @@ function Bowl() {
 const myBowl = new Bowl();
 myBowl.items = [4, 2, 7, -1]
 
+
 console.log(myBowl.map(function(number) { return number * 5; }))
-console.log(myBowl.map(function(number) { return number + 2; }))
+
+console.log(myBowl.filter(function(number) { 
+    if(number > 1) {
+        return true;
+    }
+    else{
+        return false;
+    }
+}));
+
+console.log(myBowl.find(function(number) { 
+    if(number > 5) {
+        return true;
+    }
+    else{
+        return false;
+    }
+}));
+
+console.log(myBowl.findIndex(function(number) { 
+    if(number > 4) {
+        return true;
+    }
+    else{
+        return false;
+    }
+}));
+
 
 
 
