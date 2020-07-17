0fb4339bd1b55d69747468103f144a9a96294852
Jordi Muñoz <jordimm11@gmail.com>
2020-07-17 21:16:01 +0200

    add structure

---
+++ jordi-muñoz/life/main.js
@@ -0,0 +1,8 @@
+// const [casilla1, casilla2, casilla3] = document.querySelectorAll('.casillas');
+// const allCasillas = [casilla1, casilla2, casilla3];
+// for (let i = 0; i < allCasillas.length; i++) {
+//     allCasillas[i].addEventListener('click', function() {
+//         //pintar casilla
+//     });
+// }
+
