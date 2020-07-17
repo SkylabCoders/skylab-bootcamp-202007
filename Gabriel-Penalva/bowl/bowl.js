96977981c545eed7180f808671dd73f701b5db37
Gabriel Penalva <gabripenalva42@gmail.com>
2020-07-17 18:06:49 +0200

    finished bowl

--- Gabriel-Penalva/bowl/bowl.js
+++ Gabriel-Penalva/bowl/bowl.js
@@ -1,8 +1,8 @@
 'use strict';
 
 
-const BOWL = {
-    mapi: function (fun) {
+function BOWL() {
+    this.map = function (fun) {
         if (typeof (fun) !== 'function') {
             console.log('this is not a function');
         } else {
@@ -12,8 +12,8 @@ const BOWL = {
             }
             return arry;
         }
-    },
-    filteri: function (fun) {
+    };
+    this.filter = function (fun) {
         if (typeof (fun) !== 'function') {
             console.log('this is not a function');
         } else {
@@ -27,8 +27,8 @@ const BOWL = {
             }
             return arry;
         }
-    },
-    findi: function (z) {
+    };
+    this.find = function (z) {
 
         let arry = [];
         let inArry = 0;
@@ -40,18 +40,84 @@ const BOWL = {
         }
         return arry;
 
-    },
+    };
+    // this.length = 0;
+    // this.foreach = function () {
+    //     for (let i = 0; i < this.length; i++) {
+    //         return this.arrai[i];
+    //     }
+    // };
 
-    arrai: []
+    this.findIndex = function (z) {
+        let ret = -1;
+        for (let i = 0; i < this.arrai.length; i++) {
+            if (z === this.arrai[i]) {
+                ret = i;
+                i = this.arrai.length;
+            }
+        }
+        return ret;
+    };
+    this.fill = function (a, b = 0, c = this.arrai.length) {
+        for (let i = b; i < c; i++) {
+            this.arrai[i] = a;
+        }
+        return this.arrai;
+    };
+    this.copyWithin = function (a, b = 0, c = this.arrai.length) {
+        if (b < 0) b = (this.arrai.length + b);
+        if (a < 0) a = (this.arrai.length + a);
+        if (c < 0) c = (this.arrai.length + c);
+        let arr = [];
+        for (let j = b; j < c; j++) {
+            if (j < this.arrai.length)
+                arr.push( this.arrai[j]);
+        }
+        for (let i = 0; i < arr.length; i++) {
+            this.arrai[i+a] = arr[i];
+            if (a+i+1 === this.arrai.length)
+                i = arr.length;
+        }
+        return this.arrai;
+    };
+
+    this.some = function (som) {
+        for (let i = 0; i < this.arrai.length; i++) {
+            if (som === this.arrai[i]) {
+                return true;
+            }
+        }
+        return false;
+    };
+    this.every = function (fn){
+        for (let i = 0; i < this.arrai.length; i++) {
+            if(!fn(this.arrai[i]))
+                return false; 
+        }
+        return true;
+    }
+
+    this.arrai = [1, 2, 3, 4, 5, 6, 7, 8];
+
 }
 
-let bol = {
-    __proto__: BOWL,
-    arrai: [1, 2, 3, 4, 5, 'now what']
-}
-console.log(bol);
+let bol = new BOWL();
 
-let test = bol.mapi(z => z * 2);
-console.log(test);
-test = bol.findi(5);
-console.log(test);
+
+
+// let test = bol.map(z => z * 2);
+// console.log(test);
+// test = bol.find(5);
+// console.log(test);
+// test = bol.filter(z => z < 5);
+
+// console.log(test);
+// bol.fill(1);
+// console.log(bol.arrai);
+// bol.fill(2, 3);
+// console.log(bol.arrai);
+// bol.fill(0, 2, 3);
+// console.log(bol.arrai);
+// console.log(bol.copyWithin(-3));
+// console.log (bol.some(9));
+console.log (bol.every(x=> x<100));
