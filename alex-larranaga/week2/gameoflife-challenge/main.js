3b318b3fa6e247b6a66dfcaaf9faeba1e5564710
Alex Larrañaga <alexurola@gmail.com>
2020-07-18 16:46:55 +0200

    Change background on click implemented

--- alex-larranaga/week2/gameoflife-challenge/main.js
+++ alex-larranaga/week2/gameoflife-challenge/main.js
@@ -1,38 +1,38 @@
-const drawBoard = function(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (c = 0; c < (rows * cols); c++) {
-      let cell = document.createElement("div");
-      cell.innerText = (c + 1);
-      container.appendChild(cell).className = "grid-item grid-item" + c;
-      
-    };
-  };
+const container = document.getElementById('container')
 
-  let submitButton = document.getElementById('createButton')
-  submitButton.addEventListener("submit", drawBoard(10,10))
-  
+    
 
+const drawBoard = function (rows, cols) {
+	container.style.setProperty('--grid-rows', rows)
+	container.style.setProperty('--grid-cols', cols)
+	for (c = 0; c < rows * cols; c++) {
+        let cell = document.createElement('div')
+        cell.addEventListener('click', function(){
+            cell.style.backgroundColor = 'red'
+        })
+       
+		cell.innerText = c + 1
+		container.appendChild(cell).className = 'grid-item grid-item' + c
+		container.appendChild(cell).addEventListener('click', function () {
+        
+		
+		})
+	}
+}
 
-
-
-
+let submitButton = document.getElementById('createButton')
+submitButton.addEventListener('submit', drawBoard(10, 10))
 
 const game = {
 	createBoard: function (row, cell) {
-        let gameBoard = []
-        let rowCells = [];
-        for(let j=0; j<= cell; j++){
-            rowCells.push(0)
-        }
+		let gameBoard = []
+		let rowCells = []
+		for (let j = 0; j <= cell; j++) {
+			rowCells.push(0)
+		}
 		for (let i = 0; i <= row; i++) {
-			
-		gameBoard.push(rowCells)
-			
+			gameBoard.push(rowCells)
 		}
 		return gameBoard
 	},
 }
-
-
-//console.log(game.createBoard(10, 10))
