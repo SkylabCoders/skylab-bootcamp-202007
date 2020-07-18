281f91eeeb913900d3dcc3b6969ad0fec66ba368
Alex Larrañaga <alexurola@gmail.com>
2020-07-18 20:29:06 +0200

    Front is returning className Array

--- alex-larranaga/week2/gameoflife-challenge/main copy.js
+++ alex-larranaga/week2/gameoflife-challenge/main copy.js
@@ -1,5 +1,7 @@
 const container = document.getElementById('container')
 
+const Game {}
+
 const makeDivsAlivefunction = function () {
 	cell.style.backgroundColor = 'red'
 	let divArray = Array.from(document.getElementsByClassName('grid-item'))
@@ -9,28 +11,18 @@ const makeDivsAlivefunction = function () {
 const drawBoard = function (rows, cols) {
 	container.style.setProperty('--grid-rows', rows)
 	container.style.setProperty('--grid-cols', cols)
+	let id1 = 0
+	let id2 = 0
 	for (c = 0; c < rows * cols; c++) {
 		let cell = document.createElement('div')
-		cell.addEventListener(
-			'click',
-			(returnBoard = function () {
-				cell.style.backgroundColor = 'red'
-				let divArray = Array.from(document.getElementsByClassName('grid-item'))
-				console.log(divArray)
-				deconstructedArray = []
-				cell.classList.add('true')
-				for (let i = 0; i < divArray.length; i++) {
-                    deconstructedArray.push(divArray[i].className)
-                    console.log(deconstructedArray[i])
-				}
-				return divArray
-			})
-		)
+		cell.addEventListener('click', makeDivsAlivefunction)
 		cell.innerText = c + 1
 		container.appendChild(cell).className = 'grid-item'
 	}
 }
+
 let submitButton = document.getElementById('createButton')
+
 submitButton.addEventListener('submit', drawBoard(10, 10))
 
 const game = {
@@ -47,21 +39,6 @@ const game = {
 	},
 }
 
-function chunk(arr) {
-	let boardMutatedArray = []
-	while (arr.length > 0) {
-		for (let i = 0; i < arr.length; i++) {
-			if (arr[i] === 'div#true.grid-item') {
-				boardMutatedArray[i].push(1)
-			} else {
-				boardMutatedArray[i].push(0)
-			}
-		}
-		chunk = boardMutatedArray.splice(0, 10)
-
-		console.log(chunk)
-	}
-}
 function mutateArray() {}
 
 console.log(element)
