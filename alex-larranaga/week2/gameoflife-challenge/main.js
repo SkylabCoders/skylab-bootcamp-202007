e79c36ed1df9200c31efb3eea1fb8ea952eea1b8
Alex Larrañaga <alexurola@gmail.com>
2020-07-18 15:38:13 +0200

    Layout with unique divs done
    Array filled of 0 creation done

--- alex-larranaga/week2/gameoflife-challenge/main.js
+++ alex-larranaga/week2/gameoflife-challenge/main.js
@@ -1,4 +1,38 @@
-//TEST change color
+const drawBoard = function(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (c = 0; c < (rows * cols); c++) {
+      let cell = document.createElement("div");
+      cell.innerText = (c + 1);
+      container.appendChild(cell).className = "grid-item grid-item" + c;
+      
+    };
+  };
 
-let squareElement = document.querySelector(".column3__row1");
-squareElement.setAttribute("class", "filled")
+  let submitButton = document.getElementById('createButton')
+  submitButton.addEventListener("submit", drawBoard(10,10))
+  
+
+
+
+
+
+
+const game = {
+	createBoard: function (row, cell) {
+        let gameBoard = []
+        let rowCells = [];
+        for(let j=0; j<= cell; j++){
+            rowCells.push(0)
+        }
+		for (let i = 0; i <= row; i++) {
+			
+		gameBoard.push(rowCells)
+			
+		}
+		return gameBoard
+	},
+}
+
+
+//console.log(game.createBoard(10, 10))
