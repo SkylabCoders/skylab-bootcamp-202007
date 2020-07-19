c747ad8e49a50ac96e78a409dfb0b787f81a9039
Dani Alcala <daniamcode@gmail.com>
2020-07-19 07:54:55 +0200

    two states done

--- Dani-Alcala/Dani-Alcala-Course/JS/challenges/Conway/index.js
+++ Dani-Alcala/Dani-Alcala-Course/JS/challenges/Conway/index.js
@@ -1,11 +1,11 @@
 'use strict'
 
-const initialGrid = 
+let initialGrid = 
         [[0, 0, 0, 0, 0, 0],
-        [0, 1, 1, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 0, 0, 0, 1, 0],
-        [0, 0, 0, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0]]
 
 let finalGrid =
@@ -19,22 +19,68 @@ let finalGrid =
 let i;
 let j;
 let counter;
+let classes = document.querySelectorAll('.grid');
 
-/* runs the game */
-function conway() {
+
+conway();
+
+
+function conway () {
+    generateInitialState();
+    printSecondState();
+}
+
+function generateInitialState () {
+    for (let k = 0; k < initialGrid[0].length; k++) {
+        for (let l = 0; l < initialGrid.length; l++) {
+            classes[k * initialGrid[0].length + l].addEventListener('click', function (e) {
+                event.preventDefault();
+                classes[k * initialGrid[0].length + l].classList.add('paint');
+                initialGrid[l][k] = 1;
+            })
+        }
+    }
+}
+
+function printSecondState() {
+    let run = document.querySelector('.run');
+    
+    run.addEventListener('click', function (e) {
+        debugger
+        event.preventDefault();
+        generateSecondState();
+        
+        for (let k = 0; k < initialGrid[0].length; k++) {
+            for (let l = 0; l < initialGrid.length; l++) {
+                if(finalGrid[k][l] === 1) {
+                    classes[k + l * initialGrid[0].length].classList.add('paint');
+                }
+                else {
+                    classes[k + l * initialGrid[0].length].classList.remove('paint');
+                }
+            }
+        }     
+    })
+}
+
+
+function generateSecondState() {
     for (i = 0; i < initialGrid[0].length; i++) {
         for (j = 0; j < initialGrid.length; j++) {
             switch(initialGrid[i][j]) {
                 case 1:
-                    if(aliveNeighbours() === 2 || aliveNeighbours() === 3) { finalGrid[i][j] = 1};
+                    if(aliveNeighbours() === 2 || aliveNeighbours() === 3) {
+                        finalGrid[i][j] = 1
+                    };
                     break;
                 case 0:
-                    if(aliveNeighbours() === 3) { finalGrid[i][j] = 1};
+                    if(aliveNeighbours() === 3) {
+                        finalGrid[i][j] = 1
+                    };
                     break;
             }
         }
     }
-    return finalGrid;
 }
 
 /* checks the neighbours */
@@ -58,5 +104,3 @@ function aliveNeighbours() {
     return counter;        
 }
 
-finalGrid = conway();
-console.log(finalGrid);
