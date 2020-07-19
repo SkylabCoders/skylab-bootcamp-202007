4aba4519c729c4847f3907225c08df35bb085acb
Gerard Ramon <gerardramonp@gmail.com>
2020-07-19 11:03:16 +0200

    Add test files to game

--- gerard-ramon/Diary/JavaScript/this.js
+++ gerard-ramon/Diary/JavaScript/this.js
@@ -1,41 +1,15 @@
-<<<<<<< HEAD
 var globalObject = this
 var foo = () => this
 
 const obj = {
-	name: 'dani',
-	sayHi: {
-		saybye: {
-			sayNothing: {
-				saySomething: () => this,
-			},
-		},
-	},
+    name: 'dani',
+    sayHi: {
+        saybye: {
+            sayNothing: {
+                saySomething: () => this,
+            },
+        },
+    },
 }
 
 console.log(obj.sayHi.saybye.sayNothing.saySomething())
-=======
-function Bowl() {}
-
-Bowl.prototype.length = 0;
-
-Bowl.prototype.forEach = function (callback) {
-  for (let i = 0; i < this.length; i++) {
-    callback(this[i]);
-  }
-};
-
-const myBowl = new Bowl();
-
-myBowl.length++;
-myBowl[0]=1;
-myBowl.length++;
-myBowl[1]=1;
-myBowl.length++;
-myBowl[2]=1;
-
-
-myBowl.forEach(function (elem) {
-  console.log(elem * 2);
-});
->>>>>>> origin
