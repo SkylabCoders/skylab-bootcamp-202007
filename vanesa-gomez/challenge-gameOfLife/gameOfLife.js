b092dcec86207de3cb6389a4ecd36ba327d90028
Vanesa Gómez <v.cucchiararo@gmail.com>
2020-07-19 14:40:08 +0200

    add states cells

--- vanesa-gomez/challenge-gameOfLife/gameOfLife.js
+++ vanesa-gomez/challenge-gameOfLife/gameOfLife.js
@@ -6,7 +6,15 @@ let initialState = [
     [0,0,0,0,0]
 ];
 
-let secondState = [
+let currentState = [
+    [0,0,0,0,0],
+    [0,0,0,0,0],
+    [0,1,1,1,0],
+    [0,0,0,0,0],
+    [0,0,0,0,0]
+]
+
+let nextState = [
     [0,0,0,0,0],
     [0,0,0,0,0],
     [0,0,0,0,0],
@@ -16,42 +24,54 @@ let secondState = [
 
 let counter = 0;
 function playGame(){
-    for(let i = 0; i < initialState.length; i++){
-      for(let j = 0; j < initialState[i].length; j++){
+    for(let i = 0; i < currentState.length; i++){
+      for(let j = 0; j < currentState[i].length; j++){
           let numberOfNeighbors = countNeighbors(i, j);
-          console.log(`El elemento [${i}][${j}] tiene ${numberOfNeighbors} vecinos`);
+          if(currentState[i][j] === 1) {            // SI ESTA VIVA
+              if(numberOfNeighbors === 2 || numberOfNeighbors === 3) {
+                  nextState[i][j] = 1;
+              } else {
+                  nextState[i][j] = 0;
+              }
+          } else {                              //SI ESTA MUERTA
+              if(numberOfNeighbors === 3) {
+                  nextState[i][j] = 1;
+              }
+          }
+          //   console.log(`El elemento [${i}][${j}] tiene ${numberOfNeighbors} vecinos`);
         }
     }
+    console.log(nextState)
 }
 
 function countNeighbors(i, j) {
     let count = 0;
 
-    for(let x = 0; x < initialState.length; x++) {
-        for(let y = 0; y < initialState[x].length; y++){
+    for(let x = 0; x < currentState.length; x++) {
+        for(let y = 0; y < currentState[x].length; y++){
             if(x == i-1 && y == j-1) {
-                if(initialState[x][y] == 1 ) count++
+                if(currentState[x][y] == 1 ) count++
             }
             if(x == i-1 && y == j) {
-                if(initialState[x][y] == 1 ) count++
+                if(currentState[x][y] == 1 ) count++
             }
             if(x == i-1 && y == j+1) {
-                if(initialState[x][y] == 1 ) count++
+                if(currentState[x][y] == 1 ) count++
             }
             if(x == i && y == j-1) {
-                if(initialState[x][y] == 1 ) count++
+                if(currentState[x][y] == 1 ) count++
             }
             if(x == i && y == j+1) {
-                if(initialState[x][y] == 1 ) count++
+                if(currentState[x][y] == 1 ) count++
             }
             if(x == i+1 && y == j-1) {
-                if(initialState[x][y] == 1 ) count++
+                if(currentState[x][y] == 1 ) count++
             }
             if(x == i+1 && y == j) {
-                if(initialState[x][y] == 1 ) count++
+                if(currentState[x][y] == 1 ) count++
             }
             if(x == i+1 && y == j+1) {
-                if(initialState[x][y] == 1 ) count++
+                if(currentState[x][y] == 1 ) count++
             }
           }
       }
