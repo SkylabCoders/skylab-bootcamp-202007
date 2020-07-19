35834929f41309c0fdc1ef440602ee72b7b5ea4d
Pablo <pablo_hortelano@hotmail.comm>
2020-07-20 01:51:10 +0200

    Adding dynamic grid cells lives calculator

--- pablo-hortelano/chalenge-gameoflife/index.js
+++ pablo-hortelano/chalenge-gameoflife/index.js
@@ -28,31 +28,36 @@ function RandomizeGrid(testBoard) {
   this.setInitialLiveCells = function () {
     for (let i = 0; i < this.grid[0]; i++) {
       let individualObject = {};
-      individualObject = { //Defining the object row that contains all the object-columns
+      individualObject = {
+        //Defining the object row that contains all the object-columns
         row: `${i}`,
         elements: [],
       };
       this.state.push(individualObject);
       for (let j = 0; j < this.grid[1]; j++) {
-        this.state[i].elements.push({ //Defining each individual cell of each row and giving them some properties
+        this.state[i].elements.push({
+          //Defining each individual cell of each row and giving them some properties
           column: `${i}`,
           class: `game-row-${i}-column-${j}`,
-          live: this.randomLiveOrDead() //Random function to determine if the cell is alive or not
+          liveCelssAround: 0,
+          live: this.randomLiveOrDead(), //Random function to determine if the cell is alive or not
         });
       }
     }
   };
-  this.randomLiveOrDead = function () { //Change this method code to alter proportion of live cells in the board
-    let max = 2;
+  this.randomLiveOrDead = function () {
+    //Change this method code to alter proportion of live cells in the board
+    let max = 5;
     let min = 0;
-    let binary = Math.floor(Math.random() * (max - min)) + min; 
+    let binary = Math.floor(Math.random() * (max - min)) + min;
     return binary === 1 ? true : false;
   };
 }
 
 function paintInitialBoard(arrayOfObjects) {
   this.arrayOfObjects = arrayOfObjects;
-  this.filterLiveCells = function () { //This method take an array of all the elements and return just an array of classNames
+  this.filterLiveCells = function () {
+    //This method take an array of all the elements and return just an array of classNames
     let newRow;
     let temporalArr = [];
     let definitiveArr = [];
@@ -67,7 +72,8 @@ function paintInitialBoard(arrayOfObjects) {
     }
     return definitiveArr;
   };
-  this.paintRightClasses = function (classNamesOfcellsToPaint) { //This method take an array of classNames, search for the elements with these classNames and paint them
+  this.paintRightClasses = function (classNamesOfcellsToPaint) {
+    //This method take an array of classNames, search for the elements with these classNames and paint them
     for (let elem of classNamesOfcellsToPaint) {
       let selector = document.querySelector(`.${elem}`);
       let newClass = selector.classList.value;
@@ -77,13 +83,11 @@ function paintInitialBoard(arrayOfObjects) {
   };
 }
 
-//newBoardSet.state[0].elements.filter(elem => elem.live)
-
-function setGame(rowNumber = 40, columnNumber = 40) {
-  rowNumber = document.querySelector("#number-rows").value;
-  columnNumber = document.querySelector("#number-columns").value;
-
-  if ((rowNumber || columnNumber) >= 41) {
+function setGame() {
+  //Set the game and call start game passing an object as parameter
+  let rowNumber = document.querySelector("#number-rows").value;
+  let columnNumber = document.querySelector("#number-columns").value;
+  if ((rowNumber || columnNumber) >= 41 || (rowNumber || columnNumber) === "") {
     rowNumber = 40;
     columnNumber = 40;
   }
@@ -95,94 +99,90 @@ function setGame(rowNumber = 40, columnNumber = 40) {
   } //This while delete the previous board (if there are a previous one)
   let testBoard = new ConstructBoard(rowNumber, columnNumber); //Just require the desire number of rows and columns
   testBoard.createBoard();
-
   let newBoardSet = new RandomizeGrid(testBoard.grid); //Require and array, the first position of the array is the number of rows, second one number of coumns
   newBoardSet.setInitialLiveCells(); //This method configure which cells have an initial state of live true and wich not
   let blankBoard = new paintInitialBoard(newBoardSet.state); //
   let classNamesOfcellsToPaint = blankBoard.filterLiveCells(); // It returns an array of classNames of cells that are alive
   blankBoard.paintRightClasses(classNamesOfcellsToPaint); //Send the list of classNames of cells that are live and paint them
+
+  startGame(newBoardSet, testBoard.grid[0]);
 }
 
-function StartGame() {
-  this.displayRound = function(value = 0) {
-    document.querySelector(".game-text").textContent = `Round nº ${value}`;
+function startGame(testBoard, maxSize) {
+  document.querySelector(".game-text").textContent = `Round nº ${0}`;
+  let state = testBoard.state;
+  let i = 0;
+  console.log(state);
+
+  let myLoop = function () {
+    setTimeout(function () {
+      i++;
+      if (i < 4) {
+        document.querySelector(".game-text").textContent = `Round nº ${i}`;
+        (function recalculateNumberOfCellsAlive() {
+          for (let i = 0; i < state.length; i++) {
+            for (let j = 0; j < state[i].elements.length; j++) {
+              setGridToCalculateLifes(state, i, j, 2)
+            }
+          }
+        })();
+
+        myLoop();
+      }
+    }, 1000);
+  };
+
+  myLoop();
+}
+
+function setGridToCalculateLifes(state,   i, j, maxSize) {
+  let upper, upperLeft, upperRight, left, right, bottom, bottomLeft, bottomRight
+  let emptyArr = [];
+  let cont = 0
+
+  if (i === 0 & j === 0) {
+      right = state[i].elements[j + 1].live;
+      emptyArr.push(right)
+      bottom = state[i + 1].elements[j].live;
+      emptyArr.push(bottom)
+      bottomRight = state[i + 1].elements[j + 1].live;
+      emptyArr.push(bottomRight)
+      debugger;
+  } else if (i === 0 && j === maxSize) {
+      left = state[i].elements[j - 1].live;
+      bottom = state[i + 1].elements[j].live;
+      bottomLeft = state[i + 1].elements[j - 1];
+  } else if (i === maxSize && j === 0) {
+      upper = state[i - 1].elements[j].live;
+      upperRight = state[i - 1].elements[j + 1].live;
+      right = state[i].elements[j + 1];
+  } else if (i === maxSize && j === maxSize) {
+      upper = state[i - 1].elements[j].live;
+      upperLeft = state[i - 1].elements[j - 1].live;
+      left = state[i].elements[j - 1].live;
+  } else {
+      upperLeft = state[i - 1].elements[j - 1].live;
+      upper = state[i - 1].elements[j].live;
+      upperRight = state[i - 1].elements[j + 1].live;
+      left = state[i].elements[j - 1].live;
+      right = state[i].elements[j + 1].live;
+      bottomLeft = state[i + 1].elements[j - 1].live;
+      bottom = state[i + 1].elements[j].live;
+      bottomRight = state[i + 1].elements[j + 1].live;
   }
-  this.createMatrixForEachCell = function() {}
-}
-/*
-function makeChanges(selector, ) {
-  let selector = document.querySelector(".row-1-column-4")
-  selector.setAttribute('class', 'paintItInBlack')
-}
-
-function algorithmDance(algorithmType, rounds) {
-  
+  for(let boolean of emptyArr) {
+    if(boolean === true) {
+      cont ++
+    }
+  }
+  console.log(state[i].elements[j].liveCelssAround);
 }
 
-
-document.addEventListener('click', makeChanges)
-
-// WITH CANVAS
-/* const drawBoard = (ctx, step) => {
-    for (let i = 0; i < 8; i++) {
-      for (let j = 0; j < 8; j++) {
-        ctx.fillStyle = (i + j) & 1 ? "black" : "white";
-        ctx.fillRect(j * step, i * step, step, step);
-      }
-    }
-  };
-  
-  const drawPieces = (ctx, y, color, step) => {
-    ctx.fillStyle = color;
-    
-    for (let i = y; i < 2 * step + y; i += step) {
-      for (let j = step / 2; j < 8 * step; j += step) {
-        ctx.beginPath();
-        ctx.arc(j, i - step / 2, step / 3, 0, Math.PI * 2);
-        ctx.fill();
-      }
-    }
-  };
-  
-  const step = 60;
-  const c = document.createElement("canvas");
-  c.height = c.width = step * 8;
-  document.body.appendChild(c);
-  const ctx = c.getContext("2d");
-  
-  drawBoard(ctx, step);
-  drawPieces(ctx, step, "red", step);
-  drawPieces(ctx, step * 7, "blue", step); */
-
-/*
-  const drawBoard = (ctx, step) => {
-    for (let i = 0; i < 10; i++) {
-      for (let j = 0; j < 10; j++) {
-        ctx.fillStyle = (i + j) & 1 ? "black" : "white"; //Define a red fill-color for the rectangle:
-        ctx.fillRect(j * step, i * step, step, step); //fillRect()  de la API Canvas 2D dibuja un rectángulo relleno en la posición ( x, y ).
-      }
-    }
-  };
-  
-  const drawPieces = (ctx, y, color, step) => {
-    ctx.fillStyle = color;
-    
-    for (let i = y; i < 2 * step + y; i += step) {
-      for (let j = step / 2; j < 10 * step; j += step) {
-        ctx.beginPath(); //beginPath() del API Canvas 2D comienza una nueva ruta vaciando la lista de sub-rutas
-        ctx.arc(j, i - step / 2, step / 3, 0, Math.PI * 2); //The arc() method creates an arc/curve (used to create circles, or parts of circles).
-        ctx.fill(); //The fill() method fills the current drawing (path). The default color is black.
-      }
-    }
-  };
-  
-  const step = 60; //Tamaño
-  const c = document.createElement("canvas");
-  c.height = c.width = step * 10;
-  document.body.appendChild(c);
-  const ctx = c.getContext("2d"); //crea un elemento canvas y le da contexto 2d
-  
-  drawBoard(ctx, step);
-  drawPieces(ctx, step, "red", step);
-  drawPieces(ctx, step * 9, "blue", step); //Con el step * 10 sleccionasla posición vertical del tablero, si cambias step cambia tamaño de los pixeles
-  */
+/* let one = state[i - 1].elements[j - 1].live
+              let two = state[i - 1].elements[j].live
+              let three = state[i - 1].elements[j + 1]
+              let fourth = state[i].elements[j - 1]
+              let five = state[i].elements[j + 1]
+              let six = state[i + 1].elements[j - 1]
+              let seven = state[i + 1].elements[j]
+              let eight = state[i + 1].elements[j + 1]  */
