a748b95234352f2cc023bc547c91b84166ee0573
gemmas95 <gemma1030@gmail.com>
2020-07-20 11:52:21 +0200

    my gameof life change print

--- Gemma-Sanz/Week 2/Challenge-game-of-life/gameOfLife-done.js
+++ Gemma-Sanz/Week 2/Challenge-game-of-life/gameOfLife-done.js
@@ -19,18 +19,16 @@ function newStage(){
         input = output;
         output = []
     }
+    printResult(input)
     for (let i = 0; i < input.length; i++) {
         output.push([]);
         for (let j = 0; j < input[i].length; j++) {
             output[i].push(input[i][j]);                
         }
-    }
-//printResult AQUI????
-//    printResult()
+    }    
     runStage(output, input);
 
 //Prints stage 2
-//    return newStage(output);
 };
 //We run into output
 function runStage (output, input){
@@ -84,37 +82,36 @@ function changeNum (neightbours, inputCel){
 
 //Calling the function
 
-/*
+
 //set interval
 //setInterval(function ({newStage()}), 3000);
 
-let start = document.querySelector(".start").addEventListener("click", function(){
-    event.preventDefault();
+let start = null;
+document.querySelector(".start").addEventListener("click", function(){
+    event.preventDefault(event);
     if(start === null){
         start = setInterval(function(){
             newStage(originalStage)
-        })
+        },2000);
     }
 })
 
-let pause = document.querySelector(".pause").addEventListener("click", function(){
-    event.preventDefault();
-    if(start !== null){
-        clearInterval(start)
-    })
-}
+document.querySelector(".pause").addEventListener("click", function(){
+    event.preventDefault(event);
+    clearInterval(start)
+})
 
-let reset = document.querySelector(".reset").addEventListener("click", function(){
+document.querySelector(".reset").addEventListener("click", function(){
     event.preventDefault();
     window.location.reload();
 })
 
-function printResult (originalStage){
-    let screen = querySelectorAll(.cel);
+function printResult (input){
+    let screen = document.querySelectorAll(".cel");
     let counter = 0;
-    for (let i = 0; i < originalStage.length; i++) {
-        for (let j = 0; j < originalStage.length; j++) {
-            if(originalStage[i][j]===1){
+    for (let i = 0; i < input.length; i++) {
+        for (let j = 0; j < input.length; j++) {
+            if(input[i][j]===1){
                 screen[counter].style.backgroundColor = "orange";
             } else {
                 screen[counter].style.backgroundColor = "black";
@@ -123,8 +120,3 @@ function printResult (originalStage){
         }      
     }
 }
-
-
-*/
-
-//cel.addEventListener("click", function () => ??);
