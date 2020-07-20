2d35033b6a85404e78c68c069584466b638331d1
gemmas95 <gemma1030@gmail.com>
2020-07-20 10:11:38 +0200

    added testing review

--- Gemma-Sanz/Week 2/Challenge-game-of-life/GameoflifeSpec.js
+++ Gemma-Sanz/Week 2/Challenge-game-of-life/GameoflifeSpec.js
@@ -1,4 +1,25 @@
-describe('Game of life testing', function () {
+describe('life', function () {
+	let life;
+	beforeEach(function (){
+		life = new Life();
+	});
+
+	it('should create', function () {
+		expect(life).toBeTruty();
+	});
+//Vamos recreanto los escenarios del README.md, este es el Scenario#1
+	it('should remain stable when no initialState', function () {
+		expect(life.next(undefined)).toBeTruty();
+		//next sera una propiedad dentro de mi objeto life
+		expect(life.next(null)).toBeTruty();
+		expect(life.next()).toBeTruty();
+		//no es recomendable poner más de un expect dentro el it, pero és el último tiene que responder la pregunta
+	});
+//Scenario #2
+	it('should work with Blinker states', function () {
+		expect(life.next(blinker.initialState)).toEqual(blinker.secondState);
+	});
+
 	let initialStage = [
 		[0,0,0,0,0,0,0],
 		[0,0,0,0,0,0,0],
@@ -14,6 +35,50 @@ describe('Game of life testing', function () {
         [0,0,0,1,0,0,0],
         [0,0,0,0,0,0,0]
 	];
+
+	it('should work with Toad states', function () {
+		expect(life.next(toad.initialState)).toEqual(toad.secondState);
+	});
+
+    let initialState = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 1, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ];
+    
+    let currentState = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0],
+        [0, 1, 0, 0, 1, 0],
+        [0, 1, 0, 0, 1, 0],
+        [0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+	];
+	it('should work with Beacon states', function () {
+		expect(life.next(beacon.initialState)).toEqual(beacon.secondState);
+	});
+
+    let initialState = [
+        [0, 0, 0, 0, 0, 0],
+		[0, 1, 1, 0, 0, 0],
+		[0, 1, 0, 0, 0, 0],
+		[0, 0, 0, 0, 1, 0],
+		[0, 0, 0, 1, 1, 0],
+		[0, 0, 0, 0, 0, 0]
+    ];
+    
+    let currentState = [
+        [0, 0, 0, 0, 0, 0],
+		[0, 1, 1, 0, 0, 0],
+		[0, 1, 1, 0, 0, 0],
+		[0, 0, 0, 1, 1, 0],
+		[0, 0, 0, 1, 1, 0],
+		[0, 0, 0, 0, 0, 0]
+	];
+	
 	let neightbours = 3
 
 	it('should have a finalStage', function () {
