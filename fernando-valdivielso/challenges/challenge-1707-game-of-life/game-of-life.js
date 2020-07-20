96c18a52e64f8a44784b8121d67542b9417d0f56
Fernando Valdivielso <fvaldivielso@outlook.com>
2020-07-20 10:20:58 +0200

    whatever

--- fernando-valdivielso/challenges/challenge-1707-game-of-life/game-of-life.js
+++ fernando-valdivielso/challenges/challenge-1707-game-of-life/game-of-life.js
@@ -1,3 +1,4 @@
+
 // Select color input
 let color = document.querySelector('#colorPicker');
 
@@ -6,14 +7,22 @@ let myTable = document.getElementById('pixelCanvas');
 
 // Event listener on Submit button
 let picker = document.getElementById('sizePicker');
-
 picker.addEventListener('submit', grid);
 
+
 // Game
+let turn = document.getElementById('next-turn');
+turn.addEventListener('click', function (event) {
+    checkNeighbours(nextArray);
+})
 
-
-let row = null;;
+let secondState = [];
+let elements = null;
+let row = null;
 let column = null;
+let newArray;
+let nextArray;
+let neighbours = null;
 
 // When size is submitted by the user, call grid()
 function grid(event) {
@@ -21,13 +30,16 @@ function grid(event) {
     event.preventDefault();
     row = parseInt(document.getElementById('inputHeight').value);
     column = parseInt(document.getElementById('inputWidth').value);
-    makeGrid(row, column);
-    makeArray(row, column);
+    newArray = makeGrid(row, column);
+    nextArray = makeArray(row, column);
+
 }
+
 
 // When grid() runs, call makeGrid()
 
-function makeGrid(row, column, game) {
+function makeGrid(row, column) {
+    // debugger;
     for (let i = 0; i <= row - 1; i++) {
         let myRow = myTable.insertRow();
         for (let j = 0; j <= column - 1; j++) {
@@ -35,8 +47,9 @@ function makeGrid(row, column, game) {
             myCell.addEventListener('click', function () {
                 myCell.style.backgroundColor = color.value;
                 myCell.id = i + '-' + j;
-                let elements = Array.from(document.getElementsByTagName('td'));
-                changeArray(makeArray(row, column), elements);
+                elements = Array.from(document.getElementsByTagName('td'));
+                changeArray(nextArray, elements);
+
                 console.log(elements);
             });
         };
@@ -50,7 +63,7 @@ function makeArray(row, column) {
     for (let i = 1; i <= column; i++) {
         let width = [];
         for (let j = 0; j < row; j++) {
-            width.push(0);
+            width.push(false);
         }
         game.push(width);
     }
@@ -58,22 +71,386 @@ function makeArray(row, column) {
 }
 
 function changeArray(game, elements) {
-    console.log(game);
+
     let rowCount = 0;
     let columnCount = 0;
+    let changedGame = game;
 
     for (let i = 0; i < elements.length; i++) {
         if (elements[i].id === (rowCount + '-' + columnCount)) {
-            game[rowCount][columnCount] = 1;
-            console.log(game);
+            changedGame[rowCount][columnCount] = true;
+
         }
         columnCount++;
-        if(columnCount === column){
+
+        if (columnCount === column) {
             rowCount++;
             columnCount = 0;
         }
     }
+    console.log('changed game' + changedGame);
+    secondState = changedGame;
+    return changedGame;
 }
+
+
+
+
+function checkNeighbours(array) {
+    // debugger;
+    
+    let aliveCount = 0;
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array[i].length; j++) {
+            if (array[i][j] === true) {
+                if (i === 0 && j === 0) {
+                    if (array[i][j + 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i + 1][j + 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i + 1][j] === true) {
+                        aliveCount++;
+                    }
+                }
+                if (i === 0 && (j > 0 && j < array[i].length - 1)) {
+                    if (array[i][j - 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i][j + 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i + 1][j + 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i + 1][j] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i + 1][j - 1] === true) {
+                        aliveCount++;
+                    }
+                }
+                if (i === 0 && j === array[i].length - 1) {
+                    if (array[i][j - 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i + 1][j] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i + 1][j - 1] === true) {
+                        aliveCount++;
+                    }
+                }
+                if ((i > 0 && i < array.length - 1) && (j === 0)) {
+                    if (array[i - 1][j] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i - 1][j + 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i][j + 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i + 1][j + 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i + 1][j] === true) {
+                        aliveCount++;
+                    }
+                }
+                if ((i > 0 && i < array.length - 1) && (j > 0 && j < array[i].length - 1)) {
+                    if (array[i - 1][j - 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i - 1][j] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i - 1][j + 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i][j + 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i + 1][j + 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i + 1][j] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i + 1][j - 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i][j - 1] === true) {
+                        aliveCount++;
+                    }
+                }
+                if ((i > 0 && i < array.length - 1) && (j > 0 && j === array[i].length - 1)) {
+                    if (array[i - 1][j] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i + 1][j] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i + 1][j - 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i][j - 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i - 1][j - 1] === true) {
+                        aliveCount++;
+                    }
+                }
+                if (i === array.length - 1 && j === 0) {
+                    if (array[i - 1][j] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i - 1][j + 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i][j + 1] === true) {
+                        aliveCount++;
+                    }
+                }
+                if (i === array.length - 1 && j < array[i].length - 1) {
+                    if (array[i - 1][j] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i - 1][j + 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i][j + 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i][j - 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i - 1][j - 1] === true) {
+                        aliveCount++;
+                    }
+                }
+                if (i === array.length - 1 && j === array[i].length - 1) {
+                    if (array[i - 1][j] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i][j - 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i - 1][j - 1] === true) {
+                        aliveCount++;
+                    }
+                }
+                let position = i + '-' + j;
+                console.log('estan vivos ' + aliveCount);
+
+                if (aliveCount < 2 || aliveCount > 3) {
+                    array[i][j] = false;
+                    secondState[i][j] = false;
+                    document.getElementById(position).style.backgroundColor = 'white';
+                } else if (aliveCount === 2 || aliveCount === 3) {
+                    array[i][j] = true;
+                    secondState[i][j] = true;
+                
+                }
+                aliveCount = 0;
+
+            }else if (array[i][j] === false) {
+                // debugger;
+                if (i === 0 && j === 0) {
+                    if (array[i][j + 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i + 1][j + 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i + 1][j] === true) {
+                        aliveCount++;
+                    }
+                }
+                if (i === 0 && (j > 0 && j < array[i].length - 1)) {
+                    if (array[i][j - 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i][j + 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i + 1][j + 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i + 1][j] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i + 1][j - 1] === true) {
+                        aliveCount++;
+                    }
+                }
+                if (i === 0 && j === array[i].length - 1) {
+                    if (array[i][j - 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i + 1][j] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i + 1][j - 1] === true) {
+                        aliveCount++;
+                    }
+                }
+                if ((i > 0 && i < array.length - 1) && (j === 0)) {
+                    if (array[i - 1][j] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i - 1][j + 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i][j + 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i + 1][j + 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i + 1][j] === true) {
+                        aliveCount++;
+                    }
+                }
+                if ((i > 0 && i < array.length - 1) && (j > 0 && j < array[i].length - 1)) {
+                    if (array[i - 1][j - 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i - 1][j] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i - 1][j + 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i][j + 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i + 1][j + 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i + 1][j] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i + 1][j - 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i][j - 1] === true) {
+                        aliveCount++;
+                    }
+                }
+                if ((i > 0 && i < array.length - 1) && (j > 0 && j === array[i].length - 1)) {
+                    if (array[i - 1][j] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i + 1][j] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i + 1][j - 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i][j - 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i - 1][j - 1] === true) {
+                        aliveCount++;
+                    }
+                }
+                if (i === array.length - 1 && j === 0) {
+                    if (array[i - 1][j] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i - 1][j + 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i][j + 1] === true) {
+                        aliveCount++;
+                    }
+                }
+                if (i === array.length - 1 && j < array[i].length) {
+                    if (array[i - 1][j] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i - 1][j + 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i][j + 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i][j - 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i - 1][j - 1] === true) {
+                        aliveCount++;
+                    }
+                }
+                if (i === array.length - 1 && j === array[i].length - 1) {
+                    if (array[i - 1][j] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i][j - 1] === true) {
+                        aliveCount++;
+                    }
+                    if (array[i - 1][j - 1] === true) {
+                        aliveCount++;
+                    }
+                }
+                let position = i + '-' + j;
+                console.log('estan vivos ' + aliveCount);
+                if (aliveCount === 3) {
+                    array[i][j] = true;
+                    secondState[i][j] = true;
+                    document.getElementById(position).style.backgroundColor = color.value;
+
+                }
+                aliveCount = 0;
+            }
+        }
+    }
+    revertArray(changedGame, elements);
+    return aliveCount;
+}
+        
+
+function revertArray(changedGame, elements) {
+    let rowCount = 0;
+    let columnCount = 0;
+    let secondState = changedGame;
+
+    for (let i = 0; i < seconState.length; i++) {
+        for (let j = 0; j < secondState[i].length; j++) {
+            let index = i + j;
+            if (secondState [i][j] === true) {
+                let id = i + '-' + j;
+                elements[index].id(id);  
+            }
+            
+        }        
+    }
+    document.
+}
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
