ae9cfc64d85d590af0ff23715bf39a13fe3437ca
Anna Codina <anna.co91@gmail.com>
2020-07-20 23:20:23 +0200

    create and interacgtive html and css calculator

---
+++ anna-codina/calculator/calculator-function.js
@@ -0,0 +1,26 @@
+'use strict';
+const Calculator = function () {
+    function operation(firstOperator, secondOperator, typeOperation) {
+        if (!firstOperator || !secondOperator) {
+            return;
+        }
+        let result = 0;
+        switch (typeOperation) {
+            case addSymbol:
+                result = firstOperator + secondOperator;
+                break;
+            case subSymbol:
+                result = firstOperator - secondOperator;
+                break;
+            case mulSymbol:
+                result = firstOperator * secondOperator;
+                break;
+            case divSymbol:
+                result = firstOperator / secondOperator;
+                break;
+        }
+        return result;
+    }
+
+    return { operation, }
+}
--- anna-codina/calculator/calculator.js
+++ anna-codina/calculator/calculator.js
@@ -1,25 +1,58 @@
-const Calculator = function () {
-    function operation(firstOperator, secondOperator, typeOperation) {
-        if (!firstOperator || !secondOperator) {
-            return;
-        }
-        let result = 0;
-        switch (typeOperation) {
-            case addSymbol:
-                result = firstOperator + secondOperator;
-                break;
-            case subSymbol:
-                result = firstOperator - secondOperator;
-                break;
-            case mulSymbol:
-                result = firstOperator * secondOperator;
-                break;
-            case divSymbol:
-                result = firstOperator / secondOperator;
-                break;
-        }
-        return result;
-    }
+'use strict';
+const calculator = new Calculator();
+let actualOperator = '';
+let actualResult = document.getElementById('result');
+let actualOperationType = null;
+let firstOperator = null;
+let secondOperator = null;
 
-    return { operation, }
+function clearResult() {
+    document.getElementById('result').innerHTML = 0;
+    actualOperator = '';
+    actualOperationType = null;
+    firstOperator = null;
+    secondOperator = null;
+}
+function undoLastStep() {
+    actualOperator = '';
+    document.getElementById('result').innerHTML = 0;
+}
+
+function addValorToOperator(numberValor) {
+    if (actualOperationType === null) {
+        firstOperator = null;
+        secondOperator = null;
+    }
+    actualOperator += numberValor;
+    document.getElementById("result").innerHTML = actualOperator;
+}
+
+function setActualSymbol(symbolButtonValor) {
+    if (actualOperator === '' && !actualOperationType && !firstOperator) {
+        return;
+    }
+    if (!firstOperator) {
+        firstOperator = actualOperator;
+        actualOperator = '';
+        firstOperator = parseFloat(firstOperator);
+    } else if (actualOperator != '') {
+        secondOperator = actualOperator;
+        actualOperator = '';
+        secondOperator = parseFloat(secondOperator);
+        firstOperator = calculator.operation(firstOperator, secondOperator, actualOperationType);
+    }
+    actualOperationType = symbolButtonValor;
+    document.getElementById("result").innerHTML = firstOperator;
+}
+
+function showResult() {
+    //this verification is in order to prevent show UNDEFINED to the user.
+    if (actualOperator != '' && actualOperationType) {
+        secondOperator = actualOperator;
+        actualOperator = '';
+        secondOperator = parseFloat(secondOperator);
+        firstOperator = calculator.operation(firstOperator, secondOperator, actualOperationType);
+        document.getElementById("result").innerHTML = firstOperator;
+    }
+    actualOperationType = null;
 }
