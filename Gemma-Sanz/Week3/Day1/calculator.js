da110b69cc4d59dfa9bdee6c96df2f5fbe8fe257
gemmas95 <gemma1030@gmail.com>
2020-07-20 23:50:05 +0200

    trying to do calculator

--- Gemma-Sanz/Week3/Day1/calculator.js
+++ Gemma-Sanz/Week3/Day1/calculator.js
@@ -1,57 +1,70 @@
 function Calculate() {
 
     const [button7, button8, button9, button4, button5, button6, button1, button2, button3, button0] = document.querySelectorAll('.button')
-    const buttons = [button7, button8, button9, button4, button5, button6, button1, button2, button3, button0]
+    const allButtons = [button7, button8, button9, button4, button5, button6, button1, button2, button3, button0]
 
-    for (var i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', () => {
-            return currentNumber = Number(buttons[i].innerHTML)
+
+    for (var i = 0; i < allButtons.length; i++) {
+        allButtons[i].addEventListener('click', () => {
+            currentNumber += Number(buttons[i].innerHTML)
+            return screen.innerHTML = currentNumber
         })
     }
 
-    let screen = function () {
+    const [buttonac, buttondiv, buttonmult, buttonrest, buttonsum, buttonequal] = document.querySelectorAll(".operation")
+    const allOperations = [buttonac, buttondiv, buttonmult, buttonrest, buttonsum, buttonequal];
+    let otherNumber = 0;
+    for (var j = 0; j < allOperations.length; j++) {
+        allOperations[i].addEventListener("click", () => {
+                screen.innerHTML = currentNumber + operation[i].innerHTML;
+                otherNumber += Number(buttons[i].innerHTML)
+                screen.innerHTML = otherNumber;
+
+
+            }
+        });
+}
 
 
 
-    }
 
-    // TODO refactor 
+// TODO refactor 
 
-    let sum = function (num1, num2) {
-        result = num1 + num2
-        return result;
+let sum = function (num1, num2) {
+    result = num1 + num2
+    return result;
 
-    };
-    let rest = function (num1, num2) {
-        result = num1 - num2
+};
+let rest = function (num1, num2) {
+    result = num1 - num2
 
-        return result;
+    return result;
 
-    };
-    let div = function (num1, num2) {
-        result = num1 / num2
+};
+let div = function (num1, num2) {
+    result = num1 / num2
 
-        return result;
+    return result;
 
-    };
-    let multi = function (num1, num2) {
-        result = num1 * num2
+};
+let multi = function (num1, num2) {
+    result = num1 * num2
 
-        return result;
-    };
+    return result;
+};
 
-    let ac = function () {
-        return 0;
+let ac = function () {
+    return 0;
 
-    }
-    return {
-        sum,
-        rest,
-        div,
-        multi,
-        ac,
-        screen
-    }
+}
+return {
+    sum,
+    rest,
+    div,
+    multi,
+    ac,
+    screen
+}
 }
 
 // TODO
