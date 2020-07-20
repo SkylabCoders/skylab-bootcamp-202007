29d9e75a173a527bbf5557ac6f4e203d64754575
simonbesteiro <simonfernandezbesteiro@MacBook-Pro-de-Simon.local>
2020-07-20 17:13:30 +0200

    format document

--- simon-fernandez/2020-07-17/challenge/jasmine-standalone-3.5.0/spec/Game-of-life-Spec.js
+++ simon-fernandez/2020-07-17/challenge/jasmine-standalone-3.5.0/spec/Game-of-life-Spec.js
@@ -1,76 +1,86 @@
-describe('Game', function (){
-    let blinkerStart=null,blinkerEnd=null,toadStart=null,toadEnd=null,beaconStart=null,beaconEnd=null;
-    let game=null;
-    
-    beforeAll(function(){
-        ;
-        blinkerStart=[
-            [0,0,0,0,0],
-            [0,0,0,0,0],
-            [0,1,1,1,0],
-            [0,0,0,0,0],
-            [0,0,0,0,0]
-        ];
-        blinkerEnd=[
-            [0,0,0,0,0],
-            [0,0,1,0,0],
-            [0,0,1,0,0],
-            [0,0,1,0,0],
-            [0,0,0,0,0]
-        ];
-        toadStart=[
-            [0,0,0,0,0,0],
-            [0,0,0,0,0,0],
-            [0,0,1,1,1,0],
-            [0,1,1,1,0,0],
-            [0,0,0,0,0,0],
-            [0,0,0,0,0,0]
-        ];
-        toadEnd=[
-            [0,0,0,0,0,0],
-            [0,0,0,1,0,0],
-            [0,1,0,0,1,0],
-            [0,1,0,0,1,0],
-            [0,0,1,0,0,0],
-            [0,0,0,0,0,0]
-        ];
-        beaconStart=[
-            [0,0,0,0,0,0],
-            [0,1,1,0,0,0],
-            [0,1,1,0,0,0],
-            [0,0,0,1,1,0],
-            [0,0,0,1,1,0],
-            [0,0,0,0,0,0]
-        ];
-        beaconEnd=[
-            [0,0,0,0,0,0],
-            [0,1,1,0,0,0],
-            [0,1,0,0,0,0],
-            [0,0,0,0,1,0],
-            [0,0,0,1,1,0],
-            [0,0,0,0,0,0]
-        ]
-    
-        game= new Game(blinkerStart,blinkerEnd,toadStart,toadEnd,beaconStart,beaconEnd);
-    })
-    
-    it('Blinker first period should be', function(){
-        expect(game.blinkerStart()).toEqual(blinkerEnd);
-    })
-    it('Blinker second period should be',function(){
-        expect(game.blinkerEnd()).toEqual(blinkerStart);
-    })
-    it('Toad first period should be', function(){
-        expect(game.toadStart()).toEqual(toadEnd);
-    })
-    it('Toad second period should be',function(){
-        expect(game.toadEnd()).toEqual(toadStart);
-    })
-    it('Beacon first period should be', function(){
-        expect(game.beaconStart()).toEqual(beaconEnd);
-    })
-    it('Beacon second period should be',function(){
-        expect(game.beaconStart()).toEqual(beaconEnd);
-    })
-    
+describe('Game', function () {
+	let blinkerStart = null,
+		blinkerEnd = null,
+		toadStart = null,
+		toadEnd = null,
+		beaconStart = null,
+		beaconEnd = null
+	let game = null
+
+	beforeAll(function () {
+		blinkerStart = [
+			[0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0],
+			[0, 1, 1, 1, 0],
+			[0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0],
+		]
+		blinkerEnd = [
+			[0, 0, 0, 0, 0],
+			[0, 0, 1, 0, 0],
+			[0, 0, 1, 0, 0],
+			[0, 0, 1, 0, 0],
+			[0, 0, 0, 0, 0],
+		]
+		toadStart = [
+			[0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0],
+			[0, 0, 1, 1, 1, 0],
+			[0, 1, 1, 1, 0, 0],
+			[0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0],
+		]
+		toadEnd = [
+			[0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 1, 0, 0],
+			[0, 1, 0, 0, 1, 0],
+			[0, 1, 0, 0, 1, 0],
+			[0, 0, 1, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0],
+		]
+		beaconStart = [
+			[0, 0, 0, 0, 0, 0],
+			[0, 1, 1, 0, 0, 0],
+			[0, 1, 1, 0, 0, 0],
+			[0, 0, 0, 1, 1, 0],
+			[0, 0, 0, 1, 1, 0],
+			[0, 0, 0, 0, 0, 0],
+		]
+		beaconEnd = [
+			[0, 0, 0, 0, 0, 0],
+			[0, 1, 1, 0, 0, 0],
+			[0, 1, 0, 0, 0, 0],
+			[0, 0, 0, 0, 1, 0],
+			[0, 0, 0, 1, 1, 0],
+			[0, 0, 0, 0, 0, 0],
+		]
+
+		game = new Game(
+			blinkerStart,
+			blinkerEnd,
+			toadStart,
+			toadEnd,
+			beaconStart,
+			beaconEnd
+		)
+	})
+
+	it('Blinker first period should be', function () {
+		expect(game.blinkerStart()).toEqual(blinkerEnd)
+	})
+	it('Blinker second period should be', function () {
+		expect(game.blinkerEnd()).toEqual(blinkerStart)
+	})
+	it('Toad first period should be', function () {
+		expect(game.toadStart()).toEqual(toadEnd)
+	})
+	it('Toad second period should be', function () {
+		expect(game.toadEnd()).toEqual(toadStart)
+	})
+	it('Beacon first period should be', function () {
+		expect(game.beaconStart()).toEqual(beaconEnd)
+	})
+	it('Beacon second period should be', function () {
+		expect(game.beaconStart()).toEqual(beaconEnd)
+	})
 })
