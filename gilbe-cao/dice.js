c4ec193fe8227202910d99caf347ba8e7185f56a
Gilberto Cao <gilbe.cao@gmail.com>
2020-07-21 07:39:27 +0200

    add function to call math ceil and random

--- gilbe-cao/dice.js
+++ gilbe-cao/dice.js
@@ -1,14 +1,13 @@
 'use strict';
-/*
-const firstDice = 5;
-const secondDice = 3;
 
+const getDiceRoll = function () {
+  console.log('Rolling a dice...');
+  return Math.ceil(6 * Math.random());
+};
+
+const firstDice = getDiceRoll();
+const secondDice = getDiceRoll();
 
 console.log(firstDice);
 console.log(secondDice);
 console.log(firstDice + secondDice);
-*/
-
-const value = 6 * Math.random();
-const roll = Math.ceil(value);
-console.log(roll);
