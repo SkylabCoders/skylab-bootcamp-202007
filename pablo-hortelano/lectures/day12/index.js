2963e7e4f2500103bcdc184b105089d5889d5262
Pablo <pablo_hortelano@hotmail.comm>
2020-07-21 11:58:08 +0200

    Adding second example

--- pablo-hortelano/lectures/day12/index.js
+++ pablo-hortelano/lectures/day12/index.js
@@ -1,4 +1,4 @@
-//Testing currying and closures
+//Preservation variables
 
 function outside(x) {
   function inside(y) {
@@ -11,4 +11,14 @@ let first = outside(3);
 let result = first(5); // 8
 let result1 = outside(3)(5); //8
 
-//
+//Second example of preservation
+function A(x) {
+  function B(y) {
+    function C(z) {
+      console.log(x + y + z);
+    }
+    C(1);
+  }
+  B(2);
+}
+A(3);
