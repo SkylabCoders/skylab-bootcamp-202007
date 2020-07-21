32b125ffef82bf6c73215a30977cc36f56abaf6a
Gilberto Cao <gilbe.cao@gmail.com>
2020-07-21 07:31:43 +0200

    introduce math object

--- gilbe-cao/dice.js
+++ gilbe-cao/dice.js
@@ -1,8 +1,14 @@
 'use strict';
-
+/*
 const firstDice = 5;
 const secondDice = 3;
+
 
 console.log(firstDice);
 console.log(secondDice);
 console.log(firstDice + secondDice);
+*/
+
+const value = 6 * Math.random();
+const roll = Math.ceil(value);
+console.log(roll);
