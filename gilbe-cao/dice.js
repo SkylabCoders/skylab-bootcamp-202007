b0c0e9b6fd5ebbcd3559ee70394d7faeff4a0331
Gilberto Cao <gilbe.cao@gmail.com>
2020-07-21 08:08:08 +0200

    add documentation to variable scopes

--- gilbe-cao/dice.js
+++ gilbe-cao/dice.js
@@ -1,14 +1,22 @@
 'use strict';
 
+// getDiceRoll is a global variable because is declared out of every function at the highest level
+// All scripts and functions on a web page can access it.
 const getDiceRoll = function (diceSize) {
   console.log(`Rolling a dice with ${diceSize} sides...`);
-  return Math.ceil(diceSize * Math.random());
+  // Declare a local variable
+  const result = Math.ceil(diceSize * Math.random());
+  return result;
 };
 
-const firstDice = getDiceRoll(6);
-console.log(firstDice);
+const showResult = function () {
+  const firstDice = getDiceRoll(6);
+  console.log(firstDice);
 
-const secondDice = getDiceRoll(6);
-console.log(secondDice);
+  const secondDice = getDiceRoll(6);
+  console.log(secondDice);
 
-console.log('The sum is: ', firstDice + secondDice);
+  console.log('The sum is: ', firstDice + secondDice);
+};
+
+showResult();
