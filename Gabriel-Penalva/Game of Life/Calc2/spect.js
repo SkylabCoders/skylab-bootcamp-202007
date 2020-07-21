c3d26ad79cfe562a619a85c02756ba5c187e7cf3
Gabriel Penalva <gabripenalva42@gmail.com>
2020-07-21 09:29:31 +0200

    fix

--- Gabriel-Penalva/Game of Life/Calc2/spect.js
+++ Gabriel-Penalva/Game of Life/Calc2/spect.js
@@ -6,21 +6,6 @@ describe('Calculator', function () {
     // });
 
     it('Should return the sum', function () {
-<<<<<<< HEAD
-        expect(calcular.sum(test[0], test[1])).toEqual(15);
-    });
-
-    it('Should return the rest', function () {
-        expect(calcular.rest(test[0], test[1])).toEqual(5);
-    });
-
-    it('Should return the div', function () {
-        expect(calcular.div(test[0], test[1])).toEqual(2);
-    });
-
-    it('Should return the mult', function () {
-        expect(calcular.mult(test[0], test[1])).toEqual(50);
-=======
         expect(calcular.sum(test)).toEqual(15);
     });
 
@@ -34,7 +19,6 @@ describe('Calculator', function () {
 
     it('Should return the mult', function () {
         expect(calcular.mult(test)).toEqual(50);
->>>>>>> c0b66836e96b9eaed2ef42673972fbd8f77bbb91
     });
 
 });
--- Gabriel-Penalva/challenge js/values.js
+++ Gabriel-Penalva/challenge js/values.js
@@ -1,28 +1,21 @@
- function isEqual (a,b){
-    if ((Object.is(a,0) || Object.is(b,0)) && (Object.is(a,-0) || Object.is(b,-0))){
+function isEqual(a, b) {
+    if ((Object.is(a, 0) || Object.is(b, 0)) && (Object.is(a, -0) || Object.is(b, -0))) {
         return true;
-    }else if((Object.is(a,NaN) && Object.is(b,NaN))){
+    } else if ((Object.is(a, NaN) && Object.is(b, NaN))) {
         return false;
-    }else {
-        return Object.is(a,b);
+    } else {
+        return Object.is(a, b);
     }
 }
-<<<<<<< HEAD
-=======
-console.log(isEqual(NaN,0/-0));
+console.log(isEqual(NaN, 0 / -0));
 
-console.log(isEqual(0,0));
+console.log(isEqual(0, 0));
 
-console.log(isEqual(-0,0));
+console.log(isEqual(-0, 0));
 
-console.log(isEqual(0,-0));
+console.log(isEqual(0, -0));
 
-console.log(isEqual(-0,-0));
-<<<<<<< HEAD
->>>>>>> origin
-=======
->>>>>>> d4f03168a4813a4b4dca66a36d0f7bb079d346fa
->>>>>>> c0b66836e96b9eaed2ef42673972fbd8f77bbb91
+console.log(isEqual(-0, -0));
 
-let array = [0,0,0,0,0];
+let array = [0, 0, 0, 0, 0];
 console.log(array[-1]);
