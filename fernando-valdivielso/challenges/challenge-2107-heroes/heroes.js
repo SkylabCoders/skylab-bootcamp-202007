e59486fd71af62591c8b9572330893edc19f0081
Fernando Valdivielso <fvaldivielso@outlook.com>
2020-07-22 14:55:55 +0200

    hasta los cojones de git

--- fernando-valdivielso/challenges/challenge-2107-heroes/heroes.js
+++ fernando-valdivielso/challenges/challenge-2107-heroes/heroes.js
@@ -1,12 +1,15 @@
+let batman = new Heroes('Batman', 1);
+let superman = new Heroes('Superman', 5);
+
 function Heroes(name, id) {
 
     let id = null;
     let name = null;
 
     function createHero(name, id) {
-         this.name = name;
-         this.id = id;
-         return {name, id};
+        this.name = name;
+        this.id = id;
+        return { name, id };
     }
 
     function setId(id) {
@@ -28,11 +31,25 @@ function Heroes(name, id) {
     return { createHero, setId, getId, setName, getName }
 }
 
+let heading = document.getElementsByTagName('h2')[0];
+let heroId = document.getElementsByClassName(".details__id")[0];
+let heroName = document.getElementsByClassName(".details__name")[0];
+
+heroName.addEventListener('input', function () { 
+    if( heroName.value === superman.getName()) {
+        heading.innerHTML = superman.getName();
+    }
+  })
 
 
 
 
-// let batman = new Heroes();
+
+
+
+
+
+
 // batman.setName('peipit');
 // console.log(batman.getName())
 
