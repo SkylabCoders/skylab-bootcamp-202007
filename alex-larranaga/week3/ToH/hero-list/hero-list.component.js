279fe3b6e78d87564f80929783ea63371f94315e
Alex Larrañaga <alexurola@gmail.com>
2020-07-22 20:52:05 +0200

    Changed list element no buttons

--- alex-larranaga/week3/ToH/hero-list/hero-list.component.js
+++ alex-larranaga/week3/ToH/hero-list/hero-list.component.js
@@ -2,14 +2,18 @@ function HeroListComponent() {
 	const heroes = heroList;
 	const listElement = document.getElementById('list');
 
-	this.createList = function () {
+	this.onInit = function () {
+		createList();
+	};
+
+	const createList = function () {
 		for (let i = 0; i < heroes.length; i++) {
-			let listNodesElement = document.createElement('LI');
+			let listNodesElement = document.createElement('BUTTON');
 			listElement.appendChild(listNodesElement);
-			listNodesElement.innerHTML = heroes[i].id + heroes[i].name;
+			listNodesElement.innerHTML = heroes[i].id + '   ' + heroes[i].name;
 		}
 	};
 }
 
 const showHeroList = new HeroListComponent();
-showHeroList.createList();
+showHeroList.onInit();
