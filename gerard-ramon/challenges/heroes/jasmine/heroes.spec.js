6cfebfde6718226f2f1cf6fe7772368ab3d31e52
Gerard Ramon <gerardramonp@gmail.com>
2020-07-22 08:57:22 +0200

    Add something

--- gerard-ramon/challenges/heroes/jasmine/heroes.spec.js
+++ gerard-ramon/challenges/heroes/jasmine/heroes.spec.js
@@ -1,14 +1,19 @@
 describe('Heroes', function () {
-	let hero;
-	const newHeroName = 'Gilberto';
+	let myHero;
+	let newHeroName = 'Gilberto';
 
 	beforeEach(function () {
 		myHero = new Hero();
 	});
 
+	it('Should create a new hero', function () {
+		expect(myHero).toBeDefined();
+	});
+
 	it('Should have a Name and an Id', function () {
-		expect(myHero.getHeroName()).toBeDefined();
-		expect(myHero.getHeroId()).toBeDefined();
+		myHero.initializeHero('Superman', 1);
+		expect(myHero.getHeroName()).toBeTruthy();
+		expect(myHero.getHeroId()).toBeTruthy();
 	});
 
 	it('Should change the name of the hero', function () {
