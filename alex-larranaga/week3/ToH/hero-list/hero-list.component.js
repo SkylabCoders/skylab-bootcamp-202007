f22c771c179ebd45e361a78430789afa05108ec7
Alex Larrañaga <alexurola@gmail.com>
2020-07-22 20:32:23 +0200

    List generated dinamycally

--- alex-larranaga/week3/ToH/hero-list/hero-list.component.js
+++ alex-larranaga/week3/ToH/hero-list/hero-list.component.js
@@ -2,15 +2,14 @@ function HeroListComponent() {
 	const heroes = heroList;
 	const listElement = document.getElementById('list');
 
-	/* 	this.createList = function () {
-		for (let i = 0; i < this.heroes.length; i++) {
+	this.createList = function () {
+		for (let i = 0; i < heroes.length; i++) {
 			let listNodesElement = document.createElement('LI');
-			listElement.appendChild((listNodesElement.innerHTML = heroes[i]));
+			listElement.appendChild(listNodesElement);
+			listNodesElement.innerHTML = heroes[i].id + heroes[i].name;
 		}
-	} */
+	};
 }
 
 const showHeroList = new HeroListComponent();
 showHeroList.createList();
-
-console.log(heroes);
