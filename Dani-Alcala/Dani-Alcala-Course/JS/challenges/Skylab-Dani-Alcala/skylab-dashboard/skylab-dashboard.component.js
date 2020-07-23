e5d0c65bcb76866008af47300786c7c0ec4d8a08
Dani Alcala <daniamcode@gmail.com>
2020-07-23 19:03:28 +0200

    folder creation for the skylabers project

---
+++ Dani-Alcala/Dani-Alcala-Course/JS/challenges/Skylab-Dani-Alcala/skylab-dashboard/skylab-dashboard.component.js
@@ -0,0 +1,11 @@
+function Dashboard () {
+    const skylabers = skylabersList;
+    
+    this.onInit = function () {
+
+    }
+}
+
+const dashboard = new Dashboard();
+dashboard.onInit();
+
--- Dani-Alcala/Dani-Alcala-Course/JS/challenges/Skylab-Dani-Alcala/skylab.mock.js
+++ Dani-Alcala/Dani-Alcala-Course/JS/challenges/Skylab-Dani-Alcala/skylab.mock.js
@@ -1,10 +1,92 @@
-const skylabers = [
-    { id: 10, name: Fernando},
-    { id: 11, name: Vanesa},
-    { id: 12, name: Gerard},
-    { id: 13, name: Jordi},
-    { id: 14, name: Aleix},
-    { id: 15, name: Alex},
-    { id: 16, name: Gabri},
-    { id: 17, name: Esther},
+const skylabersList = [
+    { 
+        id: 11,
+        name: 'Fernando',
+        completedChallenges: 2,
+        address: {
+            city: 'Madrid',
+            country: 'Spain'
+        }
+    },
+    { 
+        id: 12,
+        name: 'Vanesa',
+        completedChallenges: 2,
+        address: {
+            city: 'Barcelona',
+            country: 'Spain'
+        }
+    },
+    { 
+        id: 13,
+        name: 'Gerard',
+        completedChallenges: 5,
+        address: {
+            city: 'Barcelona',
+            country: 'Catalonia'
+        }
+    },
+    { 
+        id: 14,
+        name: 'Jordi',
+        completedChallenges: 4,
+        address: {
+            city: 'Barcelona',
+            country: 'Catalonia'
+        }
+    },
+    { 
+        id: 15,
+        name: 'Ramon',
+        completedChallenges: 1,
+        address: {
+            city: 'Maó',
+            country: 'Spain'
+        }
+    },
+    { 
+        id: 16,
+        name: 'Francesc',
+        completedChallenges: 12,
+        address: {
+            city: 'Barcelona',
+            country: 'Catalonia'
+        }
+    },
+    { 
+        id: 17,
+        name: 'Gemma',
+        completedChallenges: 6,
+        address: {
+            city: 'Barcelona',
+            country: 'Catalonia'
+        }
+    },
+    { 
+        id: 18,
+        name: 'Gabri',
+        completedChallenges: 7,
+        address: {
+            city: 'Barcelona',
+            country: 'Spain'
+        }
+    },
+    { 
+        id: 19,
+        name: 'Anna',
+        completedChallenges: 8,
+        address: {
+            city: 'Barcelona',
+            country: 'Spain'
+        }
+    },
+    { 
+        id: 20,
+        name: 'Santi',
+        completedChallenges: 4,
+        address: {
+            city: 'La Plata',
+            country: 'Argentina'
+        }
+    },
 ]
