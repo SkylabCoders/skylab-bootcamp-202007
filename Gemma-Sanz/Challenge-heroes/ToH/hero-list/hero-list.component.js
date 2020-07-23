8d4752da65efe472c177bc799bc4e04fab063d03
gemmas95 <gemma1030@gmail.com>
2020-07-23 20:56:20 +0200

    added link onclik

--- Gemma-Sanz/Challenge-heroes/ToH/hero-list/hero-list.component.js
+++ Gemma-Sanz/Challenge-heroes/ToH/hero-list/hero-list.component.js
@@ -1,11 +1,34 @@
-let mainContainerList = document.querySelector("#mainContainer--list");
-
 function HeroListComponent() {
-    for (let i = 0; i < heroList.length; i++) {
-        let itemsList = document.createElement("button");
-        mainContainerList.appendChild(itemsList);
-        itemsList.innerHTML = "<span class=id>" + heroList[i].id + "</span>" + "<span class=name>" + heroList[i].name + "</span>";
-    };
+    let mainContainerList = document.querySelector("#mainContainer--list");
+    let anchorContainer = [];
+    let createButtonsList = function () {
+        for (let i = 0; i < heroList.length; i++) {
+            let itemsList = document.createElement("button");
+            //itemsList.href = `../hero-detail/hero-detail.component.html?heroId=${heroList[i].id}`;
+            itemsList.innerHTML = "<span class=id>" + heroList[i].id + "</span>" + "<span class=name>" + heroList[i].name + "</span>";
+            itemsList.addEventListener("click", () => {
+                window.location.assign(`../hero-detail/hero-detail.component.html?heroId=${heroList[i].id}`);
+            })
+            mainContainerList.appendChild(itemsList);
+            anchorContainer.push(itemsList);
+        };
+    }
 
-}
-HeroListComponent();
+
+    /*     let sercherList = function (anchorContainer) {
+            debugger
+            for (let i = 0; i < anchorContainer.length; i++) {
+                let anchorListContainer = document.createElement("a");
+                anchorListContainer.href = gethref(heroList[i].id);
+                anchorContainer[i].appendChild(anchorListContainer);
+    
+            }
+        } */
+    /* 
+        function gethref(id) {
+            return `../hero-detail/hero-detail.component.html?heroId=${id}`
+        }*/
+    createButtonsList()
+/*     sercherList(anchorContainer)
+ */}
+let listComponent = new HeroListComponent();
