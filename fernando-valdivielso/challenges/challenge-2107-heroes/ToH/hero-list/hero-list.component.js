db5c1e4b6934c29b1afed25d6429466ec97dc75c
Fernando Valdivielso <fvaldivielso@outlook.com>
2020-07-23 19:32:52 +0200

    refactoring

--- fernando-valdivielso/challenges/challenge-2107-heroes/ToH/hero-list/hero-list.component.js
+++ fernando-valdivielso/challenges/challenge-2107-heroes/ToH/hero-list/hero-list.component.js
@@ -2,6 +2,9 @@ function ListComponent() {
     const myHeroList = heroList;
     const ulElement = document.getElementById('list');
     let listElement = null;
+    let transformedElement = null;
+    let idElement = null;
+    let nameElement = null;
     const elementsToHtml = [];
 
     this.onInit = function () {
@@ -11,10 +14,9 @@ function ListComponent() {
 
     function listItemsToAnchor(heroItems) {
         listElement = document.createElement('li')
-
-        let transformedElement = document.createElement('a');
-        let idElement = document.createElement('span');
-        let nameElement = document.createElement('span');
+        transformedElement = document.createElement('a');
+        idElement = document.createElement('span');
+        nameElement = document.createElement('span');
         transformedElement.href = '../hero-detail/hero-detail.component.html';
         idElement.innerText = heroItems.id;
         nameElement.innerText = heroItems.name;
@@ -23,43 +25,32 @@ function ListComponent() {
         listElement.appendChild(transformedElement);
         console.log(listElement);
         elementsToHtml.push(listElement);
-        console.log(elementsToHtml);
-    
-
     }
 
     function addAnchorToHTML() {
-        // debugger;
         for (let i = 0; i < myHeroList.length; i++) {
             ulElement.appendChild(elementsToHtml[i]);
-
         }
-        console.log(ulElement);
-
-
     }
-
-
-
-
-
-
 }
-
-
 
 const listComponent = new ListComponent();
 listComponent.onInit();
+    
 
 
 
-// const listElement = document.getElementById('list');
-// this.heroes = heroList;
+        
 
-// this.createList = function () {
-    //     for (let i = 0; i < this.heroes.length; i++) {
-        //         const listNodeElement = document.createElement('li');
-        //         listElement.appendChild(listNode);
-        //     }
-        // } 
-        // listComponent.createList();
+
+
+
+
+
+
+
+
+
+
+
+
