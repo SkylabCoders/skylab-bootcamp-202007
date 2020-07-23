af4d911c85e4e12368595e9e137af8a9e03c9967
Dani Alcala <daniamcode@gmail.com>
2020-07-23 17:01:33 +0200

    minor changes

---
+++ Dani-Alcala/Dani-Alcala-Course/JS/challenges/Skylab-Dani-Alcala/skylab.mock.js
@@ -0,0 +1,10 @@
+const skylabers = [
+    { id: 10, name: Fernando},
+    { id: 11, name: Vanesa},
+    { id: 12, name: Gerard},
+    { id: 13, name: Jordi},
+    { id: 14, name: Aleix},
+    { id: 15, name: Alex},
+    { id: 16, name: Gabri},
+    { id: 17, name: Esther},
+]
