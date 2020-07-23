faa1366b9f978f11db45889cb81bf8d0888c9d31
gemmas95 <gemma1030@gmail.com>
2020-07-23 19:10:42 +0200

    files linked

--- Gemma-Sanz/Week3/Challenge-Skylabers/skylabers.js
+++ Gemma-Sanz/Week3/Challenge-Skylabers/skylabers.js
@@ -1,4 +1,8 @@
-function skylabers() {
-    this.name: name,
-        this.is: id
+function Skylabers(id, name, completedChallenges, city, country) {
+    this.id = id;
+    this.name = name;
+    this.completedChallenges = completedChallenges;
+    this.address = { city: city, country: country }
 }
+
+console.log(Skylabers[0])
