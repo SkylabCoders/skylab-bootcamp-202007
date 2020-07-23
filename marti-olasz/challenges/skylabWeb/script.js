230041da8f14db92d669a608706aef0574c500e8
Marti Olasz <martiolasz@gmail.com>
2020-07-23 20:57:12 +0200

    All features

---
+++ marti-olasz/challenges/skylabWeb/script.js
@@ -0,0 +1,31 @@
+let id = 0;
+function skylaber(name, city, country) {
+	this.id = id;
+	this.name = name;
+	this.completedChallenges = Math.round(Math.random() * 20);
+	this.address = { city: 'BCN', country: 'Spain' };
+	id++;
+}
+
+const skylaberList = [
+	new skylaber('Aleix'),
+	new skylaber('Dani Alo'),
+	new skylaber('Esther'),
+	new skylaber('Fernando'),
+	new skylaber('Gemma'),
+	new skylaber('Martí A'),
+	new skylaber('Martí O'),
+	new skylaber('Santiago'),
+	new skylaber('Vanesa'),
+	new skylaber('Alex'),
+	new skylaber('Anna'),
+	new skylaber('Dani Alc'),
+	new skylaber('Francesc'),
+	new skylaber('Gabriel'),
+	new skylaber('Gerard'),
+	new skylaber('Jordi'),
+	new skylaber('Pablo'),
+	new skylaber('Ramon'),
+	new skylaber('Simón'),
+	new skylaber('Victor')
+];
