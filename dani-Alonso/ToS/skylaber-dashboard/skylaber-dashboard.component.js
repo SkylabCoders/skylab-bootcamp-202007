63fee1326d0de6fab9db1047784ffe049ac26c3e
Dani Alonso <danialonso85@hotmail.es>
2020-07-23 19:47:36 +0200

    add skylabers projecc html

---
+++ dani-Alonso/ToS/skylaber-dashboard/skylaber-dashboard.component.js
@@ -0,0 +1,10 @@
+function SkylaberDashboardComponent() {
+    const skylabers = skylaberList.slice(0, 4);
+    const container = document.querySelector(".skylaber-dashboard__container");
+
+
+
+
+}
+
+let dashboardComponent = new SkylaberDashboardComponent();
---
+++ dani-Alonso/ToS/skylaber.js
@@ -0,0 +1,12 @@
+function Country(city, street) {
+    this.city = city;
+    this.street = street;
+}
+
+
+function Skylaber(id, name, completedChallenges, country) {
+    this.id = id;
+    this.name = name;
+    this.completedChallenges = completedChallenges;
+    this.country = country;
+}
