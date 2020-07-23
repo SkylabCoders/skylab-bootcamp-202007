857faba247beb98657c255dbdfd318a8ff469968
Vanesa Gómez <v.cucchiararo@gmail.com>
2020-07-23 17:52:23 +0200

    implemented transform list array to html

--- vanesa-gomez/challenge-ToHVanilla/ToH/hero-list/hero-list.component.js
+++ vanesa-gomez/challenge-ToHVanilla/ToH/hero-list/hero-list.component.js
@@ -1,5 +1,29 @@
 function ListComponent() {
-	this.onInit = function () {};
+	const myHeroList = heroList;
+
+	this.onInit = function () {
+		myHeroList.forEach(listItemsToAnchor);
+		addAnchorToHtml(listItemsToAnchor);
+	};
+
+	function listItemsToAnchor(heroItems) {
+		let transformedElement = document.createElement('a');
+		transformedElement.href = '../hero-detail/hero-detail.component.html';
+		let idElement = document.createElement('span');
+		let nameElement = document.createElement('span');
+
+		idElement.innerText = heroItems.id;
+		transformedElement.appendChild(idElement);
+
+		nameElement.innerText = heroItems.name;
+		transformedElement.appendChild(nameElement);
+
+		// transformedElement.innerText =
+		// 	'id: ' + heroItems.id + '    ' + heroItems.name;
+		console.log(transformedElement);
+	}
+
+	function addAnchorToHtml(heroAnchor) {}
 }
 
 const listComponent = new ListComponent();
