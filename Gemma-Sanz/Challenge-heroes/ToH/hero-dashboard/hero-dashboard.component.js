471ac59a8b8980447126c848c73976cf81d02e99
gemmas95 <gemma1030@gmail.com>
2020-07-23 10:55:49 +0200

    dashboard changed

--- Gemma-Sanz/Challenge-heroes/ToH/hero-dashboard/hero-dashboard.component.js
+++ Gemma-Sanz/Challenge-heroes/ToH/hero-dashboard/hero-dashboard.component.js
@@ -1,17 +1,14 @@
 function HeroDashboardComponent() {
 	const heroes = heroList;
-	this.onInit = function () {
-		DashboardContent();
-	}
 	let mainContentDashboard = document.querySelector("#mainContainer--dashboard")
 
-	let DashboardContent = function () {
+	this.onInit = function DashboardContent() {
 		debugger
 		const maxVisibleItems = heroes.slice(0, 4);
 		for (let i = 0; i < maxVisibleItems.length; i++) {
 			let itemDashboard = document.createElement("button");
 			mainContentDashboard.appendChild(itemDashboard);
-			itemDashboard.innerHTML = heroList[i].name;
+			itemDashboard.innerHTML = maxVisibleItems[i].name;
 		};
 	}
 	DashboardContent()
