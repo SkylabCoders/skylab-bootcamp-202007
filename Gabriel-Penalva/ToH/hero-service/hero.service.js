f044ddf18573b569c1ef95f201a5ddc9061a5d91
Gabriel Penalva <gabripenalva42@gmail.com>
2020-07-24 17:31:14 +0200

    create service and json tests too

---
+++ Gabriel-Penalva/ToH/hero-service/hero.service.js
@@ -0,0 +1,15 @@
+function HeroService() {
+
+    this.getHeroList = function () {
+        return heroList;
+    };
+    this.getHeroByName = function (name) {
+        return heroList.find((hero) => hero.name === name);
+    };
+    this.getHeroById = function (id) {
+        return heroList.find((hero) => hero.id === id)
+    };
+
+}
+
+const heroService = new HeroService();
