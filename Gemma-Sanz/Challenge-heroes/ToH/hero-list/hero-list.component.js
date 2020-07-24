dfeff5620f671641cb0a4ec5f1bd3e9ccc5af68d
gemmas95 <gemma1030@gmail.com>
2020-07-24 16:40:29 +0200

    changes done hero components filter

--- Gemma-Sanz/Challenge-heroes/ToH/hero-list/hero-list.component.js
+++ Gemma-Sanz/Challenge-heroes/ToH/hero-list/hero-list.component.js
@@ -1,4 +1,8 @@
 function HeroListComponent() {
+    this.onInit = function () {
+        createButtonsList();
+    }
+
     let mainContainerList = document.querySelector("#mainContainer--list");
     let createButtonsList = function () {
         for (let i = 0; i < heroList.length; i++) {
@@ -9,22 +13,30 @@ function HeroListComponent() {
             })
             mainContainerList.appendChild(itemsList);
         };
+
     }
+    this.searchInput = function () {
+        debugger
+/*         event.defaultPrevented();
+ */        const listFilter = function () {
+            let filteredItems = heroList;
+            filteredItems.reduce(callback, []);
+            //!!!!
+        }
+        listFilter()
+        return heroList
+    };
 
-
-    /*     let sercherList = function (anchorContainer) {
-            for (let i = 0; i < anchorContainer.length; i++) {
-                let anchorListContainer = document.createElement("a");
-                anchorListContainer.href = gethref(heroList[i].id);
-                anchorContainer[i].appendChild(anchorListContainer);
-    
-            }
-        } */
-    /* 
-        function gethref(id) {
-            return `../hero-detail/hero-detail.component.html?heroId=${id}`
-        }*/
-    createButtonsList()
-/*     sercherList(anchorContainer)
- */}
+    function callback(acumulator, hero) {
+        if (hero.name === "Narco") {
+            acumulator = [...acumulator, hero.name]
+        }
+        return acumulator
+    }
+    /*     const countHeroes = heroList.reduce(callback, 0);
+     */
+}
 let listComponent = new HeroListComponent();
+listComponent.onInit();
+
+
