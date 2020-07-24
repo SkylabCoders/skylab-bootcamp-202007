922af569cab472c6b93021a34c67e662fb235d67
Gilberto Cao <gilbe.cao@gmail.com>
2020-07-24 17:31:00 +0200

    create mocked service

---
+++ gilbe-cao/ToH/hero.service.js
@@ -0,0 +1,11 @@
+function HeroService() {
+	this.getHeroList = function () {
+		return heroList;
+	};
+	this.getHeroById = function (id) {
+		return heroList.find((hero) => hero.id === id);
+	};
+	this.getHeroByName = function (name) {
+		return heroList.find((hero) => hero.name === name);
+	};
+}
