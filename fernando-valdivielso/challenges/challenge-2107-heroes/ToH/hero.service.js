5cb99030639753fb9fa138a6dffff2d2420d4a0f
Fernando Valdivielso <fvaldivielso@outlook.com>
2020-07-24 17:46:32 +0200

    hero service

--- fernando-valdivielso/challenges/challenge-2107-heroes/ToH/hero.service.js
+++ fernando-valdivielso/challenges/challenge-2107-heroes/ToH/hero.service.js
@@ -1,15 +1,23 @@
 function HeroService() {
 
     this.getHeroList = function () {
-        return heroList;
+        fetch('hero.json').then(response => {
+            console.log(response);
+        });
     }
 
-    this.getHeroById = function (id) {
-        return heroList.find((hero) => hero.id === id)
+    // this.getHeroById = function (id) {
+    //     return heroList.find((hero) => hero.id === id)
 
-    };
+    // };
     
-    this.getHeroByName = function (name) {
-        return heroList.find((hero) => hero.name === name)
-    };
-}
+    // this.getHeroByName = function (name) {
+    //     return heroList.find((hero) => hero.name === name)
+    // };
+};
+
+const service = new HeroService();
+service.getHeroList();
+
+
+// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Fetching_data
