b2734363d0b8cad8eee9d884d6346a2a841bc9fb
gemmas95 <gemma1030@gmail.com>
2020-07-24 09:55:29 +0200

    deleted commited text

--- Gemma-Sanz/Challenge-heroes/ToH/hero-list/hero-list.component.js
+++ Gemma-Sanz/Challenge-heroes/ToH/hero-list/hero-list.component.js
@@ -1,22 +1,18 @@
 function HeroListComponent() {
     let mainContainerList = document.querySelector("#mainContainer--list");
-    let anchorContainer = [];
     let createButtonsList = function () {
         for (let i = 0; i < heroList.length; i++) {
             let itemsList = document.createElement("button");
-            //itemsList.href = `../hero-detail/hero-detail.component.html?heroId=${heroList[i].id}`;
             itemsList.innerHTML = "<span class=id>" + heroList[i].id + "</span>" + "<span class=name>" + heroList[i].name + "</span>";
             itemsList.addEventListener("click", () => {
                 window.location.assign(`../hero-detail/hero-detail.component.html?heroId=${heroList[i].id}`);
             })
             mainContainerList.appendChild(itemsList);
-            anchorContainer.push(itemsList);
         };
     }
 
 
     /*     let sercherList = function (anchorContainer) {
-            debugger
             for (let i = 0; i < anchorContainer.length; i++) {
                 let anchorListContainer = document.createElement("a");
                 anchorListContainer.href = gethref(heroList[i].id);
