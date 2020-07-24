6f15b4f88ab47b40405064a58bf658dda38699a8
Gerard Ramon <gerardramonp@gmail.com>
2020-07-24 13:39:36 +0200

    Start filter by criteria functionality

--- santiago-robinet/skylab-directoy/student-list/studen-list.component.js
+++ santiago-robinet/skylab-directoy/student-list/studen-list.component.js
@@ -1,31 +1,65 @@
+const inputElement = document.querySelector('.student-list__input');
+console.log(inputElement);
+console.log('cabrooon');
+
 function SkylaberListComponent() {
-  const allSkylaber = skylaberList;
-  const skylaberListContainer = document.querySelector(".list__container");
+	const allSkylaber = skylaberList;
+	const skylaberListContainer = document.querySelector('.list__container');
 
-  this.onInit = function () {
-    renderHeroList().forEach((element) => {
-      if (skylaberListContainer) {
-        skylaberListContainer.appendChild(element);
-      }
-    });
-  };
+	this.onInit = function () {
+		renderSKylaberList(allSkylaber).forEach((element) => {
+			if (skylaberListContainer) {
+				skylaberListContainer.appendChild(element);
+			}
+		});
+	};
 
-  function renderHeroList() {
-    return allSkylaber.map(mapRenderAnchor);
-  }
+	function renderSKylaberList(skylaberList) {
+		return skylaberList.map(mapRenderAnchor);
+	}
 
-  function mapRenderAnchor(skylaber) {
-    const element = document.createElement("a");
-    element.href = getSkylaberLink(skylaber.id);
-    element.innerHTML = `<span>${skylaber.id}</span><span>${skylaber.name}</span><span>${skylaber.completedChallenges}</span><span>${skylaber.address.city}</span><span>${skylaber.address.city}</span>`;
-    return element;
-  }
+	function mapRenderAnchor(skylaber) {
+		const element = document.createElement('a');
+		element.href = getSkylaberLink(skylaber.id);
+		element.innerHTML = `<span>${skylaber.id}</span><span>${skylaber.name}</span><span>${skylaber.completedChallenges}</span><span>${skylaber.address.city}</span><span>${skylaber.address.city}</span>`;
+		return element;
+	}
 
-  function getSkylaberLink(id) {
-    return `../student-details/student-detail.component.html?skylaberId=${id}`;
-  }
+	function getSkylaberLink(id) {
+		return `../student-details/student-detail.component.html?skylaberId=${id}`;
+	}
+
+	this.filterByCriteria = function () {
+		// pintar el nuevo array
+		console.log('I ENTER!!');
+		debugger;
+
+		let returnArray = [];
+		const inputElementValue = document
+			.querySelector('.student-list__input')
+			.value.toLowerCase();
+
+		if (inputElementValue === '') {
+			returnArray = [...allSkylaber];
+		} else {
+			returnArray = allSkylaber.reduce(function (counter, skylaber) {
+				console.log(counter);
+				return inputElementValue === skylaber.name.toLowerCase() ||
+					inputElementValue === skylaber.id ||
+					inputElementValue === skylaber.address.city.toLowerCase() ||
+					inputElementValue ===
+						skylaber.address.country.toLowerCase() ||
+					inputElementValue === skylaber.completedChallenges
+					? (counter = [...counter, counter])
+					: counter;
+			}, []);
+		}
+		console.log(returnArray);
+	};
 }
 
 const skylaberListComponent = new SkylaberListComponent();
 
 skylaberListComponent.onInit();
+
+skylaberListComponent.filterByCriteria();
