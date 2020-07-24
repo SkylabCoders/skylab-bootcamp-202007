f1b2518f49e7ec4022a540255394bbd200a650c5
Gerard Ramon <gerardramonp@gmail.com>
2020-07-24 17:20:52 +0200

    Add ToH files

---
+++ gerard-ramon/Diary/JavaScript/ToH/hero-detail/hero-detail.component.js
@@ -0,0 +1,40 @@
+function HeroDetailComponent() {
+	let hero;
+	const idElement = document.getElementById('hero-detail__id');
+	const nameElement = document.getElementById('hero-detail__name');
+	const nameControlElement = document.getElementById(
+		'hero-detail__name-control'
+	);
+
+	this.onInit = function () {
+		hero = getHeroFromUrl();
+		updateId();
+		updateName();
+	};
+
+	this.nameChange = function (newName) {
+		hero.name = newName;
+		updateName();
+	};
+
+	function updateId() {
+		if (idElement) idElement.innerHTML = hero.id;
+	}
+
+	function updateName() {
+		if (nameElement) nameElement.innerHTML = hero.name;
+		if (nameControlElement) nameControlElement.value = hero.name;
+	}
+
+	function getHeroFromUrl() {
+		return heroList.find(compareId);
+	}
+
+	function compareId(hero) {
+		const params = new URLSearchParams(location.search);
+		return hero.id === +params.get('heroId');
+	}
+}
+
+const heroDetailComponent = new HeroDetailComponent();
+heroDetailComponent.onInit();
