60ea40bcd4801c2dcaad0059289b33f7eec2dfd8
Jordi Muñoz <jordimm11@gmail.com>
2020-07-24 11:40:23 +0200

    constructor function modify

--- jordi-muñoz/skylab-directory/skylaber.js
+++ jordi-muñoz/skylab-directory/skylaber.js
@@ -2,5 +2,11 @@ function Skylaber(id, name, completedChallenges, country) {
 	this.id = id;
 	this.name = name;
 	this.completedChallenges = completedChallenges;
-	this.country = country;
+	this.address = PaymentAddress;
 }
+
+function Address(city, country) {
+	this.city;
+	this.country;
+}
+
