3c5c27f298b6e973ce6467eb3d31a73cd30a09d0
gemmas95 <gemma1030@gmail.com>
2020-07-24 10:52:33 +0200

    changes done

--- Gemma-Sanz/Week3/Challenge-Skylabers/skylabers.js
+++ Gemma-Sanz/Week3/Challenge-Skylabers/skylabers.js
@@ -1,6 +1,12 @@
-function Skylabers(id, name, completedChallenges, city, country) {
+"use strict"
+function Skylaber(id, name, completedChallenges, city, country) {
     this.id = id;
     this.name = name;
     this.completedChallenges = completedChallenges;
     this.address = { city: city, country: country }
+}
+
+function Address(city, country) {
+    this.city = city;
+    this.country = country;
 }
