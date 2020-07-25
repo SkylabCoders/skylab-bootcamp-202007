cba1e9dc52b0824b18465feda0fe35cff98cc39d
Jordi Muñoz <jordimm11@gmail.com>
2020-07-25 11:43:28 +0200

    Changed jordi's folder name to make scanner work

---
+++ jordi-munoz/skylab-directory/skylaber.mock.js
@@ -0,0 +1,122 @@
+const skylaberList = [
+    {
+        id: 0,
+        name: 'Jordi',
+        completedChallenges: 7,
+        address: { city: 'Barcelona', country: 'Catalunya' }
+    },
+    {
+        id: 1,
+        name: 'Alex',
+        completedChallenges: 3,
+        address: { city: 'Azpeitia', country: 'Euskal Herria' }
+    },
+    {
+        id: 2,
+        name: 'Francesc',
+        completedChallenges: 999,
+        address: { city: 'Palau', country: 'Catalunya' }
+    },
+    {
+        id: 3,
+        name: 'Esther',
+        completedChallenges: 5,
+        address: { city: 'Palma', country: 'Mallorca' }
+    },
+    {
+        id: 4,
+        name: 'Dani Alcala',
+        completedChallenges: 4,
+        address: { city: 'Sans', country: 'BCN' }
+    },
+    {
+        id: 5,
+        name: 'Dani Alonso',
+        completedChallenges: 3,
+        address: { city: 'bcn', country: 'cat' }
+    },
+    {
+        id: 6,
+        name: 'Marti 1',
+        completedChallenges: 7,
+        address: { city: 'Granu', country: 'Street' }
+    },
+    {
+        id: 7,
+        name: 'Marti 2',
+        completedChallenges: 1,
+        address: { city: 'Granu', country: 'Street' }
+    },
+    {
+        id: 8,
+        name: 'Victor',
+        completedChallenges: 2,
+        address: { city: 'Granu', country: 'Street' }
+    },
+    {
+        id: 9,
+        name: 'Gabriel',
+        completedChallenges: 1,
+        address: { city: 'Granu', country: 'Street' }
+    },
+    {
+        id: 10,
+        name: 'Gerard',
+        completedChallenges: 5,
+        address: { city: 'Granu', country: 'Street' }
+    },
+    {
+        id: 11,
+        name: 'Gemma',
+        completedChallenges: 5,
+        address: { city: 'Granu', country: 'Street' }
+    },
+    {
+        id: 12,
+        name: 'Ramon',
+        completedChallenges: 6,
+        address: { city: 'Granu', country: 'Street' }
+    },
+    {
+        id: 13,
+        name: 'Anna',
+        completedChallenges: 9,
+        address: { city: 'Granu', country: 'Street' }
+    },
+    {
+        id: 14,
+        name: 'Simon',
+        completedChallenges: 2,
+        address: { city: 'Granu', country: 'Street' }
+    },
+    {
+        id: 15,
+        name: 'Aleix',
+        completedChallenges: 3,
+        address: { city: 'Granu', country: 'Street' }
+    },
+    {
+        id: 16,
+        name: 'Vanessa',
+        completedChallenges: 1,
+        address: { city: 'Granu', country: 'Street' }
+    },
+    {
+        id: 17,
+        name: 'Fernando',
+        completedChallenges: 4,
+        address: { city: 'Granu', country: 'Street' }
+    },
+    {
+        id: 18,
+        name: 'Santi',
+        completedChallenges: 5,
+        address: { city: 'Granu', country: 'Street' }
+    },
+    {
+        id: 19,
+        name: 'Pablo',
+        completedChallenges: 6,
+        address: { city: 'Granu', country: 'Street' }
+    }
+];
