175d2afd0bfb9f6f92106f05197de5e8c5f4d141
Jordi Muñoz <jordimm11@gmail.com>
2020-07-25 15:29:23 +0200

    add name filter list

--- jordi-munoz/skylab-directory/skylab-directory-list/skylab-directory-list.component.js
+++ jordi-munoz/skylab-directory/skylab-directory-list/skylab-directory-list.component.js
@@ -2,17 +2,16 @@
 function StudentListComponent() {
     const inputFilter = document.querySelector('.inputFilter');
     let componentStudentList = skylaberList;
- 
 
-    this.createDomElements = function (acc = componentStudentList) {
-        console.log(acc);
+
+    this.createDomElements = function (filter = componentStudentList) {
         let container = document.querySelector('.container');
         container.innerHTML = '';
-        for (let i = 0; i < acc.length; i++) {
+        for (let i = 0; i < filter.length; i++) {
             let generateDiv = document.createElement('div');
             generateDiv.classList.add(`student-${i}`);
             let generateA = document.createElement('a');
-            generateA.href = `../skylab-directory-detail/skylab-directory-detail.component.html?studentId=${acc[i].id}`;
+            generateA.href = `../skylab-directory-detail/skylab-directory-detail.component.html?studentId=${filter[i].id}`;
             generateA.classList.add('anchor');
             let generateP = document.createElement('p');
             generateP.classList.add('para');
@@ -21,26 +20,29 @@ function StudentListComponent() {
             generateDiv.appendChild(generateP);
             container.appendChild(generateDiv);
 
-            printStudents(acc, i);
+            printStudents(filter, i);
         }
     }
 
-    const printStudents = function (acc, i) {
+    const printStudents = function (filter, i) {
         let tempA = document.querySelector(`.student-${i} a`);
         let tempP = document.querySelector(`.student-${i} p`);
-        tempA.innerHTML = acc[i].id;
-        tempP.innerHTML = acc[i].name;
+        tempA.innerHTML = filter[i].id;
+        tempP.innerHTML = filter[i].name;
     }
 
-    this.searchList = function() {
+    this.searchList = function () {
         let result = componentStudentList.reduce(callback, []);
         this.createDomElements(result);
     };
     function callback(acc, currentValue) {
-        if (currentValue.name === inputFilter.value) {
+        if (currentValue.name.toLowerCase() === inputFilter.value.toLowerCase()) {
             acc = [...acc, currentValue];
         }
-        return acc; 
+        if (inputFilter.value === '') {
+            acc = componentStudentList;
+        }
+        return acc;
     };
 
 }
