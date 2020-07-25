2ab3cf039f80525160ef4d16e5be775db0dd8cc6
Jordi Muñoz <jordimm11@gmail.com>
2020-07-25 11:34:08 +0200

    add change

--- jordi-muñoz/skylab-directory/skylab-directory-dashboard/skylab-directory-dashboard.component.js
+++ jordi-muñoz/skylab-directory/skylab-directory-dashboard/skylab-directory-dashboard.component.js
@@ -5,12 +5,12 @@ function StudentDashboardComponent() {
     this.onInIt = function displayDashBoard() {
         const containerList = document.querySelector('.containerList');
         checkChallenges();
-        let fourStudents = studentFilter.slice(0,4);
+        let fourStudents = studentFilter.slice(0, 4);
         containerList.innerHTML = renderStudentList(fourStudents);
     }
     function renderStudentList(student) {
         const detailsLink = '../skylab-directory-detail/skylab-directory-detail.component.html';
-        let elements = student.map(function(e) {
+        let elements = student.map(function (e) {
             return `<a href='${detailsLink}?studentId=${e.id}' >${e.name}</a>`;
         });
         return elements.join('');
