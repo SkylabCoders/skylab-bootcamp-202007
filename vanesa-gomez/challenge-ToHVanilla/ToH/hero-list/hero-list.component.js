b90a61f9d98181070b2992a6ba3cc33c76d701ea
Vanesa Gómez <v.cucchiararo@gmail.com>
2020-07-26 20:39:44 +0200

    ToH done

--- vanesa-gomez/challenge-ToHVanilla/ToH/hero-list/hero-list.component.js
+++ vanesa-gomez/challenge-ToHVanilla/ToH/hero-list/hero-list.component.js
@@ -2,39 +2,36 @@ function ListComponent() {
 	const myHeroList = heroList;
 	const ulElement = document.getElementById('list');
 	let listElement = null;
-	const elementsToHtml = [];
 	let transformedElement = null;
 	let idElement = null;
 	let nameElement = null;
-
+	const elementsToHtml = [];
 	this.onInit = function () {
 		myHeroList.forEach(listItemsToAnchor);
-		addAnchorToHtml(listItemsToAnchor);
+		addAnchorToHTML(listItemsToAnchor);
 	};
-
 	function listItemsToAnchor(heroItems) {
 		listElement = document.createElement('li');
 		transformedElement = document.createElement('a');
 		idElement = document.createElement('span');
 		nameElement = document.createElement('span');
-
 		transformedElement.href =
 			'../hero-detail/hero-detail.component.html?heroid=' + heroItems.id;
+		transformedElement.className = 'hero-list';
+		nameElement.className = 'name-list';
+		idElement.className = 'id-list';
 		idElement.innerText = heroItems.id;
-		transformedElement.appendChild(idElement);
 		nameElement.innerText = heroItems.name;
+		transformedElement.appendChild(idElement);
 		transformedElement.appendChild(nameElement);
 		listElement.appendChild(transformedElement);
 		elementsToHtml.push(listElement);
 	}
-
-	function addAnchorToHtml() {
-		for (i = 0; i < myHeroList.length; i++) {
+	function addAnchorToHTML() {
+		for (let i = 0; i < myHeroList.length; i++) {
 			ulElement.appendChild(elementsToHtml[i]);
 		}
 	}
 }
-
 const listComponent = new ListComponent();
-
 listComponent.onInit();
