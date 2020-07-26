5460fdd86690737f45394650ab5ee9d2c8833052
Alex Larrañaga <alexurola@gmail.com>
2020-07-26 11:58:16 +0200

    Find. Clean Code

--- alex-larranaga/week2/bowl/bowl.js
+++ alex-larranaga/week2/bowl/bowl.js
@@ -29,12 +29,31 @@ function Bowl() {
 		}
 		return filterArray;
 	};
+
+	this.findCallback = function (x) {
+		if (x < 5) {
+			return x;
+		}
+	};
+
+	this.find = function (arr, callback) {
+		let result;
+		for (let el of arr) {
+			if (callback(el)) {
+				return el;
+			}
+		}
+	};
 }
 
-let myArray = [6, 2, 3, 4, 5, 1, 7, 8, 9, 10];
+let myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 let myBowl = new Bowl();
 
 //MAP
+
 //console.log("Map Method:" myBowl.map(myArray, myBowl.mapCallback)); // [2, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-console.log('Filter Method:' + myBowl.filter(myArray, myBowl.filterCallback));
+//FILTER
+//console.log('Filter Method:' + myBowl.filter(myArray, myBowl.filterCallback)); // [6,7,8,9,10]
+//FIND
+console.log('Find method: ' + myBowl.find(myArray, myBowl.findCallback));
