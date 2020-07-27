18ba9260752bb3ec8a535a14d29622030bdd3716
Gilberto Cao <gilbe.cao@gmail.com>
2020-07-27 17:02:49 +0200

    add hero service

--- gilbe-cao/ToH/services/hero.service.spec.js
+++ gilbe-cao/ToH/services/hero.service.spec.js
@@ -1,28 +1,27 @@
 describe('HeroService', function () {
 	let heroService;
+	let hero;
 
 	beforeEach(function () {
 		heroService = new HeroService();
+		hero = { id: 18, name: 'Dr IQ' };
 	});
 
-	it('should create a hero service object', function () {
+	it('should create a HeroService', function () {
 		expect(heroService).toBeTruthy();
 	});
 
 	it('should get hero list', function () {
-		expect(heroService.getHeroList()).toBeDefined();
-		expect(heroService.getHeroList()).toEqual(heroList);
+		expect(heroService.getHeroList()).toBe(heroList);
 	});
 
 	it('should get one hero by id', function () {
-		const id = 14;
-		const hero = heroList.find((hero) => hero.id === id);
+		const id = 18;
 		expect(heroService.getHeroById(id)).toEqual(hero);
 	});
 
 	it('should get one hero by name', function () {
 		const name = 'Dr IQ';
-		const hero = heroList.find((hero) => hero.name === name);
 		expect(heroService.getHeroByName(name)).toEqual(hero);
 	});
 });
