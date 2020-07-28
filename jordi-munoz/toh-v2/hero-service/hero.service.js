29f95f92fedad4762923897a54bcd956d37910c0
Jordi Muñoz <jordimm11@gmail.com>
2020-07-28 10:37:56 +0200

    add promises

--- jordi-munoz/toh-v2/hero-service/hero.service.js
+++ jordi-munoz/toh-v2/hero-service/hero.service.js
@@ -5,7 +5,15 @@ class HeroService {
     }
 
     getHeroById(id) {
-        return heroList.find((hero) => hero.id === id);
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                const response = heroList.find((hero) => hero.id === id);
+                response
+                    ? resolve(response)
+                    : reject('There is no hero with id: ' + id);
+            }, 2000);
+        });
+        //return heroList.find((hero) => hero.id === id);
     }
 
     getHeroByName(name) {
