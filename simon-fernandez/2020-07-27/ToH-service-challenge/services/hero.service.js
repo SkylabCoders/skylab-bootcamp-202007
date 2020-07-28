ebc821bf5dc55f7c60ee80308a7572e51218efc7
simonbesteiro <simonfernandezbesteiro@MacBook-Pro-de-Simon.local>
2020-07-28 16:26:49 +0200

    add fetch to the get hero by id service

--- simon-fernandez/2020-07-27/ToH-service-challenge/services/hero.service.js
+++ simon-fernandez/2020-07-27/ToH-service-challenge/services/hero.service.js
@@ -11,12 +11,25 @@ class HeroService {
 		});*/
 	}
 	getHeroById(id) {
-		return new Promise((resolve, reject) => {
+		return fetch('../hero-list.json').then((response) => {
+			return response
+				.json()
+				.then((response) => {
+					console.log(response);
+					const hero = response.find((element) => element.id === id);
+					if (!hero) throw 'No hay hero';
+					return hero;
+				})
+				.catch((error) => {
+					return console.log('un error', error);
+				});
+		});
+		/*return new Promise((resolve, reject) => {
 			setTimeout(() => {
 				const response = resolve(heroList.find((hero) => hero.id === id));
 				response ? resolve(response) : reject('There is no hro with id: ' + id);
 			}, 2000);
-		});
+		});*/
 	}
 	getHeroByName(name) {
 		return heroList.find((hero) => hero.name === name);
