96782ca637c42876f5777a8e98ecf60c16dcf462
simonbesteiro <simonfernandezbesteiro@MacBook-Pro-de-Simon.local>
2020-07-29 10:32:41 +0200

    create click counter app

---
+++ simon-fernandez/2020-07-29/click-counter/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App(props) {
+	const clicks = 0;
+	return (
+		<div id="click-counter">This div has been clicked {clicks} times!</div>
+	);
+}
+
+export default App;
