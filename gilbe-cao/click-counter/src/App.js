8501e6c101022623ae94864c9366d347fd8d8a5c
Gilberto Cao <gilbe.cao@gmail.com>
2020-07-29 12:49:28 +0200

    change function component to class component

--- gilbe-cao/click-counter/src/App.js
+++ gilbe-cao/click-counter/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 
-function App({ clicks, clickChange, resetCounter }) {
-	return (
-		<div onClick={clickChange} onMouseLeave={resetCounter}>
-			This div has been clicked {clicks} times!
-		</div>
-	);
+class App extends React.Component {
+	render() {
+		return (
+			<div
+				onClick={this.props.clickChange}
+				onMouseLeave={this.props.resetCounter}
+			>
+				This div has been clicked {this.props.clicks} times!
+			</div>
+		);
+	}
 }
 
 export default App;
