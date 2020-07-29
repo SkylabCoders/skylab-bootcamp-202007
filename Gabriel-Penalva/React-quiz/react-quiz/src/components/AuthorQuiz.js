cbc10bef470f83f833c86788c17b62c270ee512b
gaeremtro <gabripenalva42@gmail.com>
2020-07-29 16:49:33 +0200

    components Test,Book

---
+++ Gabriel-Penalva/React-quiz/react-quiz/src/components/AuthorQuiz.js
@@ -0,0 +1,18 @@
+import React from 'react';
+//import './AuthorQuiz.css';
+import Turn from '../Turn';
+
+
+function AuthorQuiz({ }) {
+  return (
+    <div >
+      <div className="jumbotron col-10 offset-1">
+        <h1>AuthorQuiz!</h1>
+      </div>
+      <Turn />
+    </div>
+  );
+}
+
+
+export default AuthorQuiz;
--- Gabriel-Penalva/React-quiz/react-quiz/src/index.js
+++ Gabriel-Penalva/React-quiz/react-quiz/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './bootstrap.min.css';
+import AuthorQuiz from './components/AuthorQuiz';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthorQuiz />
   </React.StrictMode>,
   document.getElementById('root')
 );
