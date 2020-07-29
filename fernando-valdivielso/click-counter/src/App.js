d9f02b74272befafc950775ef485242c22d4aa35
Fernando <fvaldivielso@outlook.com>
2020-07-29 15:10:51 +0200

    final commit

--- fernando-valdivielso/click-counter/src/App.js
+++ fernando-valdivielso/click-counter/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App({ clicks, clickChange, leaveArea }) { // props = { clicks }
+function App({ clicks, clickChange, leaveArea }) {
   return (
     <div onClick={clickChange} onMouseOut={leaveArea}>
       This div has been clicked {clicks} times!
--- fernando-valdivielso/click-counter/src/index.js
+++ fernando-valdivielso/click-counter/src/index.js
@@ -6,23 +6,23 @@ import * as serviceWorker from './serviceWorker';
 
 let counter = 0;
 function updateCounter(newValue) {
-	counter = newValue || 0; //si newValue es null/undefined (que equivale a false), counter vale 0
-	render(); //funcion que tenemos que crear para que nos pinte el html (creada en linea 15)
+  counter = newValue || 0; //si newValue es null/undefined, counter vale 0
+  render(); //funcion que tenemos que crear para que nos pinte el html (creada en linea 15)
 }
 
 
 
 function render() {  //esta funcion envuelve el ReactDOM.render(), que pinta el html
-	ReactDOM.render(
-		<React.StrictMode>
-      <App 
-      clicks={counter} 
-      clickChange={() => updateCounter(++counter)}
-      leaveArea = {() => updateCounter()}  //funcion que llama a updateCounter 
+  ReactDOM.render(
+    <React.StrictMode>
+      <App
+        clicks={counter}
+        clickChange={() => updateCounter(++counter)}
+        leaveArea={() => updateCounter()}  //funcion que llama a updateCounter 
       />
-		</React.StrictMode>,
-		document.getElementById('root')
-	);
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
 }
 
 render();
