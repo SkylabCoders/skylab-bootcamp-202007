46ffdbc73da4ff99c02770ec719b3fdf2e31a88e
Ramon Saura <ramon.saura@gmail.com>
2020-07-29 17:30:50 +0200

    create react demo

---
+++ ramon-saura/react-demo/src/SayHi.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function SayHi({ now }) {
+	return <h1>Hello at {now}</h1>;
+}
+
+export default SayHi;
---
+++ ramon-saura/react-demo/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import SayHi from './SayHi';
+import * as serviceWorker from './serviceWorker';
+
+setInterval(() => {
+	ReactDOM.render(
+		<React.StrictMode>
+			<SayHi now={new Date().toISOString()} />
+		</React.StrictMode>,
+		document.getElementById('root')
+	);
+}, 1000);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
