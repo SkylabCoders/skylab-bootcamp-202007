38b83b1c65efc4a164ab37e12c772c3788949e70
gaeremtro <gabripenalva42@gmail.com>
2020-07-29 15:50:52 +0200

    appdate

--- Gabriel-Penalva/React-lecture/react-demo/src/index.js
+++ Gabriel-Penalva/React-lecture/react-demo/src/index.js
@@ -2,13 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+import Hello from './Hello';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <h1>Hello at now {new Date().toString()}</h1>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+setInterval(() => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Hello
+        now={new Date().toISOString()} />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}, 1000);
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
