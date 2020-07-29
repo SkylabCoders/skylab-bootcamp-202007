677cf107750387ef8ea8bdc9f44c8ba5a77a9237
Dani Alonso <danialonso85@hotmail.es>
2020-07-29 12:23:42 +0200

    add counter project

---
+++ dani-Alonso/click-counter/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App({ clicks, clickChange, resetCounter }) {
+    return ( < div onClick = { clickChange }
+        onMouseLeave = { resetCounter } > This div has been clicked { clicks }
+        times! < /div>);
+    }
+
+    export default App;
---
+++ dani-Alonso/click-counter/src/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+let counter = 0;
+
+
+
+function updateCounter(newValue) {
+    counter = newValue || 0;
+    render();
+}
+
+function render() {
+
+    ReactDOM.render( < React.StrictMode >
+        <
+        App clicks = { counter }
+        clickChange = {
+            () => updateCounter(++counter)
+        }
+        resetCounter = {
+            () => updateCounter()
+        }
+        /> < /
+        React.StrictMode > ,
+        document.getElementById('root')
+    );
+}
+render();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
