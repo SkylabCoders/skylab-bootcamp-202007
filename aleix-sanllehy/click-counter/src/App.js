ed9c25b984618bc63313f8dafa89e04dda2df059
alishpls <aleixsaar@gmail.com>
2020-07-29 15:12:53 +0200

    Add class

--- aleix-sanllehy/click-counter/src/App.js
+++ aleix-sanllehy/click-counter/src/App.js
@@ -2,6 +2,21 @@ import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
 
+//class
+/* class App extends React.Component {
+	render() {
+		return (
+			<div
+				onClick={this.props.clickChange}
+				onPointerLeave={this.props.resetCounter}
+			>
+				This div has been clicked {this.props.clicks} times!
+			</div>
+		);
+	}
+} */
+
+//function
 function App({ clicks, clickChange, resetCounter }) {
 	// No const but props, so props added as argument in function App
 	//const clicks = 0;
@@ -13,24 +28,3 @@ function App({ clicks, clickChange, resetCounter }) {
 }
 
 export default App;
-
-/* function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
-	);
-} */
