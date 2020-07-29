733786d89f5f51d9cab3304765608deb85833259
Dani Alcala <daniamcode@gmail.com>
2020-07-29 14:46:20 +0200

    small changes

--- Dani-Alcala/click-counter/src/App.js
+++ Dani-Alcala/click-counter/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App({ clicks, clickChange, clickReset }) {
   return (
     <div onClick={clickChange} onMouseLeave={clickReset}>This div has been clicked {clicks} times</div>
@@ -9,3 +11,15 @@ function App({ clicks, clickChange, clickReset }) {
 }
 
 export default App;
+
+
+
+// class App extends React.Component {
+// 	render() {
+// 		return <div onClick={this.props.clickChange} onMouseLeave={this.props.clickReset}>
+// 			This div has been clicked {this.props.clicks} times
+// 		</div>;
+// 	}
+// }
+
+// export default App;
