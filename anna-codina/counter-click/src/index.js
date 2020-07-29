1bd615b7a279a94bd200c7a44707ea20c1772b8e
Anna Codina <anna.co91@gmail.com>
2020-07-29 12:24:17 +0200

    Create click-counter app

--- anna-codina/counter-click/src/index.js
+++ anna-codina/counter-click/src/index.js
@@ -3,13 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { render } from '@testing-library/react';
+let counter = 0;
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function updateCounter(newValue) {
+  counter = newValue || 0;
+  renderAgain();
+}
+
+function renderAgain() {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App
+        clicks={counter}
+        clickChange={() => updateCounter(++counter)}
+        resetCounter={() => updateCounter()}
+      />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+renderAgain();
+renderAgain();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
