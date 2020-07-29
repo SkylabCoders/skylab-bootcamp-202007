96d3494c593187a4f38ca5d4725c6de370a61b02
simonbesteiro <simonfernandezbesteiro@MacBook-Pro-de-Simon.local>
2020-07-29 16:47:20 +0200

    create turn.css and apply bootstrat classes to Turn.js

--- simon-fernandez/2020-07-29/author-quiz/src/Turn.js
+++ simon-fernandez/2020-07-29/author-quiz/src/Turn.js
@@ -1,16 +1,21 @@
 import React from 'react';
-
 import Book from './Book';
-
+import './Turn.css';
 function Turn() {
 	return (
-		<div>
-			<img
-				src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/800px-React-icon.svg.png"
-				alt="react logo"
-			></img>
-			<Book title={'The Infinite Game'} />
-			<Book title={'El Quijote'} />
+		<div className="row">
+			<div className="col-4 offset-1">
+				<img
+					className="author-image"
+					src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/800px-React-icon.svg.png"
+					alt="react logo"
+				></img>
+			</div>
+
+			<div className="col-6">
+				<Book title={'The Infinite Game'} />
+				<Book title={'El Quijote'} />
+			</div>
 		</div>
 	);
 }
