2d258c50ca8b284a1799b935ce7e8945e2e3cb91
Anna Codina <anna.co91@gmail.com>
2020-07-29 16:52:48 +0200

    Create components

--- anna-codina/author-quiz/src/index.js
+++ anna-codina/author-quiz/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './bootstrap.min.css';
 import * as serviceWorker from './serviceWorker';
+import AuthorQuiz from './components/AuthorQuiz';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<AuthorQuiz />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
