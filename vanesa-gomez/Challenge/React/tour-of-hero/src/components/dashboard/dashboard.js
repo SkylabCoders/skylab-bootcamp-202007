10e77fdd4f7683d5b39ec53473f3652157d358d2
Vanesa Gómez <v.cucchiararo@gmail.com>
2020-07-30 09:53:22 +0200

    ..

--- vanesa-gomez/Challenge/React/tour-of-hero/src/components/dashboard/dashboard.js
+++ vanesa-gomez/Challenge/React/tour-of-hero/src/components/dashboard/dashboard.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './dashboard.css';
 
 function Dashboard({ heroes }) {
-	const promotedHeroList = heroes.slice((0, 4) => {});
+	const promotedHeroList = heroes.slice((0, 4) => {
+		<li></li>;
+	});
 	return <div>promotedHeroList</div>;
 }
 
