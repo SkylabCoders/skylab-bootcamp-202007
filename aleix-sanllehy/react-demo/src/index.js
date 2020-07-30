be226b3e340e73b99802654d77126025f7ededb7
alishpls <aleixsaar@gmail.com>
2020-07-30 12:04:11 +0200

    Add typescript / js

--- aleix-sanllehy/react-demo/src/index.js
+++ aleix-sanllehy/react-demo/src/index.js
@@ -3,12 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
 import Hello from './Hello';
+import Sum from './Sum';
 import * as serviceWorker from './serviceWorker';
+
+const myAttributes = { a: 6, b: 7 };
+
+const { a } = myAttributes;
 
 //setInterval(() => {
 ReactDOM.render(
 	<React.StrictMode>
 		<Hello /*now={new Date().toISOString()}*/ />
+		<Sum a={'key'} b={2} />
+		{React.createElement(Sum, { a: 4, b: 2 })}
+		{React.createElement(
+			'h1',
+			null,
+			React.createElement(Sum, { a: 4, b: 7 }, null)
+		)}
+		<Sum {...myAttributes} />
 	</React.StrictMode>,
 	document.getElementById('root')
 );
