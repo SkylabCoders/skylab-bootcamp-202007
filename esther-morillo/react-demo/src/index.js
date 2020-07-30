0aac893db7714a2709aeafc0b0267d187c951a40
Esther Morillo <esther.morillo@gmail.com>
2020-07-30 11:43:12 +0200

    add prop-types

--- esther-morillo/react-demo/src/index.js
+++ esther-morillo/react-demo/src/index.js
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import Hello from './Hello';
+
+import Sum from './Sum'
+
 
 
 
   ReactDOM.render(
     <React.StrictMode>
-      <Hello />
+      
+      <Sum 
+        a={4} 
+        b={2}/>
     </React.StrictMode>,
     document.getElementById('root')
   );
