4b72de7bd61559d2684e28defbabaca0618a1c5e
Marti Olasz <martiolasz@gmail.com>
2020-07-30 16:10:04 +0200

    Add input functionalities in details

--- marti-olasz/challenges/heroes/toh/src/component/Details.js
+++ marti-olasz/challenges/heroes/toh/src/component/Details.js
@@ -1,30 +1,36 @@
 import React from 'react';
 
-class HeroDetails extends React.Component{
-	constructor(props){
-		super(props)
+class Details extends React.Component {
+	constructor(props) {
+		super(props);
 		this.state = {
-			hero = {
-				name: '',
-				id: null
-			}
-		}
+			name: props.hero.name,
+			id: props.hero.id
+		};
+		this.onFieldChange = this.onFieldChange.bind(this);
 	}
-}
+	onFieldChange(event) {
+		this.setState({
+			name: event.target.value
+		});
+	}
 
-function Details({ hero }) {
-	return (
-		<div>
-			<h1>Details</h1>
+	render() {
+		return (
 			<form>
-				<p>{hero.name} details!</p>
-				<p>id: {hero.id}</p>
-				<lable htmlFor="heroName">
-					name: <input value={hero.name} name="heroname"></input>
-				</lable>
+				<p>{this.state.name} details!</p>
+				<p>id: {this.state.id}</p>
+				<label htmlFor="heroName">
+					name:{' '}
+					<input
+						value={this.state.name}
+						name="heroname"
+						onChange={this.onFieldChange}
+					></input>
+				</label>
 			</form>
-		</div>
-	);
+		);
+	}
 }
 
 export default Details;
