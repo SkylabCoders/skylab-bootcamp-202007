9875c0d8c01dff8764879c589f8597ced87f0d9e
Vanesa Gómez <v.cucchiararo@gmail.com>
2020-07-30 10:33:48 +0200

    state and lifecycle

--- vanesa-gomez/Lecture/React/react-demo/src/Hello.js
+++ vanesa-gomez/Lecture/React/react-demo/src/Hello.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
-function Hello({ now }) {
-	return <h1>Hello at {now}</h1>;
+class Hello extends React.Component {
+	interval = null;
+	constructor(props) {
+		super(props);
+		this.state = {
+			now: new Date().toISOString()
+		};
+	}
+
+	componentDidMount() {
+		this.interval = setInterval(() => {
+			this.setState({
+				now: new Date().toISOString()
+			});
+		}, 1000);
+	}
+	componentWillUnmount() {
+		clearInterval(this.interval);
+	}
+
+	render() {
+		return <h1>Hello at {this.state.now}</h1>;
+	}
 }
 
 export default Hello;
