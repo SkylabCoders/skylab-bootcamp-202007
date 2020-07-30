7c04e1c7ab213223deec22297397fa14659ec4e8
gemmas95 <gemma1030@gmail.com>
2020-07-30 15:04:12 +0200

    my button done

--- Gemma-Sanz/react-demo/src/index.js
+++ Gemma-Sanz/react-demo/src/index.js
@@ -2,12 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+import Hello from './Hello';
 import Sum from './Sum';
+import Clicker from './Clicker';
+import MyButton from './MyButton'
+import ConditionalDisplay from './ConditionalDisplay';
 
+const myAttribute = {
+  a: 4,
+  b: 2
+}
+const myLastName = ["G", "E", "M", "A"];
 ReactDOM.render(
   <React.StrictMode>
-    <Sum a={4} b={2} />
-  </React.StrictMode>,
+    <Hello />
+    <Clicker
+      MyButton={MyButton}
+      handleClick={(letter) => console.log(letter)}
+      data={myLastName} />
+    <ConditionalDisplay isVisible={true}>
+      {/* react agafa h1 y p y es com el children de props de ConditionalDisplay. Al momento de la reconciliación els afaga com a children.
+      Props.children son els elements que anida quan isVisible es true*/}
+      <h1>Hello</h1>
+      <p>world!</p>
+    </ConditionalDisplay>
+
+  </React.StrictMode >,
   document.getElementById('root')
 );
 
