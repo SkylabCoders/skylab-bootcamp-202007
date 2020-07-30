59cbf69c7c2400822d0ce042dabaff29fcca5ce8
alishpls <aleixsaar@gmail.com>
2020-07-30 10:33:52 +0200

    Add Hello class

--- aleix-sanllehy/react-demo/src/index.js
+++ aleix-sanllehy/react-demo/src/index.js
@@ -5,14 +5,14 @@ import './index.css';
 import Hello from './Hello';
 import * as serviceWorker from './serviceWorker';
 
-setInterval(() => {
-	ReactDOM.render(
-		<React.StrictMode>
-			<Hello now={new Date().toISOString()} />
-		</React.StrictMode>,
-		document.getElementById('root')
-	);
-}, 1000);
+//setInterval(() => {
+ReactDOM.render(
+	<React.StrictMode>
+		<Hello /*now={new Date().toISOString()}*/ />
+	</React.StrictMode>,
+	document.getElementById('root')
+);
+//}, 1000);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
