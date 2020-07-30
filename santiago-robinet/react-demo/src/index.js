f05d36ab417d0e1afb11ef4ebd71227e218a612c
Santiago <santiagorobinet@gmail.com>
2020-07-30 14:45:11 +0200

    add display, sum and clicker components

--- santiago-robinet/react-demo/src/index.js
+++ santiago-robinet/react-demo/src/index.js
@@ -2,20 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import Sum from './component-Sum/Sum';
+import ConDitionalDisplay from './conditional-Display/ConditionalDisplay';
+
+
 
 ReactDOM.render(
-	<React.StrictMode>
-		<Sum a={'something'} b={2} />
-		{React.createElement(Sum, { a: 4, b: 2 }, null)}
-		{React.createElement(
-			'h2',
-			null,
-			React.createElement(Sum, { a: 4, b: 7 }, null)
-		)}
-	</React.StrictMode>,
-	document.getElementById('root')
-);
+  <React.StrictMode>
+    <ConDitionalDisplay isVisible={true}>
+      <h1>Hello</h1>
+      <p>world!</p>
+    </ConDitionalDisplay>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
