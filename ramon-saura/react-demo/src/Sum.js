6fe072441b5e3b6b560fbe145c2079d585599abf
Ramon Saura <ramon.saura@gmail.com>
2020-07-30 10:41:24 +0200

    date and sum

---
+++ ramon-saura/react-demo/src/Sum.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Sum(props) {
+	return (
+		<h1>
+			{props.a} + {props.b} = {props.a + props.b}
+		</h1>
+	);
+}
+export default Sum;
--- ramon-saura/react-demo/src/index.js
+++ ramon-saura/react-demo/src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import SayHi from './SayHi';
+import Sum from './Sum';
 import * as serviceWorker from './serviceWorker';
 
-setInterval(() => {
-	ReactDOM.render(
-		<React.StrictMode>
-			<SayHi now={new Date().toISOString()} />
-		</React.StrictMode>,
-		document.getElementById('root')
-	);
-}, 1000);
+ReactDOM.render(
+	<React.StrictMode>
+		<SayHi />
+		<Sum a={'somthing'} b={2} />
+	</React.StrictMode>,
+	document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
