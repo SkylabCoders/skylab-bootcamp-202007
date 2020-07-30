fc563cd9c1036a3bb933c70fa2804da980eee831
Victor Alemany <victor.alemany@skylabcoders.com>
2020-07-30 11:44:13 +0200

    Adding new features of react-demo app

--- victor-alemany/react-demo/src/Sum.js
+++ victor-alemany/react-demo/src/Sum.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
 function Sum(props){
-return <h1>{props.a}+{props.b} ={props.a}+{props.b} </h1>
+ 
+return (<h1>{props.a}+{props.b}={props.a + props.b} </h1>)
 }
+
+Sum.propTypes = {
+    a: PropTypes.number.isRequired,
+    b: PropTypes.number.isRequired
+};
 
 export default Sum;
--- victor-alemany/react-demo/src/index.js
+++ victor-alemany/react-demo/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
-import Hello from './Hello';
 import Sum from './Sum';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Hello />
-    <Sum a={4} b={2} />
+    <Sum a={4} b={2}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
