9518a39efbbc95df01bcc79936344903700e26e3
Gilberto Cao <gilbe.cao@gmail.com>
2020-07-31 11:24:26 +0200

    redirect with condition

--- gilbe-cao/toh-react/src/components/PageNotFound.js
+++ gilbe-cao/toh-react/src/components/PageNotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 
 function PageNotFound() {
+	const condition = true;
 	return (
 		<section>
+			{condition === true && <Redirect to="/" />}
 			<h1>Error - 404</h1>
 			<p>Nothing to see here !</p>
 			<Link to="/">Go back to Dashboard</Link>
