e6e31908eb6b3b7248f6bbca71e07a26282f305b
Esther Morillo <esther.morillo@gmail.com>
2020-08-01 10:38:43 +0200

    tours normal and react

--- esther-morillo/toh-react/src/App.js
+++ esther-morillo/toh-react/src/App.js
@@ -6,12 +6,12 @@ import Header from './components/Header';
 import HeroDashboard from './components/HeroDashboard';
 import { Route } from 'react-router-dom';
 
-function App() {
+function App(props) {
 	return (
-		<div className="lightbox">
+		<div>
 			<Header />
 			<Route path="/" exact component={HeroDashboard} />
-			<Route path="/heroes" component={HeroList} />
+			<Route path="/hero" component={HeroList} />
 			<Route path="/hero/:heroId" component={HeroDetail} />
 		</div>
 	);
