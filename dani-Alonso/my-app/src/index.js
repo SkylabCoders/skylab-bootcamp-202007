3174b993638ff63b8a3492bc8bb59ce0fb59a40b
Dani Alonso <danialonso85@hotmail.es>
2020-08-01 18:21:28 +0200

    add functions

--- dani-Alonso/my-app/src/index.js
+++ dani-Alonso/my-app/src/index.js
@@ -1,27 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Sum from './Sum'
-import Clicker from './Clicker';
-import MyButton from './MyButton';
-import ConditionalDisplay from './ConditionalDisplay'
-
-
-const myAttributes = {
-  a: 4,
-  b: 2
-}
 
 ReactDOM.render(
   <React.StrictMode>
-
-    <Sum {...myAttributes} />
-    <Clicker handleClick={(letter) => console.log(letter)} />
-    <ConditionalDisplay isVisible={true}>
-      <h1>Hello</h1>
-      <h2>world!</h2>
-    </ConditionalDisplay>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
--- dani-Alonso/react-demo/src/Clicker.js
+++ dani-Alonso/react-demo/src/Clicker.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import MyButton from './MyButton';
 function Clicker({ handleClick, myButton }) {
-    const myLastName = ['G', 'I']
+    const myLastName = ['D', 'A', 'N', 'I']
 
     return (<>
 
