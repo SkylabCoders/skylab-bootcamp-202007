298920a35a156d8c739a5e82ea9a2b28eaa6fefd
gemmas95 <gemma1030@gmail.com>
2020-08-03 18:39:42 +0200

    added footer and banner fixed

--- ProjectGGJ/projectggj/src/components/footerComponent/footerComponent.js
+++ ProjectGGJ/projectggj/src/components/footerComponent/footerComponent.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './footerComponent.css';
 
 function FooterComponent() {
     return (
-        <div>
-            <button type="button" onClick={() => window.history.back()}>Back</button>
+        <div className="mainContainer">
+            <img alt="Dragon Ball One Start" src="https://i.pinimg.com/originals/a5/f9/a2/a5f9a2eb5c0bfb1f66988696e1f31334.png" onClick={() => window.history.back()}></img>
+            <p>By GGJ</p>
         </div>
     )
 }
