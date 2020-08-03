1670b5e5a66ff856a94e5c78d4061649d89bd854
Gerard Ramon <gerardramonp@gmail.com>
2020-08-03 18:25:53 +0200

    Add logo to navbar

--- 00-GROUP-CHALLENGE/04-polke/src/components/Navbar/HeaderNabvar.js
+++ 00-GROUP-CHALLENGE/04-polke/src/components/Navbar/HeaderNabvar.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../shared/generalStyles.css';
+import './HeaderNavbar.css';
 import { Navbar, Nav } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
-import { logo } from '../../assets/img/logo.png';
 
 function HeaderNavbar() {
-	const imgLogo = <img src={logo} alt="Logo" />;
+	const imgLogo = (
+		<img
+			src={require('../../assets/img/ironcat.png')}
+			className="navbar__logo"
+		/>
+	);
 	return (
 		<>
 			<Navbar expand="lg" className="--bg-color-navBar">
 				<Navbar.Brand>
-					<img src="../../assets/img/logo.png" alt="Logo" />
-					<NavLink to="/"></NavLink>
+					<NavLink to="/">{imgLogo}</NavLink>
 				</Navbar.Brand>
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
