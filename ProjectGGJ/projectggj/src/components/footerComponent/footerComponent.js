a4c1a4f1e489337924143e4678a90e93e9df8f17
gemmas95 <gemma1030@gmail.com>
2020-08-03 17:04:50 +0200

    added img in both media and delete button

--- ProjectGGJ/projectggj/src/components/footerComponent/footerComponent.js
+++ ProjectGGJ/projectggj/src/components/footerComponent/footerComponent.js
@@ -6,7 +6,6 @@ function FooterComponent() {
         <div className="mainContainer">
             <img alt="Dragon Ball One Start" src="https://i.pinimg.com/originals/a5/f9/a2/a5f9a2eb5c0bfb1f66988696e1f31334.png" onClick={() => window.history.back()}></img>
             <p>By GGJ</p>
-            <button type="button" onClick={() => window.history.back()}>Back</button>
         </div>
     )
 }
