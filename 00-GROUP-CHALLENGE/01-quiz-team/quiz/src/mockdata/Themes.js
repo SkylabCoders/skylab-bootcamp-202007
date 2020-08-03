66d3e1314ebd1e0d61055efd0cf1f7beda03406d
fcesc <bpfran@gmail.com>
2020-08-03 16:31:50 +0200

    folder update

---
+++ 00-GROUP-CHALLENGE/01-quiz-team/quiz/src/mockdata/Themes.js
@@ -0,0 +1,9 @@
+/* quizteam - skylab bootcamp 202007 */
+'use strict';
+
+export default THEMES_LIST = ['General Knowledge', 'Entertainment: Books', 'Entertainment: Film', 'Entertainment: Music',
+ 'Entertainment: Musicals & Theatre', 'Entertainment: Television', 'Entertainment: Video Games',
+ 'Entertainment: Board Games', 'Science & Nature', 'Science: Computers', 'Science: Mathematics',
+ 'Mythology', 'Sports', 'Geography', 'History', 'Politics', 'Art', 'Celebrities', 'Animals', 'Vehicles',
+ 'Entertainment: Comics', 'Science: Gadgets', 'Entertainment: Japanese Anime & Manga', 'Entertainment: Cartoon',
+ 'Animations'];		
