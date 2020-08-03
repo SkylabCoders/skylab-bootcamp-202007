2af6ef5ce907ff9e5fad2d0cb7def2061a26e008
MartiAmat <martiamat@hotmail.com>
2020-08-04 00:24:12 +0200

    recommendation component

--- 00-GROUP-CHALLENGE/05-recypefy/recipefy/src/components/Main.js
+++ 00-GROUP-CHALLENGE/05-recypefy/recipefy/src/components/Main.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import Recommendation from './Recommendation';
 
-
-function Main(){
-    return(
-        <>
-          <p> I'm da Main!! And I work!!</p>
-        </>
-    );
+function Main() {
+	return (
+		<>
+			<p> I'm da Main!! And I work!!</p>
+			<Recommendation />
+		</>
+	);
 }
 
 export default Main;
