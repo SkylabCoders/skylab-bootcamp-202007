02b427e933668fe7ced2d0599d2b52d1ec0daf95
Fernando <fvaldivielso@outlook.com>
2020-08-03 10:29:33 +0200

    copy from Gilbe

--- fernando-valdivielso/toh-react/src/App.js
+++ fernando-valdivielso/toh-react/src/App.js
@@ -1,25 +1,54 @@
 import React from 'react';
 import './App.css';
 import HeroDetail from './components/HeroDetail';
-import HeroList from './components/HeroList';
 import Header from './components/Header';
 import HeroDashboard from './components/HeroDashboard';
 import { Route, Switch } from 'react-router-dom';
+import PageNotFound from './components/PageNotFound';
+import HeroesPage from './components/HeroesPage';
 
 function App(props) {
 	return (
-		<div>
+		<div className="container">
 			<Header />
 			<Switch>
-
 				<Route path="/" exact component={HeroDashboard} />
 				<Route path="/hero/:heroId" component={HeroDetail} />
-
-				<Route path="/hero" component={HeroList} />
-
+				<Route path="/hero" component={HeroDetail} />
+				<Route path="/heroes" component={HeroesPage} />
+				<Route component={PageNotFound} />
 			</Switch>
 		</div>
 	);
 }
 
 export default App;
+
+
+
+
+// import React from 'react';
+// import './App.css';
+// import HeroDetail from './components/HeroDetail';
+// import HeroList from './components/HeroList';
+// import Header from './components/Header';
+// import HeroDashboard from './components/HeroDashboard';
+// import { Route, Switch } from 'react-router-dom';
+
+// function App(props) {
+// 	return (
+// 		<div>
+// 			<Header />
+// 			<Switch>
+
+// 				<Route path="/" exact component={HeroDashboard} />
+// 				<Route path="/hero/:heroId" component={HeroDetail} />
+
+// 				<Route path="/hero" component={HeroList} />
+
+// 			</Switch>
+// 		</div>
+// 	);
+// }
+
+// export default App;
