7e6c256f522951b4facdf0faa56279fa861f6d98
gemmas95 <gemma1030@gmail.com>
2020-08-03 16:38:56 +0200

    added img

--- ProjectGGJ/projectggj/src/components/footerComponent/footerComponent.js
+++ ProjectGGJ/projectggj/src/components/footerComponent/footerComponent.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
-import footerComponent from "footerComponent";
+import './footerComponent.css';
 
 function FooterComponent(props) {
     return (
-        <div>
+        <div className="mainContainer">
+            <p>By GGJ</p>
             <button type="button" onClick={() => this.props.history.goBack()}>Back</button>
+            <img alt="Dragon Ball One Start" src="https://i.pinimg.com/originals/a5/f9/a2/a5f9a2eb5c0bfb1f66988696e1f31334.png" onClick={() => this.props.history.goBack()}></img>
         </div>
     )
 }
