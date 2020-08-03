1a30bda0fdcb9fc4da631073b8283f309cb4655d
Dani Alonso <danialonso85@hotmail.es>
2020-08-03 18:06:00 +0200

    Add img logo and hide search and buttons when screen is in mobile

--- 00-GROUP-CHALLENGE/04-polke/src/components/Navbar/HeaderNabvar.js
+++ 00-GROUP-CHALLENGE/04-polke/src/components/Navbar/HeaderNabvar.js
@@ -3,26 +3,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../shared/generalStyles.css';
 import { Navbar, Nav } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
+import { logo } from '../../assets/img/logo.png';
 
 function HeaderNavbar() {
+	const imgLogo = <img src={logo} alt="Logo" />;
 	return (
 		<>
-			<Navbar expand="lg">
+			<Navbar expand="lg" className="--bg-color-navBar">
 				<Navbar.Brand>
-					<NavLink to="/">Git-Data</NavLink>
+					<img src="../../assets/img/logo.png" alt="Logo" />
+					<NavLink to="/"></NavLink>
 				</Navbar.Brand>
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
+					<input></input>
+					<button>Go to URL</button>
+
+					<button>SwitchLD</button>
 					<Nav className="mr-auto">
 						<Nav.Link>
 							<NavLink to="/userDetail">UserDetail</NavLink>
 						</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
-				<input></input>
-				<button>Go to URL</button>
-
-				<button>SwitchLD</button>
 			</Navbar>
 		</>
 	);
