7231252dab98c682cb8cd8331c1d554baf43ab37
gaeremtro <gabripenalva42@gmail.com>
2020-08-03 18:28:15 +0200

    targect done

--- ProjectGGJ/projectggj/src/components/charListComponent/charListComponent.js
+++ ProjectGGJ/projectggj/src/components/charListComponent/charListComponent.js
@@ -20,12 +20,28 @@ function CharListComponent() {
             <ul>
 
                 {chars.map((char) => (
-                    <li key={char._id}>
-                        <img src={char.image} alt='' width={'50'}></img>
-                        <Link to={`/details/${char.name}`}>
-                            {char._id}: {char.name}
-                        </Link>
-                    </li>
+                    <Link to={`/details/${char.name}`}>
+                        <div class="card mb-3" >
+                            <div class="row no-gutters">
+                                <div class="col-md-4">
+                                    <img src={char.image} class="card-img" alt="..."></img>
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title">{char.name}</h5>
+                                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </Link>
+                    // <li key={char._id}>
+                    //     <Link to={`/details/${char.name}`}>
+                    //         <img src={char.image} alt='' width={'50'}></img>
+                    //         {char._id}: {char.name}
+                    //     </Link>
+                    // </li>
                 ))}
             </ul>
         </>
