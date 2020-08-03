047d467ba6aea75007c414ce44f0c1fb40abbea3
Victor Alemany <victor.alemany@skylabcoders.com>
2020-08-03 23:52:39 +0200

    Adding last features of header and footer

--- 00-GROUP-CHALLENGE/01-quiz-team/quiz/src/components/Footer.js
+++ 00-GROUP-CHALLENGE/01-quiz-team/quiz/src/components/Footer.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './../css/Footer.css'
+import './../css/styles.css'
 
 function Footer(){
     return (
-    <div className="footer"><small>Created by Francesc & Victor for Skylab Coders Bootcamp</small></div>
+    <>
+    <div className="footer border border-info">     
+        <small>Created by Francesc & Victor for Skylab Coders Bootcamp</small>
+    </div>
+    </>
     )
 }
 
--- 00-GROUP-CHALLENGE/01-quiz-team/quiz/src/components/Header.js
+++ 00-GROUP-CHALLENGE/01-quiz-team/quiz/src/components/Header.js
@@ -1,35 +1,37 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './../css/styles.css'
 import './../css/Header.css'
 
 
-function Header(props){
-    return (
-    <>	
-		<div className="header">
-			<div>
-				<h1>The Quiz</h1>
-            </div> 			
-			<nav>
-				<NavLink activeClassName="my-active-class" to="/">
-					Home
+function Header(props) {
+	return (
+		<>
+			<div className="header border border-info">
+				<div className="inner__header">
+						<div className="logo__header">
+							<img className="img__header" src="https://storage.needpix.com/rsynced_images/quiz-2074324_1280.png" alt="quiz logo" />
+						</div>
+					<nav>
+						<NavLink activeClassName="my-active-class" to="/">
+							Home
 				</NavLink>{' '}
-				{' | '}
-				<NavLink activeClassName="my-active-class" to="/leaderboard">
-					Leaderboard
+						{' | '}
+						<NavLink activeClassName="my-active-class" to="/leaderboard">
+							Leaderboard
 				</NavLink>{' '}
-				{' | '}
-				<NavLink activeClassName="my-active-class" to="/dashboard">
-					Dashboard
-				</NavLink>{' '}
-				{' | '}
-				<NavLink activeClassName="my-active-class" to="/login">
-					Login
+						{' | '}
+						<NavLink activeClassName="my-active-class" to="/dashboard">
+							Dashboard
 				</NavLink>
-			</nav>
-		</div>
+					</nav>
+					<NavLink activeClassName="my-active-class" to="/Login">
+						<button type="button" class="btn btn-info">Login</button>
+					</NavLink>
+				</div>
+			</div>
 		</>
-    )
+	)
 }
 
 export default Header;
