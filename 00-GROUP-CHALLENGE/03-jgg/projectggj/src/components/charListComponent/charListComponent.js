916b54956c9fab80f71af572cf68c13e9f71a5b3
gaeremtro <gabripenalva42@gmail.com>
2020-08-03 20:44:02 +0200

    home list view

--- 00-GROUP-CHALLENGE/03-jgg/projectggj/src/components/charListComponent/charListComponent.js
+++ 00-GROUP-CHALLENGE/03-jgg/projectggj/src/components/charListComponent/charListComponent.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import store from '../../stores/store';
 import { loadCharList } from '../../actions/actions';
+import './charListComponent.css';
 
 function CharListComponent() {
     const [chars, setChars] = useState(store.getCharacters());
@@ -17,33 +18,26 @@ function CharListComponent() {
     }
     return (
         <>
-            <ul>
 
-                {chars.map((char) => (
-                    <Link to={`/details/${char.name}`}>
-                        <div class="card mb-3" >
-                            <div class="row no-gutters">
-                                <div class="col-md-4">
-                                    <img src={char.image} class="card-img" alt="..."></img>
-                                </div>
-                                <div class="col-md-8">
-                                    <div class="card-body">
-                                        <h5 class="card-title">{char.name}</h5>
-                                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                                    </div>
-                                </div>
+
+            {chars.map((char) => (
+                <Link key={char._id} to={`/details/${char.name}`}>
+                    <div className="card father-card" >
+                        <div className="row no-gutters hinherit">
+                            <div className='hinherit img-holder'>
+
+                                <img src={char.image} className="card-img " alt="..."></img>
                             </div>
+                            <div className="card-body">
+                                <h2 className="card-title">{char.name}</h2>
+                            </div>
+
                         </div>
-                    </Link>
-                    // <li key={char._id}>
-                    //     <Link to={`/details/${char.name}`}>
-                    //         <img src={char.image} alt='' width={'50'}></img>
-                    //         {char._id}: {char.name}
-                    //     </Link>
-                    // </li>
-                ))}
-            </ul>
+                    </div>
+                </Link>
+
+            ))}
+
         </>
     );
 }
