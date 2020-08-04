779f237285c89afad40dcb7ff10bc3dc93ad394c
gemmas95 <gemma1030@gmail.com>
2020-08-04 21:20:58 +0200

    created mock sagas

--- 00-GROUP-CHALLENGE/03-jgg/projectggj/src/components/sagaComponent/sagaComponent.js
+++ 00-GROUP-CHALLENGE/03-jgg/projectggj/src/components/sagaComponent/sagaComponent.js
@@ -5,11 +5,11 @@ import { loadSagaList } from '../../actions/actions';
 import './sagaComponent.css';
 
 
-function getSagaList() {
+/* function getSagaList() {
     debugger
     var seriesList = loadSagaList();
     seriesList.forEach(char => (char.series))
-}
+} */
 
 /*     .filter((character) => {
         return character.series
@@ -26,8 +26,11 @@ function SagaComponent(props) {
     useEffect(() => {
         debugger
         store.addChangeListener(onChange);
-        if (chars.length === 0) getSagaList();
+        if (chars.length === 0) loadSagaList();
         return () => store.removeChangeListener(onChange)
+        function filterSagas() {
+            chars.forEach(char)
+        }
     }, [chars.length]);
 
     function onChange() {
@@ -35,11 +38,27 @@ function SagaComponent(props) {
     }
 
 
+
     return (
         <>
             <div className='card-holder'>
-                {chars}
-            </div>
+                {chars.map((char) => (
+                    <Link key={char._id} to={`/details/${char.name}`}>
+                        <div className="card father-card" >
+                            <div className="row no-gutters hinherit">
+                                <div className='hinherit img-holder'>
+
+                                    <img src={char.image} className="card-img " alt="..."></img>
+                                </div>
+                                <div className="card-body">
+                                    <h2 className="card-title">{char.series}</h2>
+                                </div>
+
+                            </div>
+                        </div>
+                    </Link>
+
+                ))}</div>
         </>
     )
 }
