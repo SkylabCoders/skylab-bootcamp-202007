ac761e5675470a12dfe9ede46294916d8b52b19e
Gerard Ramon <gerardramonp@gmail.com>
2020-08-04 15:50:45 +0200

    Add chart

--- 00-GROUP-CHALLENGE/04-polke/src/components/Landing/Login/Login.js
+++ 00-GROUP-CHALLENGE/04-polke/src/components/Landing/Login/Login.js
@@ -5,34 +5,36 @@ import './Login-styles.css';
 
 function Login() {
 	return (
-		<form className="Login__form">
-			<h2 className="Login__form__login-title--colored">Login</h2>
-			<FormGroup
-				className="Login__login-with-button"
-				controlId="username"
-				bsSize="large"
-			>
-				<Button>Google</Button>
-			</FormGroup>
-			<FormGroup
-				className="Login__login-with-button"
-				controlId="password"
-				bsSize="large"
-			>
-				<Button>GitHub</Button>
-			</FormGroup>
-			<FormGroup
-				className="Login__form__container-input"
-				controlId="input"
-				bsSize="large"
-			>
-				<FormControl className="Login__form__input" placeholder="&#128100;" />
-				<FormControl className="Login__form__input" placeholder="&#128272;" />
+		<>
+			<form className="Login__form">
+				<h2 className="Login__form__login-title--colored">Login</h2>
+				<FormGroup
+					className="Login__login-with-button"
+					controlId="username"
+					bsSize="large"
+				>
+					<Button>Google</Button>
+				</FormGroup>
+				<FormGroup
+					className="Login__login-with-button"
+					controlId="password"
+					bsSize="large"
+				>
+					<Button>GitHub</Button>
+				</FormGroup>
+				<FormGroup
+					className="Login__form__container-input"
+					controlId="input"
+					bsSize="large"
+				>
+					<FormControl className="Login__form__input" placeholder="&#128100;" />
+					<FormControl className="Login__form__input" placeholder="&#128272;" />
 
-				<Button className="Login__form__last-button">Enter</Button>
-			</FormGroup>
-			<a className="Login__form__anchor--colored">Register</a>
-		</form>
+					<Button className="Login__form__last-button">Enter</Button>
+				</FormGroup>
+				<a className="Login__form__anchor--colored">Register</a>
+			</form>
+		</>
 	);
 }
 
