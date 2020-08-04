f079c34d0ad4ed6b9988bce67c2af1cec43c3436
gemmas95 <gemma1030@gmail.com>
2020-08-04 17:34:58 +0200

    modified adction, actionTypes, sagaComponents

--- 00-GROUP-CHALLENGE/03-jgg/projectggj/src/components/sagaComponent/sagaComponent.js
+++ 00-GROUP-CHALLENGE/03-jgg/projectggj/src/components/sagaComponent/sagaComponent.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { loadCharListAPI, loadSagaList } from '../../actions/actions';
+import store from "../../stores/store";
+
+function getSagaList() {
+    var seriesList = loadSagaList.filter((character) => {
+        return character.series
+    });
+
+    var uniqueSerieList = [...new Set(seriesList)];
+
+    return uniqueSerieList;
+}
 
 function SagaComponent(props) {
+    const [sagas, setSagas] = useState(store.getSagas());
+
+    useEffect(() => {
+        store.addChangeListener(onChange);
+        if (sagas.length === 0) getSagaList();
+        return () => store.removeChangeListener(onChange)
+    }, [sagas.length]);
+
+    function onChange() {
+        setSagas(store.getSagas())
+    }
+
+
     return (
         <div>
-            <p>SagaComponent works!</p>
+            {sagas}
             <Link to='/charList'>Z</Link>
         </div>
 
