d76e4214d539aeb35fa20628240b4f12daf38905
gemmas95 <gemma1030@gmail.com>
2020-08-05 11:25:53 +0200

    added loginComponent

--- 00-GROUP-CHALLENGE/03-jgg/projectggj/src/components/loginComponent/loginComponent.js
+++ 00-GROUP-CHALLENGE/03-jgg/projectggj/src/components/loginComponent/loginComponent.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import './loginComponent.css'
 
 function LoginComponent(props) {
-    return <p>loginComponent works!</p>
+    return (
+        <section className="text-center container">
+            <form className="form-signin">
+                <h1 className="m-2">Please sign in</h1>
+                <label>Email address</label>
+                <input id="inputEmail" className="m-1 form-control" type="email" placeholder="Email address" required="" autofocus=""></input>
+                <label>Password</label>
+                <input id="inputPassword" className="m-1 form-control" type="password" placeholder="Password" required=""></input>
+                <button className="m-1 mt-3 mb-3 btn-primary btn-lg btn-block" type="submit">Sign in</button>
+            </form>
+        </section >
+    )
 }
 
 export default LoginComponent;
