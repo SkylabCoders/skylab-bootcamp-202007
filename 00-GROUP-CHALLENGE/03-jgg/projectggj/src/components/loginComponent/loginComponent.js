42c9b713d3ac5c0b3cb84a17cb7b624257de00ea
gemmas95 <gemma1030@gmail.com>
2020-08-05 12:09:39 +0200

    register link path done

--- 00-GROUP-CHALLENGE/03-jgg/projectggj/src/components/loginComponent/loginComponent.js
+++ 00-GROUP-CHALLENGE/03-jgg/projectggj/src/components/loginComponent/loginComponent.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './loginComponent.css'
+import { Link } from 'react-router-dom';
+
 
 function LoginComponent(props) {
     return (
@@ -7,10 +9,11 @@ function LoginComponent(props) {
             <form className="form-signin">
                 <h1 className="m-2">Please sign in</h1>
                 <label>Email address</label>
-                <input id="inputEmail" className="m-1 form-control" type="email" placeholder="Email address" required="" autofocus=""></input>
+                <input id="inputEmail" className="m-1 form-control" type="email" placeholder="Email address" required="" autoFocus=""></input>
                 <label>Password</label>
                 <input id="inputPassword" className="m-1 form-control" type="password" placeholder="Password" required=""></input>
-                <button className="m-1 mt-3 mb-3 btn-primary btn-lg btn-block" type="submit">Sign in</button>
+                <button className="m-1 mt-3 mb-3 btn-warning btn-lg btn-block" type="submit">Sign in</button>
+                <button className="m-1 mt-3 mb-3 btn-warning btn-lg btn-block" type="submit"><a className="nav-link" href="/register">Register</a></button>
             </form>
         </section >
     )
