a7ac222e59b7d8a97e2ea8be1f0b4b63a6d17714
gemmas95 <gemma1030@gmail.com>
2020-08-05 18:54:22 +0200

    added firebase

--- 00-GROUP-CHALLENGE/03-jgg/projectggj/src/components/loginComponent/loginComponent.js
+++ 00-GROUP-CHALLENGE/03-jgg/projectggj/src/components/loginComponent/loginComponent.js
@@ -1,9 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './loginComponent.css'
-import { Link } from 'react-router-dom';
+import { login, logout } from '../../actions/authAction/authAction'
+import authStore from '../../stores/authStore'
 
 
 function LoginComponent(props) {
+
+    const email = 'gemma-skylab@gmail.com';
+    const password = 'gemma-skylab';
+
+    const [isLogged, setIsLogged] = useState(authStore.isLogged());
+    const [user, setUser] = useState(authStore.getUserProfile());
+
+    useEffect(() => {
+        authStore.addChangeListener(onAuthChange);
+    }, [isLogged]);
+
+    function onAuthChange() {
+        setIsLogged(authStore.isLogged());
+        setUser(authStore.getUserProfile());
+    }
     return (
         <section className="text-center container">
             <form className="form-signin">
@@ -14,6 +30,15 @@ function LoginComponent(props) {
                 <input id="inputPassword" className="m-1 form-control" type="password" placeholder="Password" required=""></input>
                 <button className="m-1 mt-3 mb-3 btn-warning btn-lg btn-block" type="submit">Sign in</button>
                 <button className="m-1 mt-3 mb-3 btn-warning btn-lg btn-block" type="submit"><a className="nav-link" href="/register">Register</a></button>
+                {!isLogged && (
+                    <button onClick={(event) => { event.preventDefault(); login(email, password) }}>Login</button>
+                )}
+                {isLogged && (
+                    <>
+                        <p>Welcome {user && user.email}!</p>
+                        <button onClick={(event) => { event.preventDefault(); logout() }}>Logout</button>
+                    </>
+                )}
             </form>
         </section >
     )
