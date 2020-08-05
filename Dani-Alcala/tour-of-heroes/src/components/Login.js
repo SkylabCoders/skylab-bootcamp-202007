beb8e2a1233f7fb4140d0a2d83c43ac9dec67fe4
Dani Alcala <daniamcode@gmail.com>
2020-08-05 18:48:16 +0200

    add firebase to tour of heroes

---
+++ Dani-Alcala/tour-of-heroes/src/components/Login.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react'
+import { login, logout } from '../actions/authActions'
+import authStore from '../stores/authStore'
+
+
+function Login() {
+    const email = 'daniamcode@gmail.com'
+    const password = '123456'
+
+    const[isLogged, setIsLogged] = useState(authStore.isLogged())
+    const [user, setUser] = useState(authStore.getUserProfile())
+
+    useEffect(() => {
+        authStore.addChangeListener(onAuthChange)
+        
+        return () => authStore.removeChangeListener(onAuthChange)
+    }, [isLogged, user])
+
+    function onAuthChange() {
+        setIsLogged(authStore.isLogged())
+        setUser(authStore.getUserProfile())
+    }
+
+    return (
+		<>
+        {!isLogged && (
+			<button onClick={() => login(email, password)}>Login</button>
+        )}
+        {isLogged && 
+            <button onClick={() =>logout()}>Logout</button>}
+            {/* <img src={user && user.photoURL} /> */}
+            <p>{user && user.email}</p>
+		</>
+	);
+}
+
+export default Login
