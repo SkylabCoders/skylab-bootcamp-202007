9dadf48a119feb66a099dfe5ef3e4e16131e357c
gemmas95 <gemma1030@gmail.com>
2020-08-05 12:31:00 +0200

    added register view

--- 00-GROUP-CHALLENGE/03-jgg/projectggj/src/components/loginComponent/loginComponent.js
+++ 00-GROUP-CHALLENGE/03-jgg/projectggj/src/components/loginComponent/loginComponent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './loginComponent.css'
 import { Link } from 'react-router-dom';
 
--- 00-GROUP-CHALLENGE/03-jgg/projectggj/src/components/registerComponent/registerComponent.js
+++ 00-GROUP-CHALLENGE/03-jgg/projectggj/src/components/registerComponent/registerComponent.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
 function RegisterComponent(props) {
-    return <p>registerComponent works!</p>
+    return (
+        <section className="text-center container">
+            <form className="form-signin">
+                <h1 className="m-2">Please sign in</h1>
+                <label>Name</label>
+                <input id="inputName" className="m-1 form-control" type="email" placeholder="Name" required="" autoFocus=""></input>
+                <label>Last Name</label>
+                <input id="inputLastName" className="m-1 form-control" type="email" placeholder="Name" required="" autoFocus=""></input>
+                <label>Birth Date</label>
+                <input id="inputBirthDate" className="m-1 form-control" type="date" placeholder="Fecha" required="" autoFocus=""></input>
+
+                <label>Email address</label>
+                <input id="inputEmail" className="m-1 form-control" type="email" placeholder="Email address" required="" autoFocus=""></input>
+                <label>Password</label>
+                <input id="inputPassword" className="m-1 form-control" type="password" placeholder="Password" required="" minLength="5" maxLength="20" ></input>
+                <button className="m-1 mt-3 mb-3 btn-warning btn-lg btn-block" type="submit">Create account!</button>
+            </form>
+        </section >
+    )
 }
 
 export default RegisterComponent;
