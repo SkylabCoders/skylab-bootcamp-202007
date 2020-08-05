db33b5c6a10b058385354a5693a9632d5b577ac6
Pablo <pablo_hortelano@hotmail.comm>
2020-08-05 12:18:24 +0200

    Setting actions, actionTypes for UserInfo

--- 00-GROUP-CHALLENGE/04-polke/src/actions/userDetailActions.js
+++ 00-GROUP-CHALLENGE/04-polke/src/actions/userDetailActions.js
@@ -18,3 +18,21 @@ export function loadRepoList(userName) {
 		})
 		.catch((error) => console.error(error.message));
 }
+
+export function loadUserImg(userName) {
+	// Load data from somewhere
+	const endPoint = `https://api.github.com/users/${userName}/repos`;
+	fetch(endPoint, {
+		headers: {
+			accept: 'application/vnd.github.v3+json'
+		}
+	})
+		.then((response) => response.json())
+		.then((response) => {
+			dispatcher.dispatch({
+				type: actionTypes.LOAD_REPO_LIST,
+				data: response
+			});
+		})
+		.catch((error) => console.error(error.message));
+}
--- 00-GROUP-CHALLENGE/04-polke/src/components/UserDetail/UserInfo/UserInfo.js
+++ 00-GROUP-CHALLENGE/04-polke/src/components/UserDetail/UserInfo/UserInfo.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import '../../../shared/generalStyles.css';
 import './UserInfo.css';
+import { Card } from 'react-bootstrap';
 
-function UserInfo() {}
+function UserInfo() {
+	const [userImg, setuserImg] = useState([]);
+	const [userName, setUserName] = useState('phortela1n');
+
+	return (
+		<Card style={{ width: '18rem' }}>
+			<Card.Img variant="top" src="holder.js/100px180" />
+			<Card.Body>
+				<Card.Title>User Name</Card.Title>
+				<Card.Text>User description</Card.Text>
+			</Card.Body>
+		</Card>
+	);
+}
 
 export default UserInfo;
