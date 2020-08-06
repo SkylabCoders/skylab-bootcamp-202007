09baafb307e8fe7f40fcb2a506905745e17916e3
Dani Alonso <danialonso85@hotmail.es>
2020-08-06 13:21:53 +0200

    add logic to change repoCard icon

--- 00-GROUP-CHALLENGE/04-polke/src/components/RepoCard/RepoCard.js
+++ 00-GROUP-CHALLENGE/04-polke/src/components/RepoCard/RepoCard.js
@@ -2,28 +2,40 @@ import React from 'react';
 import './RepoCard.css';
 import { Button } from 'react-bootstrap';
 
-function RepoCard() {
+function RepoCard({ repoInfo }) {
+	const lockImg = null;
+
+	if (repoInfo.private === true) {
+		lockImg = (
+			<img
+				className="padlock"
+				src={require('../../assets/img/padlock.png')}
+				alt="padlock"
+			/>
+		);
+	} else {
+		lockImg = (
+			<img
+				className="padlock"
+				src={require('../../assets/img/unlock.png')}
+				alt="unlock"
+			/>
+		);
+	}
+
 	return (
 		<section className="repoCard">
 			<div className="repoCard__name-private">
 				<div className="repoCard__private">
-					<img
-						className="padlock"
-						src={require('../../assets/img/padlock.png')}
-						alt="moon"
-					/>
+					{lockImg}
 					{/* git  */}
 				</div>
-				<div className="repoCard__name">Polke!!</div>
+				<div className="repoCard__name">{repoInfo.repoName}</div>
 			</div>
-			<div className="repoCard__description">
-				As a user I want to log in with Github / Google / Email So that I can
-				access the app. **Scenario #1** Given an empty form, and a `checkForm`
-				function When user tries to log in Then it should throw an Error
-			</div>
+			<div className="repoCard__description">{repoInfo.repoDesc}</div>
 			<div className="repoCard__showlastUpdate-lang">
-				<div className="repoCard__lastUpdate">13/06/1985</div>
-				<div className="repoCard__lang">css, js, html</div>
+				<div className="repoCard__lastUpdate">{repoInfo.repoDate}</div>
+				<div className="repoCard__lang">{repoInfo.repoLang}</div>
 			</div>
 			<div className="repoCard__details">
 				<Button variant="outline-secondary" className="button__details">
