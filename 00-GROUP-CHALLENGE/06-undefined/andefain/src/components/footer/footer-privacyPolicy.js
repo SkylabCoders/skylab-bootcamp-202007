14c8e88a6f34951be2267650b6a63bdd6fd9a0e2
Vanesa Gómez <v.cucchiararo@gmail.com>
2020-08-06 09:21:03 +0200

    ok auth with google

--- 00-GROUP-CHALLENGE/06-undefined/andefain/src/components/footer/footer-privacyPolicy.js
+++ 00-GROUP-CHALLENGE/06-undefined/andefain/src/components/footer/footer-privacyPolicy.js
@@ -60,20 +60,7 @@ function PrivacyPolicy() {
 					information about you from other sources, such as your name and email
 					address when you sign in to Andefain using a third-party login
 					service.&nbsp;Click &nbsp;to see additional examples of the
-					information we receive. "Lorem ipsum dolor sit amet, consectetur
-					adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-					magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-					ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-					irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-					fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-					sunt in culpa qui officia deserunt mollit anim id est laborum." "Lorem
-					ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-					veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-					ea commodo consequat. Duis aute irure dolor in reprehenderit in
-					voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-					sint occaecat cupidatat non proident, sunt in culpa qui officia
-					deserunt mollit anim id est laborum."
+					information we receive.
 				</li>
 			</ul>
 
@@ -84,20 +71,7 @@ function PrivacyPolicy() {
 			<p>
 				We use your personal information to operate, provide, develop, and
 				improve the products and services that we offer our customers. These
-				purposes include. "Lorem ipsum dolor sit amet, consectetur adipiscing
-				elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-				Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-				ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-				reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-				pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
-				qui officia deserunt mollit anim id est laborum." "Lorem ipsum dolor sit
-				amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-				labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-				exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-				Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-				dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est
-				laborum."
+				purposes include.
 			</p>
 
 			<p>
@@ -105,27 +79,7 @@ function PrivacyPolicy() {
 				information about you from other sources, such as your name and email
 				address when you sign in to Andefain using a third-party login
 				service.&nbsp;Click &nbsp;to see additional examples of the information
-				we receive. Mauris mattis commodo lacus, non pellentesque ante. Fusce
-				vel scelerisque neque. Praesent eu odio sed nisl condimentum commodo.
-				Suspendisse libero mi, malesuada at sem vel, laoreet pulvinar orci.
-				Suspendisse euismod aliquam molestie. Nam scelerisque elementum tortor
-				id ornare.
-			</p>
-			<p>
-				Proin porta laoreet est. Vivamus ac libero hendrerit, aliquam quam non,
-				mattis diam. Nunc blandit hendrerit enim, vitae bibendum dolor egestas
-				non. Mauris viverra facilisis dignissim. Pellentesque vel pretium arcu.
-				Maecenas rutrum interdum lectus, id sagittis risus luctus in. Ut posuere
-				diam nec sem vehicula ultrices. Quisque et nibh eget mi pretium gravida.
-				Quisque elementum consectetur lacinia. Nunc dignissim bibendum neque.
-				Integer ac justo tortor. Phasellus luctus erat at eleifend imperdiet.
-				Vestibulum fringilla, enim non rhoncus posuere, ligula elit blandit dui,
-				vitae auctor lorem mauris malesuada ex. Integer ac dignissim ex, id
-				volutpat neque. Sed egestas risus urna, ut ultrices est interdum vitae.
-				In hac habitasse platea dictumst. In blandit, lacus eu fringilla
-				efficitur, neque ipsum sodales justo, quis hendrerit arcu nisi vel
-				risus. Etiam dictum in lorem ut sodales. Integer quis varius urna, sit
-				amet sagittis mauris.
+				we receive.
 			</p>
 		</div>
 	);
