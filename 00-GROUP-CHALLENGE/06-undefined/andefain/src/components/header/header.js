5ca3988fda1a76d3eebc2bbf69db7e6f7b41d929
alishpls <aleixsaar@gmail.com>
2020-08-06 13:54:32 +0200

    Fix more stuff

--- 00-GROUP-CHALLENGE/06-undefined/andefain/src/components/header/header.js
+++ 00-GROUP-CHALLENGE/06-undefined/andefain/src/components/header/header.js
@@ -4,43 +4,27 @@ import { Link } from 'react-router-dom';
 
 function Header(props) {
 	const [login, setLogin] = useState('Login');
+	const [search, setSearch] = useState('');
 
-	function display(search) {
-		console.log(search);
-	}
-
-	function submit(searchValue) {
-		window.location.pathname = '/finder/' + searchValue;
-	}
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		window.location.pathname = '/finder/' + search;
+	};
 
 	return (
 		<>
 			<div className="header-container">
 				<header className="header">
 					<Link className="header-logo" to="/"></Link>
-					<Link to="/">
-						<div className="header-menu">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								width="24"
-								height="24"
-								className="ipc-icon ipc-icon--menu"
-								viewBox="0 0 24 24"
-								fill="currentColor"
-								role="presentation"
-							>
-								<path fill="none" d="M0 0h24v24H0V0z"></path>
-								<path d="M4 18h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zm0-5h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zM3 7c0 .55.45 1 1 1h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1z"></path>
-							</svg>
-							<span className="header-menu__text">Menu</span>
-						</div>
-					</Link>
-					<input
-						onInput={(event) => display(event.target.value)}
-						onDoubleClick={(event) => submit(event.target.value)}
-						className="header-search"
-						placeholder="Search any title or name..."
-					></input>
+					<form className="header-form" onSubmit={handleSubmit}>
+						<input
+							type="text"
+							value={search}
+							className="header-form__search"
+							placeholder="Search any title or name..."
+							onChange={(event) => setSearch(event.target.value)}
+						></input>
+					</form>
 
 					<div className="header-login">
 						<div className="header-login__hidden-menu">
