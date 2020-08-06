1c9252985149dd206de25e409f4ac99add93ecc7
fcesc-code <bpfran@gmail.com>
2020-08-06 16:01:24 +0200

    update game

--- 00-GROUP-CHALLENGE/01-quiz-team/quiz/src/components/Results.js
+++ 00-GROUP-CHALLENGE/01-quiz-team/quiz/src/components/Results.js
@@ -10,6 +10,14 @@ function Results(props){
                 <p>Game started at {props.data_started} and ended at {props.data_ended}</p>
                 <p>Total playtime: {props.data_played}.</p>
                 <p>Number of questions asked: {props.data_questions_total}</p>
+                <div className="playAgain">
+                    <div className="playAgain__sameTheme">
+                        <button onClick={props.newGameClick}>Play again, same theme</button>
+                    </div>
+                    <div className="playAgain__anotherTheme">
+                        <button onClick={props.newGameClick}>Play again, another theme</button>
+                    </div>
+                </div>
             </section>
         </div>
     );
