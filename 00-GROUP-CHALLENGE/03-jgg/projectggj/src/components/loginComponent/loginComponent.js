02c402054f51bfd226680d3f0b024d876b8f26a8
Jordi Muñoz <jordimm11@gmail.com>
2020-08-06 09:07:52 +0200

    trying get input value

--- 00-GROUP-CHALLENGE/03-jgg/projectggj/src/components/loginComponent/loginComponent.js
+++ 00-GROUP-CHALLENGE/03-jgg/projectggj/src/components/loginComponent/loginComponent.js
@@ -6,11 +6,12 @@ import authStore from '../../stores/authStore'
 
 function LoginComponent(props) {
 
-    const email = 'gemma-skylab@gmail.com';
-    const password = 'gemma-skylab';
+    //const email = 'gemma-skylab@gmail.com';
+    const password = '123456';
 
     const [isLogged, setIsLogged] = useState(authStore.isLogged());
     const [user, setUser] = useState(authStore.getUserProfile());
+    const [email, setEmail] = useState();
 
     useEffect(() => {
         authStore.addChangeListener(onAuthChange);
@@ -20,18 +21,28 @@ function LoginComponent(props) {
         setIsLogged(authStore.isLogged());
         setUser(authStore.getUserProfile());
     }
+
+    function handleChange(event, email, password) {
+        console.log(email);
+        console.log(user);
+        event.preventDefault();
+        setEmail(email);
+        login(email, password);
+       
+      }
+
     return (
         <section className="text-center container">
             <form className="form-signin">
                 <h1 className="m-2">Please sign in</h1>
                 <label>Email address</label>
-                <input id="inputEmail" className="m-1 form-control" type="email" placeholder="Email address" required="" autoFocus=""></input>
+                <input id="inputEmail" onChange='' className="m-1 form-control" type="email" placeholder="Email address" required="" autoFocus=""></input>
                 <label>Password</label>
                 <input id="inputPassword" className="m-1 form-control" type="password" placeholder="Password" required=""></input>
-                <button className="m-1 mt-3 mb-3 btn-warning btn-lg btn-block" type="submit">Sign in</button>
+                <button className="m-1 mt-3 mb-3 btn-warning btn-lg btn-block" type="submit" onClick={(event) => handleChange(event, email, password)}>Sign in</button>
                 <button className="m-1 mt-3 mb-3 btn-warning btn-lg btn-block" type="submit"><a className="nav-link" href="/register">Register</a></button>
                 {!isLogged && (
-                    <button onClick={(event) => { event.preventDefault(); login(email, password) }}>Login</button>
+                    <button onClick={(event) => handleChange(event, email, password)}>Login</button>
                 )}
                 {isLogged && (
                     <>
