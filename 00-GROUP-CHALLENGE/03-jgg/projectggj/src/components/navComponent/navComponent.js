84293897ba787b4588f447f3ceec77cb8f183a8b
gemmas95 <gemma1030@gmail.com>
2020-08-06 19:19:01 +0200

    changed implemented in navComponent

--- 00-GROUP-CHALLENGE/03-jgg/projectggj/src/components/navComponent/navComponent.js
+++ 00-GROUP-CHALLENGE/03-jgg/projectggj/src/components/navComponent/navComponent.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './navComponent.css';
+import authStore from '../../stores/authStore'
 
 function NavComponent(props) {
+
+	const [isLogged, setIsLogged] = useState(authStore.isLogged());
+
+	useEffect(() => {
+		authStore.addChangeListener(onAuthChange);
+		return () => authStore.removeChangeListener(onAuthChange)
+	}, [isLogged])
+
+	function onAuthChange() {
+		setIsLogged(authStore.isLogged())
+	}
+
 	return (
 		<nav className="navbar navbar-expand-sm navbar-dark bg-dark">
 			<a className="navbar-brand" href="/">
@@ -26,11 +39,20 @@ function NavComponent(props) {
 
 			<div className="collapse navbar-collapse" id="global-area__menu">
 				<ul className="navbar-nav mr-auto">
-					<li className="nav-item">
-						<a className="nav-link" href="/login">
-							Login
+					{!isLogged && (
+						<li className="nav-item">
+							<a className="nav-link" href="/login">
+								Login
 						</a>
-					</li>
+						</li>
+					)}
+					{isLogged && (
+						<li className="nav-item">
+							<a className="nav-link" href="/login">
+								Logout
+						</a>
+						</li>
+					)}
 					<li className="nav-item active">
 						<a className="nav-link" href="/">
 							Home <span className="sr-only">(current)</span>
