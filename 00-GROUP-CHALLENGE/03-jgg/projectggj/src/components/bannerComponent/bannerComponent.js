5a3c081ea2d61bdb0cef198a1755688653435207
gemmas95 <gemma1030@gmail.com>
2020-08-07 11:11:19 +0200

    deleted query image

--- 00-GROUP-CHALLENGE/03-jgg/projectggj/src/components/bannerComponent/bannerComponent.js
+++ 00-GROUP-CHALLENGE/03-jgg/projectggj/src/components/bannerComponent/bannerComponent.js
@@ -4,7 +4,7 @@ import './bannerComponent.css';
 function BannerComponent(props) {
     return (
         <div className="bannerContainer">
-            <img className="bannerImage" src="https://smoothcomp.com/pictures/t/428311-tn68/master-roshis-gym.png?embedView=1" alt="wallpaper kamehouse"></img>
+            <img className="bannerImage" src="https://smoothcomp.com/pictures/t/428311-tn68/master-roshis-gym.png" alt="wallpaper kamehouse"></img>
             <h1>WELCOME</h1>
         </div>
     )
