f9665a847200895491251cbb76bd6a0255ab7101
gemmas95 <gemma1030@gmail.com>
2020-08-07 12:05:55 +0200

    trying to working with other img

--- 00-GROUP-CHALLENGE/03-jgg/projectggj/src/components/bannerComponent/bannerComponent.js
+++ 00-GROUP-CHALLENGE/03-jgg/projectggj/src/components/bannerComponent/bannerComponent.js
@@ -5,7 +5,7 @@ import '../../'
 function BannerComponent(props) {
     return (
         <div className="bannerContainer">
-            <img className="bannerImage" src="https://wallpapercave.com/wp/wp4855908.png" alt="wallpaper kamehouse"></img>
+            <img className="bannerImage" src="https://i.pinimg.com/originals/9d/ba/b3/9dbab3202a6331c5e82ae7fe38a104c8.png" alt="wallpaper kamehouse"></img>
             <h1>WELCOME</h1>
         </div>
     )
