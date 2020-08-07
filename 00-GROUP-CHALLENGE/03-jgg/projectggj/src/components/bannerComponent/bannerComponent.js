1aafb3db5536e8d8028303ffc9de69ebc0e8aa8a
gemmas95 <gemma1030@gmail.com>
2020-08-07 11:26:36 +0200

    other wallpaper

--- 00-GROUP-CHALLENGE/03-jgg/projectggj/src/components/bannerComponent/bannerComponent.js
+++ 00-GROUP-CHALLENGE/03-jgg/projectggj/src/components/bannerComponent/bannerComponent.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './bannerComponent.css';
+import '../../'
 
 function BannerComponent(props) {
     return (
         <div className="bannerContainer">
-            <img className="bannerImage" src="https://smoothcomp.com/pictures/t/428311-tn68/master-roshis-gym.png" alt="wallpaper kamehouse"></img>
+            <img className="bannerImage" src="https://elcomercio.pe/resizer/WO_SUH5sZGaoB6J7txISRZqZXXk=/1200x1200/smart/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/P4VYBC4YRRGNZN5RUJRBJOEK3U.jpg" alt="wallpaper kamehouse"></img>
             <h1>WELCOME</h1>
         </div>
     )
