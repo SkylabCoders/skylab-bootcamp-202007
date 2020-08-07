e8b53cee7634194dec40e1cc4fa9e42f4ea06604
gemmas95 <gemma1030@gmail.com>
2020-08-07 11:53:04 +0200

    image original

--- 00-GROUP-CHALLENGE/03-jgg/projectggj/src/components/bannerComponent/bannerComponent.js
+++ 00-GROUP-CHALLENGE/03-jgg/projectggj/src/components/bannerComponent/bannerComponent.js
@@ -5,7 +5,7 @@ import '../../'
 function BannerComponent(props) {
     return (
         <div className="bannerContainer">
-            <img className="bannerImage" src="https://elcomercio.pe/resizer/WO_SUH5sZGaoB6J7txISRZqZXXk=/1200x1200/smart/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/P4VYBC4YRRGNZN5RUJRBJOEK3U.jpg" alt="wallpaper kamehouse"></img>
+            <img className="bannerImage" src="https://wallpapercave.com/wp/wp4855908.png" alt="wallpaper kamehouse"></img>
             <h1>WELCOME</h1>
         </div>
     )
