aa9d895c579e7f212c74b241536b19661d23c6bc
gemmas95 <gemma1030@gmail.com>
2020-08-08 16:24:49 +0200

    profileComponent added css and structure

--- 00-GROUP-CHALLENGE/03-jgg/projectggj/src/components/profileComponent/profileComponent.js
+++ 00-GROUP-CHALLENGE/03-jgg/projectggj/src/components/profileComponent/profileComponent.js
@@ -1,8 +1,55 @@
 import React from 'react';
+import { logout } from '../../actions/authAction/authAction'
+import { Link } from 'react-router-dom';
+
 
 function ProfileComponent() {
     return (
-        <p>userProfileComponent works!</p>
+        <div className="details-holder">
+            <div className="flex-item">
+                <div className="row">
+                    <div className="detail-container flex-item">
+                        <div className="card hovercard">
+                            <div className="cardheader"></div>
+                            <div className="avatar">
+                                {/*                                 <img alt="" src={char.image}></img>
+ */}                            </div>
+
+                            <div className="info">
+                                <div className="title">
+                                    <a target="_blank" href="/">
+                                        PROFILE
+                                    </a>
+                                </div>
+                                <div className="details-desc-content">
+                                    <div className="details-desc">
+                                        NAME: <span>Human</span>
+                                    </div>
+                                    <div className="details-desc">
+                                        RACE: <span>Human</span>
+                                    </div>
+                                    <div className="details-desc">
+                                        STATUS: <span>Alive</span>
+                                    </div>
+                                    <div className="details-desc">
+                                        PLANET: <span>Earth</span>
+                                    </div>
+                                    {/*                                     <div className="details-desc">
+                                        GENDER: <span>{char.gender}</span>
+                                    </div> */}
+
+                                </div>
+                                <div >
+                                    <Link to="/login" >
+                                        <button onClick={(event) => { event.preventDefault(); logout() }} className="m-1 mt-3 mb-3 btn-warning btn-lg btn-block">Logout</button>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
