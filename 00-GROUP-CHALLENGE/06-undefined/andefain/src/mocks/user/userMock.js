e6710b7f94a1114d0ec620cb1357524094ed867d
Vanesa Gómez <v.cucchiararo@gmail.com>
2020-08-08 17:11:14 +0200

    create user profile

---
+++ 00-GROUP-CHALLENGE/06-undefined/andefain/src/mocks/user/userMock.js
@@ -0,0 +1,10 @@
+export const users = [
+	{
+		name: 'John',
+		lastName: 'Doe',
+		email: 'test@gmail.com',
+		image:
+			'https://preview.keenthemes.com/metronic-v4/theme/assets/pages/media/profile/profile_user.jpg',
+		movies: []
+	}
+];
