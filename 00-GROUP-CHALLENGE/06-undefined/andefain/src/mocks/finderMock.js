759a840d97d22fd83d58828bf7fdd67c69e05296
alishpls <aleixsaar@gmail.com>
2020-08-10 14:42:16 +0200

    Mocks

---
+++ 00-GROUP-CHALLENGE/06-undefined/andefain/src/mocks/finderMock.js
@@ -0,0 +1,79 @@
+const finder = {
+	searchType: 'Movie',
+	expression: 'matrix',
+	results: [
+		{
+			id: 'tt0133093',
+			resultType: 'Title',
+			image:
+				'https://imdb-api.com/images/original/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_Ratio0.7273_AL_.jpg',
+			title: 'The Matrix',
+			description: '(1999)'
+		},
+		{
+			id: 'tt11749868',
+			resultType: 'Title',
+			image:
+				'https://imdb-api.com/images/original/MV5BNjQyNWZiM2UtOWYyYS00MmVmLWFhZGUtY2ExMDQxNDc5YTE0XkEyXkFqcGdeQXVyMTEzMjQzMDM1._V1_Ratio0.7273_AL_.jpg',
+			title: 'Matrix',
+			description: '(2020)'
+		},
+		{
+			id: 'tt0234215',
+			resultType: 'Title',
+			image:
+				'https://imdb-api.com/images/original/MV5BODE0MzZhZTgtYzkwYi00YmI5LThlZWYtOWRmNWE5ODk0NzMxXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_Ratio0.7273_AL_.jpg',
+			title: 'The Matrix Reloaded',
+			description: '(2003)'
+		},
+		{
+			id: 'tt0242653',
+			resultType: 'Title',
+			image:
+				'https://imdb-api.com/images/original/MV5BNzNlZTZjMDctZjYwNi00NzljLWIwN2QtZWZmYmJiYzQ0MTk2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_Ratio0.7273_AL_.jpg',
+			title: 'The Matrix Revolutions',
+			description: '(2003)'
+		},
+		{
+			id: 'tt0295432',
+			resultType: 'Title',
+			image:
+				'https://imdb-api.com/images/original/MV5BMTIzMTA4NDI4NF5BMl5BanBnXkFtZTYwNjg5Nzg4._V1_Ratio0.7273_AL_.jpg',
+			title: 'The Matrix Revisited',
+			description: '(2001) (Video)'
+		},
+		{
+			id: 'tt12093002',
+			resultType: 'Title',
+			image:
+				'https://imdb-api.com/images/original/MV5BYzM5OTJmODgtNGE5ZC00MTc2LWI5YzgtMDk3MmI3NjgwMjE0XkEyXkFqcGdeQXVyNzEyMzYxNDU@._V1_Ratio0.7727_AL_.jpg',
+			title: 'Matrix Isolation',
+			description: '(2020)'
+		},
+		{
+			id: 'tt0270841',
+			resultType: 'Title',
+			image:
+				'https://imdb-api.com/images/original/MV5BNzU2M2E1YmEtYjk4Yi00NTNiLWE0YTItNjA1MGNmNGI3MjcxXkEyXkFqcGdeQXVyMTY5Nzc4MDY@._V1_Ratio0.7273_AL_.jpg',
+			title: 'Cyber Wars',
+			description: '(2004) aka "Matrix Hunters: Kynigoi ston kyvernohoro"'
+		},
+		{
+			id: 'tt10838180',
+			resultType: 'Title',
+			image:
+				'https://imdb-api.com/images/original/MV5BN2I5NzlmMWYtYjIwYy00Y2ZiLWI0ODgtYjAxNDZiZGJlMjlhXkEyXkFqcGdeQXVyMzk1MDQ2MQ@@._V1_Ratio1.5000_AL_.jpg',
+			title: 'The Matrix 4',
+			description: '(2022)'
+		},
+		{
+			id: 'tt0328832',
+			resultType: 'Title',
+			image:
+				'https://imdb-api.com/images/original/MV5BZDEwMGYxNjEtNTE3ZC00Zjg3LThlODMtODVhNzBiMTQ3NGI5XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_Ratio0.7273_AL_.jpg',
+			title: 'The Animatrix',
+			description: '(2003) (Video)'
+		}
+	],
+	errorMessage: ''
+};
