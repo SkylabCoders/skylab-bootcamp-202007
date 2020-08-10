b36b131c7afe4e8e55bcd25a8c17e9ff4286e269
gemmas95 <gemma1030@gmail.com>
2020-08-10 13:33:58 +0200

    tests arranged

--- Gemma-Sanz/toh-react-gilbe/src/App.test.js
+++ Gemma-Sanz/toh-react-gilbe/src/App.test.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('should sum', () => {
+    console.log('Test!');
+    expect(2 + 2).toEqual(4);
+  });
+  it('should duplicate', () => {
+    console.log('Test!');
+    expect(2 * 2).toEqual(4);
+  });
+  it('async test 1', (done) => {
+    setTimeout(done, 100);
+  });
+  it('async test 2', () => {
+    return new Promise(
+      resolve => setTimeout(resolve, 100)
+    )
+  });
+  it('async test 3', async () => {
+    myPromise = new Promise(
+      resolve => setTimeout(resolve, 100)
+    );
+    return await myPromise;
+  });
 });
