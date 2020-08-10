19ea5c8c7857cc85aac30f8b6e5ee5cd6933ee7e
Alex <alexurola@gmail.com>
2020-08-10 16:39:50 +0200

    ToH React with Snapshop testing working

--- alex-larranaga/toh-react/src/App.test.js
+++ alex-larranaga/toh-react/src/App.test.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+describe('Index', () => {
+	//beforeEach(() => console.log('Before each!'));
+	it('should sum', () => {
+		console.log('Skipped test!');
+		expect(2 + 2).toEqual(4);
+	});
+	it('async test with DNE', (done) => {
+		console.log('async test with DNE');
+		setTimeout(done, 1000);
+	});
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+	it('asyn test with PROMISES', () => {
+		console.log('asyn test witch PROMISES');
+
+		return new Promise((resolve) => setTimeout(resolve, 3000));
+	});
+
+	it('asyn test with ASYNC AWAIT', async () => {
+		return await new Promise((resolve) => setTimeout(resolve, 3000));
+	});
+
+	//afterEach(() => console.log('After each!!'));
+	//afterAll(() => console.log('After all test!'));
 });
