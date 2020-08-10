f46ee9bd0e96feaed9fa610c3b2c763f737cb754
Fernando <fvaldivielso@outlook.com>
2020-08-10 16:11:32 +0200

    Create snapshots

--- fernando-valdivielso/toh-react/src/App.js
+++ fernando-valdivielso/toh-react/src/App.js
@@ -3,22 +3,25 @@ import './App.css';
 import HeroDetail from './components/HeroDetail';
 import Header from './components/Header';
 import HeroDashboard from './components/HeroDashboard';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import PageNotFound from './components/PageNotFound';
 import HeroesPage from './components/HeroesPage';
 import HeroList from './components/HeroList'
 
 function App(props) {
 	return (
+
 		<div className="container">
-			<Header />
-			<Switch>
-				<Route path="/" exact component={HeroDashboard} />
-				<Route path="/hero/:heroId" component={HeroDetail} />
-				<Route path="/hero" exact component={HeroList} />
-				<Route path="/heroes" component={HeroesPage} />
-				<Route component={PageNotFound} />
-			</Switch>
+			<BrowserRouter>
+				<Header />
+				<Switch>
+					<Route path="/" exact component={HeroDashboard} />
+					<Route path="/hero/:heroId" component={HeroDetail} />
+					<Route path="/hero" exact component={HeroList} />
+					<Route path="/heroes" component={HeroesPage} />
+					<Route component={PageNotFound} />
+				</Switch>
+			</BrowserRouter>
 		</div>
 	);
 }
--- fernando-valdivielso/toh-react/src/App.test.js
+++ fernando-valdivielso/toh-react/src/App.test.js
@@ -1,7 +1,7 @@
 describe('App', () => {
 
     beforeAll(() => {
-        console.log('Before All!');
+        // console.log('Before All!');
     });
     it('should sum', () => {
         expect(2 + 2).toEqual(4);
@@ -10,11 +10,28 @@ describe('App', () => {
         expect(2 * 2).toEqual(4);
     });
 
+    it('aync test 1', done => {
+        setTimeout(done, 100);
+    })
+
+    it('async test 2', () => {
+        return new Promise(
+            resolve => setTimeout(resolve, 100)
+        )
+    });
+
+    it('aync test 3', async () => {
+        const myPromise = new Promise((resolve) => setTimeout(resolve, 100));
+        return await myPromise;
+    });
+
+
+
     afterEach(() => {
-        console.log('After each!')
+        // console.log('After each!')
     })
 
     afterAll(() => {
-        console.log('After All!')
+        // console.log('After All!')
     })
 })
