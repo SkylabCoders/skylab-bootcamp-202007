8d922f210aba0674ce45c9bbcdc15a580cd786ec
Marti Olasz <martiolasz@gmail.com>
2020-08-10 13:17:20 +0200

    Add async test setup

--- marti-olasz/challenges/heroes/toh-react/src/App.test.js
+++ marti-olasz/challenges/heroes/toh-react/src/App.test.js
@@ -1,14 +1,28 @@
-describe('App', () => {
-	beforeAll(() => console.log('Before All'));
-	beforeEach(() => console.log('Before Each'));
+//Only run this scope with describe.only or fdescribe (it.only or fit)
+describe.only('App', () => {
+	//beforeAll(() => console.log('Before All'));
+	//beforeEach(() => console.log('Before Each'));
 
 	it('should sum', () => {
 		expect(2 + 2).toEqual(4);
 	});
-	it('should duplicated', () => {
+
+	//Skip test with xit or it.skip (describe.skip or fdescribe)
+	it.skip('should duplicated', () => {
 		expect(2 - 2).toEqual(0);
 	});
 
-	afterAll(() => console.log('After All'));
-	afterEach(() => console.log('After Each'));
+	it('async timeout', (done) => {
+		setTimeout(done, 100);
+	});
+
+	it('async promise', () => {
+		return new Promise((resolve) => setTimeout(resolve, 100));
+	});
+
+	it('async await', async () =>
+		await new Promise((resolve) => setTimeout(resolve, 100)));
+
+	//afterAll(() => console.log('After All'));
+	//afterEach(() => console.log('After Each'));
 });
