1496f585e535dee2af96583655944b9f5466a47b
Victor Alemany <victor.alemany@skylabcoders.com>
2020-08-10 16:41:05 +0200

    adding test features

--- victor-alemany/toh-react/src/App.js
+++ victor-alemany/toh-react/src/App.js
@@ -4,21 +4,23 @@ import HeroDetail from './components/HeroDetail';
 import Header from './components/Header';
 import HeroDashboard from './components/HeroDashboard';
 import HeroList from './components/HeroList';
-import { Route, Switch, Redirect } from 'react-router-dom';
-import PageNotFound from './components/PageNotFound'
+import { Route, Switch, Redirect, BrowserRouter as Router } from 'react-router-dom';
+import PageNotFound from './components/PageNotFound';
 
 function App(props) {
 	return (
-		<main className = "main-container">
-			<Header />
-			<Switch>
-			<Route path="/" exact component={HeroDashboard}/>
-			<Route path="/hero" exact component={HeroList}/>
-			<Route path="/hero/:heroId" component={HeroDetail} />
-			<Redirect from="/heroes" to="/hero" />
-			<Route component={PageNotFound} />
-			</Switch>
-		</main>
+		<Router>
+			<main className="main-container">
+				<Header />
+				<Switch>
+					<Route path="/" exact component={HeroDashboard} />
+					<Route path="/hero" exact component={HeroList} />
+					<Route path="/hero/:heroId" component={HeroDetail} />
+					<Redirect from="/heroes" to="/hero" />
+					<Route component={PageNotFound} />
+				</Switch>
+			</main>
+		</Router>
 	);
 }
 
