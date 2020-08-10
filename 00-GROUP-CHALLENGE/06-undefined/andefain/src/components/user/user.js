c624a973f23eb54d930e6108a872a99935b96f83
Vanesa Gómez <v.cucchiararo@gmail.com>
2020-08-10 08:49:56 +0200

    create profile user

--- 00-GROUP-CHALLENGE/06-undefined/andefain/src/components/user/user.js
+++ 00-GROUP-CHALLENGE/06-undefined/andefain/src/components/user/user.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { users } from '../../mocks/user/userMock';
+import userStore from '../../stores/userStore';
+import { listFavoriteFilms } from '../../actions/userActions';
 import './user.css';
 
 function User() {
+	const [favoriteMovie, setFavoriteMovie] = useState(
+		userStore.getFavoriteFilms()
+	);
+
+	useEffect(() => {
+		userStore.addChangeListener(onChange);
+		if (favoriteMovie === 0) listFavoriteFilms();
+		return () => userStore.removeChangeListener(onChange);
+	});
+
+	function onChange() {
+		setFavoriteMovie(userStore.getFavoriteFilms());
+	}
+
 	return (
 		<div className="user-container">
 			<section className="user-info">
 				<div className="user-image">
-					<img src={users[0].image} />
+					<img src={users[0].image} alt="User profile image" />
 				</div>
 				<div className="user-detail">
 					<p>{users[0].name.toUpperCase()}</p>
