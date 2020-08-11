2db8d3105898d505609a8a9e146dc0cc72013dbd
Jordi Muñoz <jordimm11@gmail.com>
2020-08-11 13:38:53 +0200

    complete testing in heroStore

--- jordi-munoz/toh-react-g/src/stores/heroStore.spec.js
+++ jordi-munoz/toh-react-g/src/stores/heroStore.spec.js
@@ -3,16 +3,36 @@ import { hasUncaughtExceptionCaptureCallback } from 'process';
 import actionTypes from '../actions/actionTypes';
 import dispatcher from '../appDispatcher';
 
+function reduceAction(action, data) {
+    return {
+        type: action,
+        data
+    };
+}
+
 describe('HeroStore', () => {
+    let callback;
+    let action;
+
+    beforeEach(() => {
+        callback = jest.fn();
+        heroStore.addChangeListener(callback);
+
+        action = {
+            type: actionTypes.LOAD_HEROES,
+            data: [{ id: 14, name: 'Celeritas' }]
+        };
+    });
+
+    afterEach(() => {
+        heroStore.removeChangeListener(callback);
+    });
+
     it('should create', () => {
         expect(heroStore).toBeDefined();
     });
 
     it('should register LOAD_HEROES', () => {
-        const action = {
-            type: actionTypes.LOAD_HEROES,
-            data: [{ id: 14, name: 'Celeritas' }]
-        };
 
         dispatcher.dispatch(action);
 
@@ -21,16 +41,9 @@ describe('HeroStore', () => {
 
     it('should register UPDATE_HERO', () => {
 
-        let action = {
-            type: actionTypes.LOAD_HEROES,
-            data: [{ id: 14, name: 'Celeritas' }]
-        };
         dispatcher.dispatch(action);
 
-        action = {
-            type: actionTypes.UPDATE_HERO,
-            data: { id: 14, name: 'updated Celeritas' }
-        };
+        action = reduceAction(actionTypes.UPDATE_HERO, { id: 14, name: 'updated Celeritas' });
         dispatcher.dispatch(action);
 
         const updateHero = heroStore.getHeroById(action.data.id);
@@ -38,22 +51,49 @@ describe('HeroStore', () => {
     });
 
     it('should register UPDATE_HERO with invalid ID', () => {
-        let action = {
-            type: actionTypes.LOAD_HEROES,
-            data: [{ id: 14, name: 'Celeritas' }]
-        };
 
         dispatcher.dispatch(action);
 
-        action = {
-            type: actionTypes.UPDATE_HERO,
-            data: { id: 25, name: 'Celeritas' }
-        };
-
+        action = reduceAction(actionTypes.UPDATE_HERO, { id: 25, name: 'Celeritas' });
         dispatcher.dispatch(action);
 
         const updateHero = heroStore.getHeroById(action.data.id);
         expect(updateHero).not.toBeDefined();
     });
+
+    it('should create a new hero', () => {
+        const name = 'Gilberto';
+
+        action = reduceAction(actionTypes.CREATE_HERO, { name });
+        dispatcher.dispatch(action);
+
+        const isSavedHero = heroStore
+            .getHeroes()
+            .some((hero) => hero.name === name);
+
+        expect(isSavedHero).toEqual(true);
+    });
+
+    it('should delete a hero', () => {
+        let id = 14;
+
+        action = reduceAction(actionTypes.DELETE_HERO, { id });
+        dispatcher.dispatch(action);
+
+        expect(heroStore.getHeroById(id)).not.toBeDefined();
+    });
+
+    it('should handle default case for action types', () => {
+        try {
+            dispatcher.dispatch({});
+            expect(heroStore).toBeFalsy();
+        } catch (error) {
+            const message = `The action type is unknown. action.type: undefined`;
+            expect(error).toEqual(message)
+        }
+    });
+
+
+
 
 });
