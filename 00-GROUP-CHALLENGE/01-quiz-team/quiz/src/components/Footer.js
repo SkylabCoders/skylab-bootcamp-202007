472d9fc66b8c5ba1c46c441ff427dc9598295b92
Victor Alemany <victor.alemany@skylabcoders.com>
2020-08-11 09:28:56 +0200

    fixing conflicts

--- 00-GROUP-CHALLENGE/01-quiz-team/quiz/src/components/Footer.js
+++ 00-GROUP-CHALLENGE/01-quiz-team/quiz/src/components/Footer.js
@@ -1,9 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
 import '../css/footer.css'
-=======
-import './../css/footer.scss'
->>>>>>> e41e8f20275d6a600397a1b46c45efaa1e0a88b9
 
 function Footer(){
     return (
