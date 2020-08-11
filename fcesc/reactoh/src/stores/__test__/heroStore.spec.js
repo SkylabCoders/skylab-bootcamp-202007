967bb444f0758593820ac540823c168f139ba979
fcesc-code <bpfran@gmail.com>
2020-08-11 14:58:11 +0200

    upgrade test

--- fcesc/reactoh/src/stores/__test__/heroStore.spec.js
+++ fcesc/reactoh/src/stores/__test__/heroStore.spec.js
@@ -2,6 +2,7 @@ import HeroStore from './../heroStore';
 import actionTypes from './../../actions/actionTypes';
 import { loadPaginatedHeroes, loadHeroById } from './../../actions/heroActions';
 import dispatcher from './../../AppDispatcher';
+import { EventEmitter } from 'events';
 
 describe('Herostore', () => {
     it('should get LOAD_HERO_BY_ID', () => {
@@ -84,6 +85,87 @@ describe('Herostore', () => {
 
         expect(heroData).toEqual(mockHero);
     })
+
+    /*
+    it('should get LOAD_HERO_BY_ID_2', () => {
+        const mockHero = {
+            "id": 620,
+            "name": "Spider-Man",
+            "slug": "620-spider-man",
+            "powerstats": {
+              "intelligence": 90,
+              "strength": 55,
+              "speed": 67,
+              "durability": 75,
+              "power": 74,
+              "combat": 85
+            },
+            "appearance": {
+              "gender": "Male",
+              "race": "Human",
+              "height": [
+                "5'10",
+                "178 cm"
+              ],
+              "weight": [
+                "165 lb",
+                "74 kg"
+              ],
+              "eyeColor": "Hazel",
+              "hairColor": "Brown"
+            },
+            "biography": {
+              "fullName": "Peter Parker",
+              "alterEgos": "No alter egos found.",
+              "aliases": [
+                "Spiderman",
+                "Bag-Man",
+                "Black Marvel",
+                "Captain Universe",
+                "Dusk",
+                "Green Hood",
+                "Hornet",
+                "Mad Dog 336",
+                "Peter Palmer",
+                "Prodigy",
+                "Ricochet",
+                "Scarlet Spider",
+                "Spider-Boy",
+                "Spider-Hulk",
+                "Spider-Morphosis"
+              ],
+              "placeOfBirth": "New York, New York",
+              "firstAppearance": "Amazing Fantasy #15",
+              "publisher": "Marvel Comics",
+              "alignment": "good"
+            },
+            "work": {
+              "occupation": "Freelance photographer, teacher",
+              "base": "New York, New York"
+            },
+            "connections": {
+              "groupAffiliation": "Member of the Avengers, formerly member of Outlaws, alternate Fantastic Four",
+              "relatives": "Richard Parker (father, deceased), Mary Parker(mother, deceased), Benjamin Parker (uncle, deceased), May Parker (aunt), Mary Jane Watson-Parker (wife), May Parker (daughter, allegedly deceased)"
+            },
+            "images": {
+              "xs": "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/xs/620-spider-man.jpg",
+              "sm": "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/sm/620-spider-man.jpg",
+              "md": "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/md/620-spider-man.jpg",
+              "lg": "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/lg/620-spider-man.jpg"
+            }
+          };
+        const someName = {
+            action: actionTypes.LOAD_HERO_BY_ID,
+            data: mockHero
+        }
+        dispatcher.dispatch(someName);
+        const heroData = HeroStore.getHeroById(mockHeroId);
+
+        console.log('AQUI DEL STORE', heroData);
+
+        expect(heroData).toEqual(mockHero);
+    })
+*/
 
     it('should get paginated hero list LOAD_PAGINATED_HERO_LIST', () => {
         const mockPaginatedList = [
@@ -207,6 +289,16 @@ describe('Herostore', () => {
         //console.log('DATA FROM STORE', dataFromStore);
 
         expect(dataFromStore).toEqual(mockPaginatedList);
+    })
+
+    it('should invoke a change listener callback', () => {
+        const callback = jest.fn();
+
+        HeroStore.addChangeListener.apply(callback);
+        HeroStore.emitChange();
+
+        expect(callback).toHaveBeenCalled();
+        expect(callback).toHaveBeenCalledTimes(1);
     })
 
 })
