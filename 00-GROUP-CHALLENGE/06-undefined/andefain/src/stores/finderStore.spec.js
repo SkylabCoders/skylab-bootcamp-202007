fcb146f14a634c7e48cf0b143a410484d115d9ea
alishpls <aleixsaar@gmail.com>
2020-08-11 18:05:15 +0200

    Finder store spec

--- 00-GROUP-CHALLENGE/06-undefined/andefain/src/stores/finderStore.spec.js
+++ 00-GROUP-CHALLENGE/06-undefined/andefain/src/stores/finderStore.spec.js
@@ -11,7 +11,20 @@ describe('FinderStore', () => {
 		FinderStore.addChangeListener(myCallbackMockFunction);
 	});
 
+	afterEach(() => {
+		FinderStore.removeChangeListener(myCallbackMockFunction);
+	});
 	it('should create', () => {
 		expect(FinderStore).toBeDefined();
+	});
+
+	it('should handle default case for action types', () => {
+		try {
+			dispatcher.dispatch({});
+			expect(FinderStore).toBeFalsy();
+		} catch (error) {
+			const message = `Unknown action type. action.type: undefined`;
+			expect(error).toEqual(message);
+		}
 	});
 });
