86756cd7e5687e2e44efd5da7b78593440d359ad
Victor Alemany <victor.alemany@skylabcoders.com>
2020-08-12 21:41:08 +0200

    adding last store test

--- 00-GROUP-CHALLENGE/01-quiz-team/quiz/src/stores/test/authStore.spec.js
+++ 00-GROUP-CHALLENGE/01-quiz-team/quiz/src/stores/test/authStore.spec.js
@@ -1,9 +1,57 @@
-import authStore from './../gameStore';
+import authStore from './../authStore';
+import actionTypes from '../../actions/actionTypes'
+import dispatcher from './../../dispatcher';
+
+let action;
+let actionSecondary;
+let sessionMockData;
+let mockCallback;
+
+function reduceAction (actionType, data){
+    return {
+        type: actionType,
+        data
+    }
+}
+
+beforeEach(()=>{
+    sessionMockData = {
+        isLogged: false,
+        userProfile: 'me@test.mail'
+    }
+
+
+    mockCallback = jest.fn();
+    authStore.addChangeListener(mockCallback);
+
+    action = reduceAction(actionTypes.LOGIN, [{
+        data: 'prueba'
+    }]);
+
+    actionSecondary = reduceAction(actionTypes.LOGOUT, [{
+        data: 'prueba'
+    }]);
+    
+    dispatcher.dispatch(action);
+    dispatcher.dispatch(actionSecondary);
+})
+
+afterEach(()=>{
+    authStore.removeChangeListener(mockCallback);
+})
 
 describe('authStore',()=>{
 
     it('should create',() =>{
         expect(authStore).toBeDefined();
+    })
+
+    it('should get user profile',()=>{
+        expect(authStore.getUserProfile()).toBeDefined();
+    })
+
+    it('should get if user is loged',()=>{
+        expect(authStore.isLogged()).toBeDefined();
     })
 
 })
--- 00-GROUP-CHALLENGE/01-quiz-team/quiz/src/stores/test/userStore.spec.js
+++ 00-GROUP-CHALLENGE/01-quiz-team/quiz/src/stores/test/userStore.spec.js
@@ -1,4 +1,46 @@
 import userStore from './../userStore';
+import actionTypes from '../../actions/actionTypes'
+import dispatcher from './../../dispatcher';
+
+let action;
+let actionSecondary;
+let sessionMockData;
+let mockCallback;
+
+function reduceAction (actionType, data){
+    return {
+        type: actionType,
+        data
+    }
+}
+
+beforeEach(()=>{
+    sessionMockData = {
+        isLogged: false,
+        userProfile: 'me@test.mail'
+    }
+
+
+    mockCallback = jest.fn();
+    userStore.addChangeListener(mockCallback);
+
+    action = reduceAction(actionTypes.GET_USER_DATA, [{
+        
+        data: 'prueba'
+    }]);
+
+    actionSecondary = reduceAction(actionTypes.UPDATE_USER_DATA, [{
+        
+        data: 'prueba'
+    }]);
+    
+    dispatcher.dispatch(action);
+    dispatcher.dispatch(actionSecondary);
+})
+
+afterEach(()=>{
+    userStore.removeChangeListener(mockCallback);
+})
 
 describe('userStore',()=>{
 
@@ -7,4 +49,8 @@ describe('userStore',()=>{
     })
 
 })
+
+
+
+
 
