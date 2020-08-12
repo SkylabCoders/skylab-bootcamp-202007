9c9f8a90cff9d3d504864f37b551e0c8e22e1b85
fcesc-code <bpfran@gmail.com>
2020-08-12 15:44:30 +0200

    improvement test authActions

--- 00-GROUP-CHALLENGE/01-quiz-team/quiz/src/actions/__test__/authActions.spec.js
+++ 00-GROUP-CHALLENGE/01-quiz-team/quiz/src/actions/__test__/authActions.spec.js
@@ -1,10 +1,13 @@
 import { login, loginGoogle, logout } from './../authActions';
 import * as MOCKUSER from './../../mockdata/User';
+import authStore from './../../stores/authStore';
 
 describe('Test set for authentication actions', ()=>{
-    test('Test login action from authentication actions, should not be undefined', ()=>{
+    beforeEach(()=>{
         const email = MOCKUSER.email;
         const password = MOCKUSER.password;
+    })
+    test('Test login action from authentication actions, should not be undefined', ()=>{
         expect(login(email, password)).toBeDefined();
     })
     test('Test loginGoogle action from authentication actions, should not be undefined', ()=>{
@@ -12,5 +15,9 @@ describe('Test set for authentication actions', ()=>{
     })
     test('Test logout action from authentication actions, should not be undefined', ()=>{
         expect(logout()).toBeDefined();
+    })
+    test('Test login action from authentication actions, send data to store', async ()=>{
+        await login(email, password);
+        expect(authStore.isLogged()).toBe(true);
     })
 })
