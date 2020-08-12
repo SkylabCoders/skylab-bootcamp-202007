de69db1dab4e06c1b59f7e3296f7c10034140a1c
gemmas95 <gemma1030@gmail.com>
2020-08-12 12:17:40 +0200

    working in sagaComponent.spec.js

---
+++ 00-GROUP-CHALLENGE/03-jgg/projectggj/src/components/sagaComponent/sagaComponent.spec.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import render from 'react-test-renderer';
+import SagaComponent from './sagaComponent';
+import { BrouserRouter } from 'react-router-dom';
+import { isMainThread } from 'worker_threads';
+
+function renderSagaComponent(arg) {
+
+};
+describe('SagaComponent', () => {
+    let sagaComponentTree;
+});
+it('should ')
