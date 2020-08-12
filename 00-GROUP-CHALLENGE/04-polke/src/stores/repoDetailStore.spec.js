628339110fdd107e288d9264f48f22e5a29224a1
Pablo <pablo_hortelano@hotmail.comm>
2020-08-12 08:58:29 +0200

    Testing changes

---
+++ 00-GROUP-CHALLENGE/04-polke/src/stores/repoDetailStore.spec.js
@@ -0,0 +1,100 @@
+import repoDetailStore from '../stores/repoDetailStore';
+import actionTypes from '../actions/actionTypes';
+/* import userDetailActions, { loadRepoList } from '../actions/userDetailActions'; */
+import repoDetailActions, {
+	loadGroupRepoInfo,
+	loadRankingRepoInfo
+} from '../actions/repoDetailActions';
+import dispatcher from '../appDispatcher';
+
+describe('RepoDetail Store', () => {
+	it('should create', () => {
+		expect(repoDetailStore).toBeDefined();
+	});
+
+	it('should register LOAD_GROUP', () => {
+		const action = {
+			type: actionTypes.LOAD_GROUP,
+			data: [
+				{
+					active: 'Inactive',
+					data: [
+						{
+							author: 'phortela1n',
+							description: 'This repo has a description.',
+							id: 1,
+							language: 'Lang. mix',
+							lastUpdate: '10/02/2020',
+							name: 'Dani',
+							userName: 'danio'
+						}
+					],
+					lastFourthWeekCommits: 0,
+					total: NaN,
+					weeksOfWorkLastYear: 0
+				}
+			]
+		};
+		dispatcher.dispatch(action);
+		expect(repoDetailStore.getGroupRepoInfo()).toBeDefined();
+	});
+	it('should register LOAD_RANKING', () => {
+		const action = {
+			type: actionTypes.LOAD_RANKING,
+			data: ''
+		};
+		dispatcher.dispatch(action);
+		expect(repoDetailStore.getRankingRepoInfo()).toBeDefined();
+	});
+	it('should register ', () => {
+		const action = {
+			type: actionTypes.LOAD_GROUP,
+			data: ''
+		};
+		dispatcher.dispatch(action);
+		expect(loadGroupRepoInfo().toBeUndefined());
+	});
+	/*
+	it('should register LOAD_USER_IMG', () => {
+		const action = {
+			type: actionTypes.LOAD_USER_IMG,
+			data: ''
+		};
+		dispatcher.dispatch(action);
+		expect(loadUserImg(githubUserName).toBeUndefined());
+	});
+	it('should create a new repo', () => {
+		const action = {
+			type: actionTypes.CREATE_REPO,
+			data: ''
+		};
+		dispatcher.dispatch(action);
+
+		expect(loadRepoList(githubUserName)).toBeUndefined();
+    });
+    */
+	it('should handle default case for action types', () => {
+		try {
+			dispatcher.dispatch({});
+			expect(repoDetailStore).toBeFalsy();
+		} catch (errorMessage) {
+			const message = 'Action type is unknow';
+			/* expect(errorMessage).toEqual(message); */
+		}
+	});
+
+	it('should subscribe to addChangeListener', () => {
+		const mockFunct = jest.fn();
+		repoDetailStore.addChangeListener(mockFunct);
+		repoDetailStore.emitChange();
+		expect(mockFunct).toHaveBeenCalled();
+	});
+
+	it('should unsubscribe from addChangeListener', () => {
+		const mockFunct = jest.fn();
+		repoDetailStore.addChangeListener(mockFunct);
+		repoDetailStore.emitChange();
+		repoDetailStore.removeChangeListener(mockFunct);
+		expect(mockFunct).toHaveBeenCalled();
+	});
+});
