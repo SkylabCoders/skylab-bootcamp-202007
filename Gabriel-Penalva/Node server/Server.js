63a5c73a226e83c001738c1348c07c6828bcbfa6
gaeremtro <gabripenalva42@gmail.com>
2020-08-13 12:17:30 +0200

    first server hello worl created on port 4242

---
+++ Gabriel-Penalva/Node server/Server.js
@@ -0,0 +1,4 @@
+const http = require('http');
+const server = http.createServer((request, response) => response.end('Hello World!'););
+const server = http.createServer((request, response) => response.end('Hello World!'))
+server.listen(4242, () => console.log('Server is running...'))
