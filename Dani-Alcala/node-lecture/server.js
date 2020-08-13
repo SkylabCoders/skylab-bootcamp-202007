1f07196cc5490b8c7c3a5ce9c2519d7d0624752e
Dani Alcala <daniamcode@gmail.com>
2020-08-13 20:30:40 +0200

    create first simple server with node

---
+++ Dani-Alcala/node-lecture/server.js
@@ -0,0 +1,7 @@
+const http = require('http');
+
+const server = http.createServer((request, response) =>
+	response.end('Hey there')
+);
+
+server.listen(4242, () => console.log('Server is running'));
