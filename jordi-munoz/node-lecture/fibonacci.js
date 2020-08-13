7d06e64dabeab5fead6b5bbd74911a8c7855065c
Jordi Muñoz <jordimm11@gmail.com>
2020-08-13 22:21:10 +0200

    class exercices

---
+++ jordi-munoz/node-lecture/fibonacci.js
@@ -0,0 +1,22 @@
+const { sign } = require("crypto");
+
+function customFib(signature, indexes, n) {
+    let add = 0;
+
+    while (signature.length <= n) {
+        for (let i = 0; i < indexes.length; i++) {
+            for (let j = 0; j < signature.length; j++) {
+                if (j === indexes[i]) {
+                    add = add + signature[j];
+                }
+            }
+
+        }
+        signature.push(add);
+    }
+
+
+
+    return signature[n];
+}
+customFib([7, 3, 4, 1], [1, 1], 6);
