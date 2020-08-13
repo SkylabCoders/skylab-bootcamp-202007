ec4f8d3e8233645d2dbb522b0134702cb71fdd75
Marti Olasz <martiolasz@gmail.com>
2020-08-13 13:11:29 +0200

    setTimeout example

--- marti-olasz/Node/server.js
+++ marti-olasz/Node/server.js
@@ -1,5 +1,7 @@
 const http = require('http');
 const server = http.createServer((request, response) =>
-  response.end('Hello World!')
+	response.end('Hello World at ' + new Date())
 );
-server.listen(4242, () => console.log('Server is running...'));
+server.listen('4242', '192.168.0.99', () =>
+	console.log('Server is running...')
+);
---
+++ marti-olasz/Node/setTimeout.js
@@ -0,0 +1,4 @@
+const customNumber = 4;
+setTimeout(() => {
+	console.log(`Hello after ${customNumber} seconds!`);
+}, customNumber * 1000);
