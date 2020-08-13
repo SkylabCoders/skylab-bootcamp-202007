d2046398ed790f0d8a6da909cbb8aff849d8dcc5
gaeremtro <gabripenalva42@gmail.com>
2020-08-13 12:18:20 +0200

    fix server : -Removed the ; fails

--- Gabriel-Penalva/Node server/Server.js
+++ Gabriel-Penalva/Node server/Server.js
@@ -1,4 +1,3 @@
 const http = require('http');
-const server = http.createServer((request, response) => response.end('Hello World!'););
-const server = http.createServer((request, response) => response.end('Hello World!'))
-server.listen(4242, () => console.log('Server is running...'))
+const server = http.createServer((request, response) => response.end('Hello World!'));
+server.listen(4242, () => console.log('Server is running...'));
