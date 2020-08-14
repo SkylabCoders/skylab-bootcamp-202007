db8113f76da2d7edf28e2e8ca2515ad1b864d613
Pablo <pablo_hortelano@hotmail.comm>
2020-08-14 16:25:11 +0200

    Changing server.js to use ejs

--- pablo-hortelano/node-lectures/scripts/server.js
+++ pablo-hortelano/node-lectures/scripts/server.js
@@ -2,13 +2,17 @@ const express = require('express');
 
 const server = express();
 
+server.set('view engine', 'ejs'); // Añadida con ejs
+
 server.get('/', (request, response) => {
-  response.send('Hello Expresss');
+  /* response.send('Hello Expresss'); */
+  response.render('index');
 });
 
 server.get('/about', (request, response) => {
   console.dir(request, { depth: 0 });
-  response.send('About works');
+  /* response.send('About works'); */
+  response.render('about');
 });
 
 server.listen(4200, () => {
