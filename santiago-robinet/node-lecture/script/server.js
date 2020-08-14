89026f0e64cd57f5d27327f77a9bfdbd473070f2
Santiago <santiagorobinet@gmail.com>
2020-08-14 17:06:17 +0200

    add EJS files

--- santiago-robinet/node-lecture/script/server.js
+++ santiago-robinet/node-lecture/script/server.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const server = express();
 
+server.set('view engine', 'ejs');// esto va siempre que usemos EJS
+
 server.get('/', (request, response) => {
-  response.send('Hello Express!!');
+  response.render('index');
 });
 
 server.get('/about', (request, response) => {
-  response.send('About Works.....!');
+  response.render('about');
 });
 
 server.listen(4200, () => console.log('Server running in port 4200...'));
