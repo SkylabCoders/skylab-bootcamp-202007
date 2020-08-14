e48efb1e9314e8d3e23a53f553fb8896afd29e90
Anna Codina <anna.co91@gmail.com>
2020-08-14 20:57:53 +0200

    Create scripts

--- anna-codina/node-lecture/custom-env-variables.js
+++
@@ -1,4 +0,0 @@
-console.log('Current user is ', process.env.USER);
-console.log('\n Script ejectued with: ');
-console.log('VAL1 equals to: ', process.env.VAL1);
-console.log('VAL2 equals to: ', process.env.VAL2);
