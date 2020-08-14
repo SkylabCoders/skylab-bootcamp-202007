d2defc24609b1d885df8dfa185a0f391ea0b59cf
Pablo <pablo_hortelano@hotmail.comm>
2020-08-14 16:11:46 +0200

    Finishing basic example express

--- pablo-hortelano/node-lectures/scripts/server.js
+++ pablo-hortelano/node-lectures/scripts/server.js
@@ -3,14 +3,14 @@ const express = require('express');
 const server = express();
 
 server.get('/', (request, response) => {
-	response.send('Hello Expresss');
+  response.send('Hello Expresss');
 });
 
 server.get('/about', (request, response) => {
-	console.dir(request, { depth: 0 });
-	response.send('About works');
+  console.dir(request, { depth: 0 });
+  response.send('About works');
 });
 
 server.listen(4200, () => {
-	console.log('Server running in port 4200');
+  console.log('Server running in port 4200');
 });
