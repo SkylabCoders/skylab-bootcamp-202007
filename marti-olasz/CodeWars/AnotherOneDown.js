5af587c1290589b464a977cc887af1c19bc1b424
Marti Olasz <martiolasz@gmail.com>
2020-08-15 15:03:25 +0200

    CodeWars + node lecture

--- marti-olasz/CodeWars/AnotherOneDown.js
+++ marti-olasz/CodeWars/AnotherOneDown.js
@@ -1,13 +1,15 @@
 function removeSmallest(n, arr) {
-	if (n > 0) {
-		result = [...arr]
+	if (n > 0 && n < arr.length - 1) {
+		const result = [...arr]
 			.sort(function (a, b) {
 				return a - b;
 			})
-			.slice(n);
-		return (difference = arr.filter((x) => result.includes(x)));
+			.slice(0, n);
+		const copy = [...arr];
+		return result.forEach((element) => {
+			copy.splice(copy.indexOf(element), 1);
+		});
 	} else {
 		return arr;
 	}
 }
-console.log(removeSmallest(2, [5, 3, 2, 1, 4]));
--- marti-olasz/Node/server.js
+++ marti-olasz/Node/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
-const server = http.createServer((request, response) =>
-	response.end('Hello World at ' + new Date())
-);
-server.listen('4242', '192.168.0.99', () =>
-	console.log('Server is running...')
-);
+const server = http.createServer((request, response) => {
+	response.write('Hello World at ' + new Date());
+	response.end();
+});
+
+server.listen('4242', () => console.log('Server is running...'));
