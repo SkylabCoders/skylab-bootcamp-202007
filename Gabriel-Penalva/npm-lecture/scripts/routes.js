572ff5e38053e059707ecc3f16639036ff0244d1
gaeremtro <gabripenalva42@gmail.com>
2020-08-15 18:56:58 +0200

    routes and server configuration

---
+++ Gabriel-Penalva/npm-lecture/scripts/routes.js
@@ -0,0 +1,11 @@
+const routes = [
+    {
+        file: 'index',
+        path: '/'
+    },
+    {
+        file: 'about',
+        path: '/about'
+    }
+];
+exports.routes = routes;
--- Gabriel-Penalva/npm-lecture/scripts/server.js
+++ Gabriel-Penalva/npm-lecture/scripts/server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const { request, response } = require('express');
-
+const { routes } = require('./routes')
 const server = express();
 server.set('view engine', 'ejs');
+//server.set('views', path.join(__dirname, './views'))
 
-server.get('/', (request, response) => {
-  response.render('index');
-});
-
-server.get('/about', (request, response) => {
-  response.render('about');
-});
+for (x of routes) {
+  generateRoutes(x)
+}
+function generateRoutes({ path, file }) {
+  server.get(path, (request, response) => {
+    response.render(file);
+  });
+}
 
 server.listen(4200, () => console.log('Server runing in port 4200 with express'));
