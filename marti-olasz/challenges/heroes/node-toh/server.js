ae860b5919df99236b78aac71e852837d59c09c8
Marti Olasz <martiolasz@gmail.com>
2020-08-15 15:17:05 +0200

    Create server file

---
+++ marti-olasz/challenges/heroes/node-toh/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const server = express();
+
+server.set('view engine', 'ejs');
+
+server.get('/', (request, response) => {
+	response.render('dashboard.ejs');
+});
+
+server.get('/list', (request, response) => {
+	response.render('list.ejs');
+});
+
+server.get('/hero', (request, response) => {
+	response.render('hero.ejs');
+});
+
+server.listen(2020, () => {
+	console.log('Server running...');
+});
