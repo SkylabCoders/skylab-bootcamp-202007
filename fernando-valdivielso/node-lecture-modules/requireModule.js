301730019c13364a33fbf8fe531fab18608b3fee
Fernando <fvaldivielso@outlook.com>
2020-08-17 10:54:34 +0200

    modules lecture

---
+++ fernando-valdivielso/node-lecture-modules/requireModule.js
@@ -0,0 +1,16 @@
+// const moduleApi = require('./index')
+
+// const { city, bootcamp, language } = require('./index')
+
+// console.log(moduleApi); 
+
+// console.log(city);
+// console.log(bootcamp);
+// console.log(language);
+
+// const { list } = require('./index')
+
+// console.log(list);
+
+const templateGenerator = require('./index');
+console.log(templateGenerator('mister', 'Fernando'));
