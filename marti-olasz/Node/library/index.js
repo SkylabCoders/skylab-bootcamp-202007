0c95f064e3b758ded78b7bbf808f7a48eb755d65
Marti Olasz <martiolasz@gmail.com>
2020-08-17 15:33:46 +0200

    Add bootstrap dependencies

---
+++ marti-olasz/Node/library/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const VIEW_FOLDER = 'src/views';
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, VIEW_FOLDER, 'index.html'));
+});
+
+app.get('/books', (req, res) => {
+	res.sendFile(path.join(__dirname, VIEW_FOLDER, 'books.html'));
+});
+
+const PORT = 2427;
+app.listen(PORT, () => console.log(`Server is running at port ${PORT}`));
