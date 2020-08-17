4f3b146aafe31bb1af271d181668ca69508c6764
Anna Codina <anna.co91@gmail.com>
2020-08-17 16:12:48 +0200

    Prepare dependencies

--- anna-codina/node-lecture/library-app/app.js
+++ anna-codina/node-lecture/library-app/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const VIEW_FOLDER = 'src/views';
+const debug = require('debug')('app');
+const chalk = require('chalk');
+const morgan = require('morgan');
 
 const app = express();
 
@@ -12,5 +15,20 @@ app.get('/books', function (req, res) {
 	res.sendFile(path.join(__dirname, VIEW_FOLDER, 'boocks.html'));
 });
 
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+	'/css',
+	express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css'))
+);
+app.use(
+	'/js',
+	express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js'))
+);
+app.use(
+	'/js',
+	express.static(path.join(__dirname, 'node_modules/jquery/dist'))
+);
+
 const PORT = 3000;
-app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
+app.listen(PORT, () => debug(`Server is running in port` + chalk.green(PORT)));
