7fc2aad47362b38a6b11707ea5a1308a97614e32
Dani Alonso <danialonso85@hotmail.es>
2020-08-17 17:18:18 +0200

    add

--- dani-Alonso/Node/library-app/app.js
+++ dani-Alonso/Node/library-app/app.js
@@ -1,18 +1,35 @@
 const express = require('express');
-const { response } = require('express');
 const path = require('path');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const chalk = require('chalk');
 
 const app = express();
 
+app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+	'/css',
+	express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css'))
+);
+app.use(
+	'/js',
+	express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js'))
+);
+app.use(
+	'/js',
+	express.static(path.join(__dirname, 'node_modules/jquery/dist'))
+);
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, 'src/views', 'index.html'));
 });
 
-const PORT = 3000;
-app.listen(3000, () => console.log('server is running in port' + PORT));
-
-app.get('/books', function (req, res) {
+app.get('/books', (req, res) => {
 	res.sendFile(path.join(__dirname, 'src/views', 'books.html'));
 });
+
+const PORT = 3000;
+app.listen(3000, () =>
+	debug(`server is running in port${chalk.greenBright(PORT)}`)
+);
