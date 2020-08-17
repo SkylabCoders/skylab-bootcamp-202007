7f4cc3c292ced759651b7d7a07fce81e99e9aa3f
Marti Olasz <martiolasz@gmail.com>
2020-08-17 16:10:26 +0200

    Prepare enviroment

--- marti-olasz/Node/library/index.js
+++ marti-olasz/Node/library/index.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const path = require('path');
+const debug = require('debug')('app');
+const chalk = require('chalk');
+const morgan = require('morgan');
 
 const app = express();
 
 const VIEW_FOLDER = 'src/views';
 
+app.use(morgan('tiny'));
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+	'/css',
+	express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css'))
+);
+app.use(
+	'/js',
+	express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js'))
+);
+app.use(
+	'/js',
+	express.static(path.join(__dirname, 'node_modules/jquery/dist/'))
+);
 
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, VIEW_FOLDER, 'index.html'));
@@ -16,4 +33,6 @@ app.get('/books', (req, res) => {
 });
 
 const PORT = 2427;
-app.listen(PORT, () => console.log(`Server is running at port ${PORT}`));
+app.listen(PORT, () =>
+	debug(chalk.red(`Server is running at port `) + chalk.green(PORT))
+);
