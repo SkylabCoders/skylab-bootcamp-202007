16d6261e4b3ec73fede966a59de2ebbcd57c1338
fcesc-code <bpfran@gmail.com>
2020-08-18 20:21:41 +0200

    crash

---
+++ fcesc/skylabers/config/DATABASE.js
@@ -0,0 +1,11 @@
+const db = {
+  user: "fran-jordi",
+  password: "Skylab202007",
+  server: "tohdb.database.windows.net",
+  databse: "tohdb",
+  option: {
+    encrypt: true
+  }
+}
+
+module.exports = db;
