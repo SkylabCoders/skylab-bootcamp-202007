38edd5216f2f45f77bc45deadac9df4ea6960bef
Dani Alcala <daniamcode@gmail.com>
2020-08-18 12:31:53 +0200

    toh with basic ejs, import list of heroes

--- Dani-Alcala/node-lecture/toh-express/index.js
+++ Dani-Alcala/node-lecture/toh-express/index.js
@@ -15,11 +15,23 @@ app.set('views', './src/views')
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.render('dashboard')
+    res.render('dashboard', {title: 'Top Heroes'})
 })
 
 app.get('/heroes', (req, res) => {
-    res.render('heroes')
+    res.render('heroes', {title: 'My Heroes', heroes: [
+        { id: 11, name: 'Dr Nice' },
+        { id: 12, name: 'Narco' },
+        { id: 13, name: 'Bombasto' },
+        { id: 14, name: 'Celeritas' },
+        { id: 15, name: 'Magneta' },
+        { id: 16, name: 'RubberMan' },
+        { id: 17, name: 'Dynama' },
+        { id: 18, name: 'Dr IQ' },
+        { id: 19, name: 'Magma' },
+        { id: 20, name: 'Tornado' }
+      ]
+    })
 })
 
 app.get('/heroes/:heroId', (req, res) => {
