03a8bb912b5c4a05dbf366d7578bbc3cc3dc3eb5
Dani Alcala <daniamcode@gmail.com>
2020-08-18 13:48:45 +0200

    add routes to toh-express

--- Dani-Alcala/node-lecture/toh-express/index.js
+++ Dani-Alcala/node-lecture/toh-express/index.js
@@ -1,41 +1,35 @@
-const express = require('express')
-const debug = require('debug')('app')
-const chalk = require('chalk')
-const morgan = require('morgan')
-const path = require('path')
+const express = require('express');
+const debug = require('debug')('app');
+const chalk = require('chalk');
+const morgan = require('morgan');
+const path = require('path');
+const heroes = require('./heroes');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(morgan('tiny'))
+app.use(morgan('tiny'));
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('views', './src/views')
-app.set('view engine', 'ejs')
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+const nav = [
+	{ link: '/', title: 'Dashboard' },
+	{ link: '/heroes', title: 'Heroes' }
+];
 
 app.get('/', (req, res) => {
-    res.render('dashboard', {title: 'Top Heroes'})
-})
+	res.render('dashboard', {
+		nav,
+		title: 'Top Heroes',
+		heroes: heroes.slice(0, 4)
+	});
+});
 
-app.get('/heroes', (req, res) => {
-    res.render('heroes', {title: 'My Heroes', heroes: [
-        { id: 11, name: 'Dr Nice' },
-        { id: 12, name: 'Narco' },
-        { id: 13, name: 'Bombasto' },
-        { id: 14, name: 'Celeritas' },
-        { id: 15, name: 'Magneta' },
-        { id: 16, name: 'RubberMan' },
-        { id: 17, name: 'Dynama' },
-        { id: 18, name: 'Dr IQ' },
-        { id: 19, name: 'Magma' },
-        { id: 20, name: 'Tornado' }
-      ]
-    })
-})
+const heroRoutes = require('./src/routes/heroRoutes')(nav, heroes);
 
-app.get('/heroes/:heroId', (req, res) => {
-    res.render('hero-detail')
-})
+app.use('/heroes', heroRoutes);
 
-app.listen(port, () => debug(`listening on port ${chalk.magenta(port)}`))
+app.listen(port, () => debug(`listening on port ${chalk.magenta(port)}`));
