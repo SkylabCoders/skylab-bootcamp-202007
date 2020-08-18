864469934643c42253a5713e6454c07bcc6ecd9c
Fernando <fvaldivielso@outlook.com>
2020-08-18 18:18:27 +0200

    toh finished

--- fernando-valdivielso/toh-express/index.js
+++ fernando-valdivielso/toh-express/index.js
@@ -1,15 +1,28 @@
-const express = require('express')
-const debug = require('debug')('app')
-const chalk = require('chalk')
-const morgan = require('morgan')
-const path = require('path')
+const express = require('express');
+const debug = require('debug')('app');
+const chalk = require('chalk');
+const morgan = require('morgan');
+const path = require('path');
+const sql = require('mssql');
 
 const heroes = require('./heroes');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(morgan('tiny'))
+app.use(morgan('tiny'));
+
+const config = {                        // database config
+    user: 'hydroFlask',
+    password: 'Skylab123',              // el password no se envia al cliente, porque no está en render
+    server: 'dfkylab.database.windows.net',
+    database: 'ToH-db',
+    option: {
+        encrypt: true  // because we are using MS Azure
+    }
+}
+
+sql.connect(config).catch(debug)
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', './src/views')
@@ -17,9 +30,7 @@ app.set('view engine', 'ejs')
 
 const nav = [
     { link: '/', title: 'Dashboard' },
-    { link: '/heroes', title: 'Heroes' },
-    // { link: '/heroes/?heroId', title: 'Details' }
-
+    { link: '/heroes', title: 'Heroes' }
 ]
 
 
@@ -30,8 +41,9 @@ app.get('/', (req, res) => {
         heroes: heroes.slice(0, 4)
     });
 })
-const heroRoutes = require('./src/routes/heroRoutes')(nav, heroes); // esta const apunta al resultado de la funcion router en heroRoutes.js pasandole los parametros nav y heroes
+const heroRoutes = require('./src/routes/heroRoutes')(nav); // esta const apunta al resultado de la funcion router en heroRoutes.js pasandole los parametros nav y heroes
 
 app.use('/heroes', heroRoutes)
 
 app.listen(port, () => debug(`listening on port ${chalk.magenta(port)}`))
+
--- fernando-valdivielso/toh-express/src/routes/heroRoutes.js
+++ fernando-valdivielso/toh-express/src/routes/heroRoutes.js
@@ -1,26 +1,44 @@
 const express = require('express');
+const debug = require('debug')('app:heroRoutes');
+const sql = require('mssql');
 
 const heroRoutes = express.Router();
-
-function router(nav, heroes) {
+function router(nav) {
     heroRoutes.route('/').get((req, res) => {
-        res.render('heroes', {
-            nav,
-            title: 'My Heroes',
-            heroes
+        (async function query() {
+            const request = new sql.Request();
+            try {
+                const { recordset } = await request.query('SELECT * FROM heroes');
+                res.render('heroes', {
+                    nav,
+                    title: 'My Heroes',
+                    heroes: recordset
+                });
+            } catch (error) {
+                debug(error.stack);
+            }
+        })();
+    });
+    heroRoutes.route('/:heroId')
+        .all((req, res, next) => {
+            const id = +req.params.heroId;
+            (async function query() {
+                try {
+                    const request = new sql.Request();
+                    const { recordset } = await request
+                        .input('id', sql.Int, id)
+                        .query(
+                            `SELECT * FROM heroes WHERE id= @id`);
+                    [res.hero] = recordset;
+                    next();
+                } catch (error) {
+                    debug(error.stack);
+                }
+            })();
         })
-    })
-
-    heroRoutes.route('/:heroId').get((req, res) => {
-        res.render('hero-detail', { nav, hero: heroes[0] })
-    })
-
+        .get((req, res) => {
+            res.render('hero-detail', { nav, hero: res.hero });
+        });
     return heroRoutes;
 }
-
 module.exports = router;
-
-
-
-
-
