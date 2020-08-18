9de871fa72c0c73067bf972732f9658c4e7e8a74
Pablo <pablo_hortelano@hotmail.comm>
2020-08-18 20:18:49 +0200

    Final version Pablo

--- esther-morillo/db-skylabers/src/routes/skylaberRoutes.js
+++ esther-morillo/db-skylabers/src/routes/skylaberRoutes.js
@@ -5,22 +5,45 @@ const sql = require('mssql');
 const skylaberRoutes = express.Router();
 
 function router(nav) {
-	skylaberRoutes.route('/').get((req, res) => {		
-		(async function query(){
+	skylaberRoutes.route('/').get((req, res) => {
+		(async function query() {
 			const request = new sql.Request();
 			try {
 				const { recordset } = await request.query('SELECT * FROM skylabers');
 				res.render('skylabers', {
 					nav,
 					title: 'Tour Skylabers',
-					heroes: recordset
+					skylabers: recordset
 				});
 			} catch (error) {
 				debug(error.stack);
 			}
-		}());		
+		})();
 	});
-
+	skylaberRoutes
+		.route('/:heroId')
+		.all((req, res, next) => {
+			const id = +req.params.heroId;
+			(async function query() {
+				try {
+					const request = new sql.Request();
+					const { recordset } = await request
+						.input('id', sql.Int, id)
+						.query(`SELECT * FROM skylabers WHERE id = ${id}`);
+					[res.skylaber] = recordset;
+					next();
+				} catch (error) {
+					/* debug(error.stack); */
+					console.log(error);
+				}
+			})();
+		})
+		.get((req, res) => {
+			res.render('skylaberDetail', {
+				nav,
+				skylaber: res.skylaber
+			});
+		});
 
 	return skylaberRoutes;
 }
