1b1f43820d5bac1c651c0b8162f426a13c63e57f
Marti Olasz <martiolasz@gmail.com>
2020-08-18 14:46:19 +0200

    fix routes

--- marti-olasz/Node/ToH/index.js
+++ marti-olasz/Node/ToH/index.js
@@ -4,10 +4,10 @@ const debug = require('debug')('app');
 const chalk = require('chalk');
 const morgan = require('morgan');
 
-const router = express.Router();
-
 const app = express();
 const port = process.env.PORT || 2427;
+
+const { heroList, nav } = require('./heroMock');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -15,38 +15,14 @@ app.set('views', './src/views');
 app.set('view engine', 'ejs');
 app.use(morgan('tiny'));
 
-const nav = [
-	{ link: '/', title: 'Dasboard' },
-	{ link: '/list', title: 'Dasboard' }
-];
-
-const heroList = [
-	{ id: 11, name: 'Dr Nice' },
-	{ id: 12, name: 'Narco' },
-	{ id: 13, name: 'Bombasto' },
-	{ id: 14, name: 'Celeritas' },
-	{ id: 15, name: 'Magneta' },
-	{ id: 16, name: 'RubberMan' },
-	{ id: 17, name: 'Dynama' },
-	{ id: 18, name: 'Dr IQ' },
-	{ id: 19, name: 'Magma' },
-	{ id: 20, name: 'Tornado' }
-];
-
 app.get('/', (req, res) => {
 	const dashboardList = heroList.slice(0, 4);
 	res.render('dashboard', { nav, dashboardList });
 });
 
-app.get('/list', (req, res) => {
-	res.render('list', { nav, heroList });
-});
+const heroRoutes = require('./src/routes/heroRoutes')(nav, heroList);
 
-app.get('/hero', (req, res) => {
-	const id = +req.query.id;
-	const hero = heroList.find((element) => element.id === id);
-	res.render('detail', { nav, hero });
-});
+app.use('/heroes', heroRoutes);
 
 app.listen(port, () =>
 	debug(chalk.red(`Server is running at port `) + chalk.green(port))
