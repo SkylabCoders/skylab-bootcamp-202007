f7d44d756e91fb7f46e32e580df11c2083fcc060
gemmas95 <gemma1030@gmail.com>
2020-08-18 13:59:09 +0200

    adding routes

--- Gemma-Sanz/node-lecture/toh-express-azure/index.js
+++ Gemma-Sanz/node-lecture/toh-express-azure/index.js
@@ -3,9 +3,15 @@ const debug = require('debug')('app');
 const chalk = require('chalk');
 const morgan = require('morgan');
 const path = require('path');
+const heroes = require('./heroes');
 
 const app = express();
 const port = 3000;
+
+const nav = [
+	{ link: '/', title: 'Dashboard' },
+	{ link: '/heroes', title: 'Heroes' }
+];
 
 app.use(morgan('tiny'));
 
@@ -16,15 +22,22 @@ app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-	res.render('dashboard');
+	res.render('dashboard', { title: 'Top Heroes' });
 });
 
 app.get('/heroes', (req, res) => {
-	res.render('heroes');
+	res.render('heroes', {
+		nav,
+		heroes: heroes[0]
+	});
 });
 
 app.get('/heroes/:heroId', (req, res) => {
 	res.render('heroDetail');
 });
+
+const heroRoutes = require('./src/routes/heroRoutes')(nav, heroes);
+
+app.use('/detail', heroRoutes);
 
 app.listen(port, () => debug(`Listener on port ${chalk.yellowBright(port)}`));
