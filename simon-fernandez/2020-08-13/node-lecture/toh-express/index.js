1dcbe7492d83263191636b4ef21d0086b57832e0
Simón Fernández Besteiro <simonbesteiro@gmail.com>
2020-08-18 11:54:11 +0200

    toh again

---
+++ simon-fernandez/2020-08-13/node-lecture/toh-express/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const debug = require('debug')('app');
+const chalk = require('chalk');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(morgan('tiny'));
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req, res) => {
+	res.render('dashboard');
+});
+
+app.get('/heroes', (req, res) => {
+	res.render('heroes');
+});
+
+app.get('/heroes/:heroId', (req, res) => {
+	res.render('details');
+});
+
+app.listen(port, () => {
+	debug(`server running in port: ${port}`);
+});
