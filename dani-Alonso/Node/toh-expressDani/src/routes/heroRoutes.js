3fbbaab86b188005d479f308dec13e7c997b3567
Dani Alonso <danialonso85@hotmail.es>
2020-08-18 18:52:05 +0200

    add reoutes

--- dani-Alonso/Node/toh-expressDani/src/routes/heroRoutes.js
+++ dani-Alonso/Node/toh-expressDani/src/routes/heroRoutes.js
@@ -21,25 +21,31 @@ function router(nav) {
 		})();
 	});
 
-	heroRoutes.route('/:heroId').get((req, res) => {
-		const id = +req.params.heroId;
+	heroRoutes
+		.route('/:heroId')
+		.all((req, res, next) => {
+			const id = +req.params.heroId;
 
-		(async function query() {
-			try {
-				const request = new sql.Request();
-				const { recordset } = await request.query(
-					`SELECT * from heroes WHERE id=${id}`
-				);
-				const [hero] = recordset;
-				res.render('hero-detail', {
-					nav,
-					hero
-				});
-			} catch (error) {
-				debug(error.stack);
-			}
-		})();
-	});
+			(async function query() {
+				try {
+					const request = new sql.Request();
+					const { recordset } = await request
+						.input('id', sql.Int, id)
+						.query(`SELECT * from heroes WHERE id=@id`);
+					[res.hero] = recordset;
+					next();
+				} catch (error) {
+					debug(error.stack);
+				}
+			})();
+		})
+
+		.get((req, res) => {
+			res.render('hero-detail', {
+				nav,
+				hero: res.hero
+			});
+		});
 
 	return heroRoutes;
 }
