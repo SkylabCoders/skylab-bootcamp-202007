93ed18f393a494f2849f651c75cca8d4b1a296b5
Gilberto Cao <gilbe.cao@gmail.com>
2020-08-18 11:56:39 +0200

    add server and templates

---
+++ gilbe-cao/node-lecture/toh-express/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const debug = require('debug')('app');
+const chalk = require('chalk');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(morgan('tiny'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+	res.render('dashboard');
+});
+
+app.get('/heroes', (req, res) => {
+	res.render('heroes');
+});
+
+app.get('/heroes/:heroId', (req, res) => {
+	res.render('hero-detail');
+});
+
+app.listen(port, () => debug(`Listening on port ${chalk.green(port)}`));
