0d5b9cf8bea20a2c759104feac5ae898967f99e0
Alex <alexurola@gmail.com>
2020-08-19 18:49:34 +0200

    delete implemented

--- alex-larranaga/week6/toh-express/src/routes/heroRoutes.js
+++ alex-larranaga/week6/toh-express/src/routes/heroRoutes.js
@@ -4,27 +4,54 @@ const { MongoClient, ObjectID } = require('mongodb');
 
 const heroRoutes = express.Router();
 function router(nav) {
-	heroRoutes.route('/').get((req, res) => {
-		const url = 'mongodb://localhost:27017';
-		const dbName = 'heroes';
-		let client = null;
-		(async function query() {
-			try {
-				client = await MongoClient.connect(url);
-				debug('Connection established...');
-				const db = await client.db(dbName);
-				const collection = await db.collection('heroes');
-				const heroes = await collection.find().toArray();
-				res.render('heroes', {
-					nav,
-					title: 'My Heroes',
-					heroes
-				});
-			} catch (error) {
-				debug(error.stack);
-			}
-		})();
-	});
+	heroRoutes
+		.route('/')
+		.get((req, res) => {
+			const url = 'mongodb://localhost:27017';
+			const dbName = 'heroes';
+			let client = null;
+			(async function query() {
+				try {
+					client = await MongoClient.connect(url);
+					debug('Connection established...');
+					const db = await client.db(dbName);
+					const collection = await db.collection('heroes');
+					const heroes = await collection.find().toArray();
+					res.render('heroes', {
+						nav,
+						title: 'My Heroes',
+						heroes
+					});
+				} catch (error) {
+					debug(error.stack);
+				}
+			})();
+		})
+		.post((req, res) => {
+			const url = 'mongodb://localhost:27017';
+			const dbName = 'heroes';
+			const { hero } = req.body;
+
+			let client = null;
+
+			(async function query() {
+				try {
+					client = await MongoClient.connect(url);
+					debug('Connection established...');
+					const db = await client.db(dbName);
+					const collection = await db.collection('heroes');
+					await collection.deleteOne({ _id: new ObjectID(hero) });
+					const heroes = await collection.find().toArray();
+					res.render('heroes', {
+						nav,
+						title: 'My Heroes',
+						heroes
+					});
+				} catch (error) {
+					debug(error.stack);
+				}
+			})();
+		});
 	heroRoutes
 		.route('/:heroId')
 		.all((req, res, next) => {
