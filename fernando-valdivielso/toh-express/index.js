9dc0800cd01545f951fdca121d2ff40743d34591
Fernando <fvaldivielso@outlook.com>
2020-08-19 15:48:01 +0200

    change sql to mongo

--- fernando-valdivielso/toh-express/index.js
+++ fernando-valdivielso/toh-express/index.js
@@ -3,26 +3,27 @@ const debug = require('debug')('app');
 const chalk = require('chalk');
 const morgan = require('morgan');
 const path = require('path');
-const sql = require('mssql');
+// const sql = require('mssql');
 
-const heroes = require('./heroes');
+const { MongoClient } = require('mongodb');
+
 
 const app = express();
 const port = 3000;
 
 app.use(morgan('tiny'));
 
-const config = {                        // database config
-    user: 'hydroFlask',
-    password: 'Skylab123',              // el password no se envia al cliente, porque no está en render
-    server: 'dfkylab.database.windows.net',
-    database: 'ToH-db',
-    option: {
-        encrypt: true  // because we are using MS Azure
-    }
-}
+// const config = {                        // database config
+//     user: 'hydroFlask',
+//     password: 'Skylab123',              // el password no se envia al cliente, porque no está en render
+//     server: 'dfkylab.database.windows.net',
+//     database: 'ToH-db',
+//     option: {
+//         encrypt: true  // because we are using MS Azure
+//     }
+// }
 
-sql.connect(config).catch(debug)
+// sql.connect(config).catch(debug)
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', './src/views')
@@ -35,15 +36,40 @@ const nav = [
 
 
 app.get('/', (req, res) => {
-    res.render('dashboard', {
-        nav,
-        title: 'My Heroes',
-        heroes: heroes.slice(0, 4)
-    });
-})
+    const url = 'mongodb://localhost:27017';
+    const dbName = 'shieldHeroes';
+    const collectionName = 'heroes'
+    let client;
+
+    try {
+        (async function mongo() {
+            client = await MongoClient.connect(url);
+            debug('Connection stablished... :)');
+
+            const db = client.db(dbName);
+
+            const collection = db.collection(collectionName);
+            const heroes = await collection.find().toArray();
+
+            res.render('dashboard', {
+                nav,
+                title: "Top Heroes",
+                heroes: heroes.slice(0, 4)
+            });
+            client.close();
+        }())
+    } catch (error) {
+        debug(error.stack);
+    }
+});
+
 const heroRoutes = require('./src/routes/heroRoutes')(nav); // esta const apunta al resultado de la funcion router en heroRoutes.js pasandole los parametros nav y heroes
 
 app.use('/heroes', heroRoutes)
+
+const shieldRoutes = require('./src/routes/shieldRoutes')(nav); // llama a la funcion router, con parametro 'nav' en shieldRoutes.js
+
+app.use('/shield', shieldRoutes);  // utiliza
 
 app.listen(port, () => debug(`listening on port ${chalk.magenta(port)}`))
 
