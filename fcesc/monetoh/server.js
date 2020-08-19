b98615c44cb894fa23b2aac50da3641540b1dc10
fcesc-code <bpfran@gmail.com>
2020-08-19 12:12:06 +0200

    starting monetoh

---
+++ fcesc/monetoh/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const debut = require('debut')('app:shieldRoutes');
+const { MongoClient } = require('mongodb');
+
+const shieldRoutes = express.Router();
+
+function router(nav){
+  shieldRoutes.route('/').get((req, res)=>{
+    res.send('Shield mola más que cualquier otra empresa de super heroes...');
+  })
+  return shieldRoutes;
+}
+
+module.exports = router;
