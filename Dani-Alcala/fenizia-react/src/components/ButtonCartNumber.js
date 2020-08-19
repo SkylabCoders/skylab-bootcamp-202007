df543e59a4867278d2e7d389238f8fd2b348ac32
Dani Alcala <daniamcode@gmail.com>
2020-08-20 00:26:32 +0200

    add fenizia to my folder

---
+++ Dani-Alcala/fenizia-react/src/components/ButtonCartNumber.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+const ButtonCartNumber = styled.ButtonCartNumber`
+    background-color: #ffcc00;
+    color: #fff;
+    width: 20px;
+    height: 20px;
+    border-radius: 10px;
+    font-size: .8rem;
+    text-align: center;
+    position: absolute;
+    top: 5px;
+    left: 5px;
+`
+
+export default ButtonCartNumber;
