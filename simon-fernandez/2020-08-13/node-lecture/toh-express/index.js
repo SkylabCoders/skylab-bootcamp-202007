d468039b3795585b23b2f74b0e577d32335d3793
Simón Fernández Besteiro <simonbesteiro@gmail.com>
2020-08-19 20:33:11 +0200

    apply delete and create with post

--- simon-fernandez/2020-08-13/node-lecture/toh-express/index.js
+++ simon-fernandez/2020-08-13/node-lecture/toh-express/index.js
@@ -3,24 +3,13 @@ const debug = require('debug')('app');
 const chalk = require('chalk');
 const morgan = require('morgan');
 const path = require('path');
-const sql = require('mssql');
+
 const { MongoClient } = require('mongodb');
+const bodyParser = require('body-parser');
 let heroes = require('./heroes');
 
 const app = express();
 const port = 3000;
-
-const config = {
-	user: 'simonbesteiro',
-	password: 'Simon.78951236',
-	server: 'skylibrary.database.windows.net',
-	database: 'tohdb',
-	option: {
-		encrypt: true // Because Azure force us
-	}
-};
-
-sql.connect(config).catch(debug);
 
 const nav = [
 	{ link: '/', title: 'Dashboard' },
@@ -29,37 +18,52 @@ const nav = [
 
 app.use(morgan('tiny'));
 
+app.use((request, response, next) => {
+	debug('***********************************************');
+	debug('Done by @simonbesteiro');
+	debug('***********************************************');
+
+	next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('views', './src/views');
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-	const url = 'mongodb://localhost:27017';
-	const dbName = 'shieldHeroes';
-	let client;
-	(async function query() {
-		try {
-			client = await MongoClient.connect(url);
-			debug('Connection established...');
+app
+	.route('/')
+	.all((req, res, next) => {
+		const url = 'mongodb://localhost:27017';
+		const dbName = 'shieldHeroes';
+		let client;
+		(async function query() {
+			try {
+				client = await MongoClient.connect(url);
+				debug('Connection established...');
 
-			const db = client.db(dbName);
+				const db = client.db(dbName);
 
-			const collection = await db.collection('heroes');
+				const collection = await db.collection('heroes');
 
-			heroes = await collection.find().limit(4).toArray();
-
-			res.render('dashboard', {
-				nav,
-				title: 'Top Heroes',
-				heroes
-			});
-		} catch (error) {
-			debug(error.stack);
-		}
-	})();
-});
+				heroes = await collection.find().limit(4).toArray();
+				next();
+			} catch (error) {
+				debug(error.stack);
+			}
+		})();
+	})
+	.get((req, res) => {
+		res.render('dashboard', {
+			nav,
+			title: 'Top Heroes',
+			heroes
+		});
+	});
 
 const heroRoutes = require('./src/routes/heroRoutes')(nav);
 
--- simon-fernandez/2020-08-13/node-lecture/toh-express/src/routes/heroRoutes.js
+++ simon-fernandez/2020-08-13/node-lecture/toh-express/src/routes/heroRoutes.js
@@ -5,31 +5,75 @@ const { MongoClient, ObjectID } = require('mongodb');
 const heroRoutes = express.Router();
 
 function router(nav) {
-	heroRoutes.route('/').get((req, res) => {
-		const url = 'mongodb://localhost:27017';
-		const dbName = 'shieldHeroes';
-		let client;
-		(async function query() {
-			try {
-				client = await MongoClient.connect(url);
-				debug('Connection established...');
+	heroRoutes
+		.route('/')
+		.get((req, res) => {
+			const url = 'mongodb://localhost:27017';
+			const dbName = 'shieldHeroes';
+			let client;
+			(async function query() {
+				try {
+					client = await MongoClient.connect(url);
+					debug('Connection established...');
 
-				const db = client.db(dbName);
+					const db = client.db(dbName);
 
-				const collection = await db.collection('heroes');
+					const collection = await db.collection('heroes');
 
-				const heroes = await collection.find().toArray();
+					const heroes = await collection.find().toArray();
 
-				res.render('heroes', {
-					nav,
-					title: 'My Heros',
-					heroes
-				});
-			} catch (error) {
-				debug(error.stack);
-			}
-		})();
-	});
+					res.render('heroes', {
+						nav,
+						title: 'My Heros',
+						heroes
+					});
+				} catch (error) {
+					debug(error.stack);
+				}
+			})();
+		})
+		.post((req, res) => {
+			const url = 'mongodb://localhost:27017';
+			const dbName = 'shieldHeroes';
+			const { deleteHero, createHero } = req.body;
+			let client;
+			(async function query() {
+				try {
+					client = await MongoClient.connect(url);
+					debug('Connection established...');
+
+					const db = client.db(dbName);
+
+					const collection = await db.collection('heroes');
+					if (createHero) {
+						const [{ id }] = await collection
+							.find()
+							.sort({ id: -1 })
+							.limit(1)
+							.toArray(); // for MAX
+
+						collection.insertOne({ id: id + 1, name: createHero });
+					}
+
+					if (deleteHero === 'all') {
+						await collection.deleteMany({});
+					} else {
+						await collection.deleteOne({
+							_id: new ObjectID(deleteHero)
+						});
+					}
+					const heroes = await collection.find().toArray();
+
+					res.render('heroes', {
+						nav,
+						title: 'My Heros',
+						heroes
+					});
+				} catch (error) {
+					debug(error.stack);
+				}
+			})();
+		});
 	heroRoutes
 		.route('/:heroId')
 		.all((req, res, next) => {
@@ -57,6 +101,35 @@ function router(nav) {
 			})();
 
 			debug(ID);
+		})
+		.post((req, res) => {
+			const updateQuery = { $set: req.body };
+			const filter = { _id: new ObjectID(req.params.heroId) };
+			const url = 'mongodb://localhost:27017';
+			const dbName = 'shieldHeroes';
+			const collectName = 'heroes';
+			let client;
+
+			(async function mongo() {
+				try {
+					client = await MongoClient.connect(url);
+
+					const db = client.db(dbName);
+
+					const collection = await db.collection(collectName);
+
+					await collection.updateOne(filter, updateQuery, (error, response) => {
+						if (error) {
+							throw error;
+						}
+						debug(response);
+						res.redirect('/heroes');
+					});
+				} catch (error) {
+					debug(error.stack);
+				}
+				client.close();
+			})();
 		})
 		.get((req, res) => {
 			res.render('heroDetail', {
