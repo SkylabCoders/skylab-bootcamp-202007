ff27a0075b3adae2a135f0d65e1a2b59992a1747
alishpls <aleixsaar@gmail.com>
2020-08-20 19:11:16 +0200

    Add skylabers.json, delete public copy

---
+++ 00-GROUP-CHALLENGE/06-undefined/andefain/src/components/finder/finder.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
+import { finderSearch } from '../../actions/finderActions';
+import finderStore from '../../stores/finderStore';
+import './finder.css';
+
+function Finder(title, name) {
+	const [finder, setFinder] = useState(finderStore.getFinder());
+
+	useEffect(() => {
+		finderStore.addChangeListener(onChange);
+		if (finder.length === 0) finderSearch();
+		return () => finderStore.removeChangeListener(onChange);
+	}, [finder.length]);
+
+	function onChange() {
+		setFinder(finderStore.getFinder());
+	}
+
+	return (
+		<div className="finder">
+			{finder.map((element) => {
+				return (
+					<div key={element.i} className="finder-result">
+						<img
+							className="finder-result__img"
+							src={!element.i ? '' : element.i.imageUrl}
+						/>
+						<div className="finder-result__details">
+							<div className="finder-result__name">{element.l}</div>
+							<div className="finder-result__year">{element.s}</div>
+						</div>
+					</div>
+				);
+			})}
+		</div>
+	);
+}
+
+export default Finder;
