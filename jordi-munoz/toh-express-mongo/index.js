600d7305e3254e053d15e0e554de2c29413001c9
Jordi Muñoz <jordimm11@gmail.com>
2020-08-20 17:27:05 +0200

    add local strategy

--- jordi-munoz/toh-express-mongo/index.js
+++ jordi-munoz/toh-express-mongo/index.js
@@ -3,7 +3,9 @@ const debug = require('debug')('app');
 const chalk = require('chalk');
 const morgan = require('morgan');
 const path = require('path');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
 
 const { MongoClient } = require('mongodb');
 
@@ -15,13 +17,19 @@ app.use(morgan('tiny'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(cookieParser());
+app.use(expressSession({ secret: 'heroes' }));
+
+require('./src/config/passport')(app);
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 const nav = [
 	{ link: '/', title: 'Dashboard' },
-	{ link: '/heroes', title: 'Heroes' }
+	{ link: '/heroes', title: 'Heroes' },
+	{ link: '/auth/signin', title: 'Signin' },
 ];
 
 
@@ -61,5 +69,9 @@ app.use('/heroes', heroRoutes);
 const shieldRoutes = require('./src/routes/shieldRoutes')(nav);
 
 app.use('/shield', shieldRoutes);
+
+const authRoutes = require('./src/routes/authRoutes')(nav);
+
+app.use('/auth', authRoutes);
 
 app.listen(port, () => debug(`Listening on port ${chalk.green(port)}`));
