6c58e5821743294444f132ead4da2e6d22abfec2
Ramon Saura <ramon.saura@gmail.com>
2020-08-20 23:56:55 +0200

    create mainRoutes and fix other routes

--- ramon-saura/toh-express/index.js
+++ ramon-saura/toh-express/index.js
@@ -3,66 +3,45 @@ const path = require('path');
 const debug = require('debug')('index');
 const chalk = require('chalk');
 const morgan = require('morgan');
-const sql = require('mssql');
-const { MongoClient } = require('mongodb');
+const bodyParser = require('body-parser');
+const coockieParser = require('cookie-parser');
+const expressSession = require('express-session');
 
 const index = express();
 const port = process.env.PORT || 3000;
 
-const config = {
-	user: 'mons',
-	password: 'Marina2016',
-	server: 'skylabmons.database.windows.net',
-	database: 'toh-database',
-	option: { encrypt: true /* because we are using Microsoft Aseure */ }
-};
-
-sql.connect(config).catch(debug);
-
 index.use(morgan('tiny'));
 index.use(express.static(path.join(__dirname, 'public')));
+
+index.use(bodyParser.json());
+index.use(bodyParser.urlencoded({ extended: false }));
+index.use(coockieParser());
+index.use(expressSession({ secret: 'heroes' }));
+require('./src/config/passport')(index);
 
 index.set('view engine', 'ejs');
 const nav = [
 	{ link: '/', title: 'Dashboard' },
-	{ link: '/heroes', title: 'Heroes' }
+	{ link: '/heroes', title: 'Heroes' },
+	{ link: '/auth', title: 'Login' }
 ];
 index.set('views', './src/views');
 
-index.get('/', (req, res) => {
-	const url = 'mongodb://localhost:27017';
-	const dbName = 'shieldHeroes';
-	let client;
-	(async function query() {
-		try {
-			client = await MongoClient.connect(url);
-			debug('Connection stablished...');
+const mainRoutes = require('./src/routes/mainRoutes')(nav);
 
-			const db = client.db(dbName);
-
-			const colection = await db.collection('heroes');
-
-			const heroesList = await colection.find().toArray();
-
-			res.render('dashboard', {
-				nav,
-				title: 'My Heros',
-				heroes: heroesList.slice(0, 4)
-			});
-		} catch (error) {
-			debug(error.stack);
-		}
-		client.close();
-	})();
-});
+index.use('/', mainRoutes);
 
 const heroRoutes = require('./src/routes/heroRoutes')(nav);
 
 index.use('/heroes', heroRoutes);
 
-const shieldRoutes = require('./src/routes/shieldRoutes')(nav);
+const shieldRoutes = require('./src/routes/shieldRoutes')();
 
 index.use('/shield', shieldRoutes);
+
+const authRoutes = require('./src/routes/authRoutes')(nav);
+
+index.use('/auth', authRoutes);
 
 index.listen(port, () =>
 	debug(`Server is running in port ${chalk.yellow(port)}`)
