8ae38f72f6d9cebf38d83c6eae510311391790ae
Gerard Ramon <gerardramonp@gmail.com>
2020-08-21 16:49:45 +0200

    this shit dont work

--- organic-market/group06/src/routes/appRoute.js
+++ organic-market/group06/src/routes/appRoute.js
@@ -2,6 +2,8 @@ const express = require('express');
 const debug = require('debug')('app:appRoutes');
 const { MongoClient, ObjectID } = require('mongodb');
 
+const MONGO = require('../../public/mongoConstants');
+
 const appRoute = express.Router();
 
 function router(nav) {
@@ -10,29 +12,69 @@ function router(nav) {
 	});
 
 	appRoute.route('/cart').get((res, req) => {
-		const { _id } = req.user;
-		(async function mongo() {
-			const client = MongoClient.connect(MONGO.url);
-			const db = client.db(MONGO.dbName);
-			const collection = db.collection(MONGO.usersCollection);
+		// const cart = [
+		// 	{
+		// 		_id: '5f3fc61e9a183552ccc8772d',
+		// 		title: 'Apple',
+		// 		rating: 4,
+		// 		price: 10
+		// 	},
+		// 	{
+		// 		_id: '5f3fc61e9a183552ccc87729',
+		// 		title: 'Phone',
+		// 		rating: 5,
+		// 		price: 250
+		// 	},
+		// 	{
+		// 		_id: '5f3fc61e9a183552ccc87727',
+		// 		title: 'Computer',
+		// 		rating: 4.5,
+		// 		price: 1200
+		// 	}
+		// ];
 
-			//const user = collection.find({ _id: new ObjectID(_id) });
+		res.render('cart', {
+			cart,
+			nav,
+			title: ''
+		});
 
-			const cart = [
-				{ _id: '5f3fc61e9a183552ccc8772d', quantity: 3 },
-				'5f3fc61e9a183552ccc87729',
-				'5f3fc61e9a183552ccc87727'
-			];
+		// const { _id } = req.user;
+		// (async function mongo() {
+		// 	try {
+		// 		const client = await MongoClient.connect(MONGO.url);
+		// 		const db = client.db(MONGO.dbName);
+		// 		const collection = await db.collection(MONGO.usersCollection);
+		// 		client.close();
+		// 		const user = collection.find({ _id: new ObjectID(_id) });
 
-			res.render('cart', {
-				cart,
-				nav,
-				title: ''
-			});
-		})();
+		// 	} catch (error) {
+		// 		debug(error.stack);
+		// 	}
+		// })();
 	});
 
 	return appRoute;
+}
+
+module.exports = router;
+
+
+
+const express = require('express');
+const debug = require('debug')('app:dbRoutes');
+const { MongoClient } = require('mongodb');
+const products = require('../../public/products.json');
+const MONGO = require('../../public/mongoConstants');
+
+const dbRoutes = express.Router();
+
+function router(nav) {
+	dbRoutes.route('/').get((req, res) => {
+		
+	}
+
+	return dbRoutes;
 }
 
 module.exports = router;
