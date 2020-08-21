a814a5d01691443735ff873e9831c8c068b807e8
gemmas95 <gemma1030@gmail.com>
2020-08-21 14:26:40 +0200

    added nav in index

--- organic-market/group03/index.js
+++ organic-market/group03/index.js
@@ -9,13 +9,20 @@ const app = express();
 
 const PORT = 3002;
 
+const nav = [
+	{ link: '/', title: 'Home' },
+	{ link: '/list', title: 'Recipies' },
+	{ link: '/detail', title: 'Details' },
+	{ link: '/signin', title: 'SignIn' }
+];
+
 app.use(morgan('tiny'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const recipesRoutes = require('./src/routes/recipeRoutes');
-app.use('/recipes', bookRouter);
+const recipesRoutes = require('./src/routes/recipesRoutes');
+app.use('/recipes', recipesRouter);
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
