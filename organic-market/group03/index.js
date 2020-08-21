c202bda4a1634aeb72ffeb31fc74bdda7469f608
Marti Olasz <martiolasz@gmail.com>
2020-08-21 15:13:20 +0200

    Resolve css import errors

--- organic-market/group03/index.js
+++ organic-market/group03/index.js
@@ -15,7 +15,7 @@ const nav = [
 	{ link: '/', title: 'Home' },
 	{ link: '/list', title: 'Recipies' },
 	{ link: '/detail', title: 'Details' },
-	{ link: '/signin', title: 'SignIn' }
+	{ link: '/auth/signin', title: 'SignIn' }
 ];
 
 app.use(morgan('tiny'));
