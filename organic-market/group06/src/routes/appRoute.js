5d265cd06d7041c2567f99891c39461644db6e43
Anna Codina <anna.co91@gmail.com>
2020-08-21 19:26:27 +0200

    create route all

--- organic-market/group06/src/routes/appRoute.js
+++ organic-market/group06/src/routes/appRoute.js
@@ -6,24 +6,29 @@ const MONGO = require('../../public/mongoConstants');
 const appRoute = express.Router();
 
 function router(nav) {
-	appRoute.route('/:productId').get((req, res) => {
-		let client;
-		const id = req.params.productId;
-		(async function query() {
-			try {
-				client = await MongoClient.connect(MONGO.url);
-				debug('Connection stablished...');
-				const db = client.db(MONGO.dbName);
-				const collection = db.collection(MONGO.itemsCollection);
-				let item = await collection.find({ _id: new ObjectID(id) }).toArray();
-				[item] = item;
-				res.render('detail', { nav, item: item });
-			} catch (error) {
-				debug(error.stack);
-			}
-			client.close();
-		})();
-	});
+	appRoute
+		.route('/:productId')
+		.all((req, res, next) => {
+			let client;
+			const id = req.params.productId;
+			(async function query() {
+				try {
+					client = await MongoClient.connect(MONGO.url);
+					debug('Connection stablished...');
+					const db = client.db(MONGO.dbName);
+					const collection = db.collection(MONGO.itemsCollection);
+					res.item = await collection.find({ _id: new ObjectID(id) }).toArray();
+					next();
+				} catch (error) {
+					debug(error.stack);
+				}
+				client.close();
+			})();
+		})
+		.get((req, res) => {
+			[item] = res.item;
+			res.render('detail', { nav, item: item });
+		});
 
 	return appRoute;
 }
