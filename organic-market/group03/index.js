f47f4697d2656c0edd57f37db4bf3e04b11163d8
gemmas95 <gemma1030@gmail.com>
2020-08-21 12:51:24 +0200

    base not working yet all

---
+++ organic-market/group03/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const debug = require('debug')('app');
+const chalk = require('chalk');
+const morgan = require('morgan');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const PORT = 3002;
+
+app.use(morgan('tiny'));
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded);
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+
+app.listen(PORT, () => debug(`Listening in port ${chalk.green(PORT)}...`));
