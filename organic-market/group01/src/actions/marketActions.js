590d2af02b3c96729bee0a7379d8e7e7c6cb0325
Alex <alexurola@gmail.com>
2020-08-21 16:07:33 +0200

    Added dbconf constant
    Added marketActions file
    
    added getAllProducts() function with embebbed dbconf

--- organic-market/group01/src/actions/marketActions.js
+++ organic-market/group01/src/actions/marketActions.js
@@ -3,3 +3,23 @@ const DBCONF = require('../dbConf');
 let cart = [];
 
 console.log(DBCONF);
+
+
+module.exports = function getAllProducts(){
+    (async function mongo() {
+        const allProducts;
+        try {
+            const client = await MongoClient.connect(DBCONF.url);
+            const db = client.db(DBCONF.dbName);
+            const collection = await db.collection(DBCONF.marketCall);
+             allProducts = await collection.find({});
+            
+        } catch (error) {
+            debug(error.stack)
+        }
+        return allProducts
+    
+})
+}
+
+
--- organic-market/group01/src/dbConf.js
+++ organic-market/group01/src/dbConf.js
@@ -1,8 +1,8 @@
-const DBCONFIG = {
+module.exports = const DBCONFIG = {
 	url: 'mongodb://localhost:27017',
 	dbName: 'market',
 	marketColl: 'market',
 	usersColl: 'users'
 };
 
-module.exports = DBCONFIG;
+
