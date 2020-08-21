49df70c3a3891534da8f9651837f2394c63d0d6b
Anna Codina <anna.co91@gmail.com>
2020-08-21 14:28:46 +0200

    Create server

---
+++ organic-market/group06/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const debug = require('debug')('app');
+const chalk = require('chalk');
+const morgan = require('morgan');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
+
+const { MongoClient } = require('mongodb');
+
+const app = express();
+const port = 3000;
+
+app.use(morgan('tiny'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(cookieParser());
+app.use(expressSession({ secret: 'heroes' }));
+
+require('./src/config/passport')(app);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+	res.send('Hi I works');
+});
+
+app.listen(port, () => debug(`Listening on port ${chalk.green(port)}`));
