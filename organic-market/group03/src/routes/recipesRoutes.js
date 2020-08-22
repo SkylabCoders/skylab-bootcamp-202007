94b19e83103802cf8b4bdbd1d9bda11d69a76569
Pablo <pablo_hortelano@hotmail.comm>
2020-08-22 12:51:49 +0200

    refactoring recipeRoutes

--- organic-market/group03/src/routes/recipesRoutes.js
+++ organic-market/group03/src/routes/recipesRoutes.js
@@ -58,6 +58,38 @@ function router(nav) {
 				}
 			})();
 		});
+	recipesRouter.route('/create').post((req, res) => {
+		const { deletedRecipe } = req.body;
+		const url = 'mongodb://localhost:27017';
+		const dbName = 'organicMarket';
+		const collectionName = 'recipes';
+		let client;
+		res.send('Inserting books');
+		(async function mongo() {
+			let client;
+			try {
+				/* client = await MongoClient.connect(url);
+					debug('Connect sucesfully');
+	
+					const db = client.db(dbName);
+	
+					const response = await db.collection('books').insertMany(books);
+					res.json(response); */
+				/*client = await MongoClient.connect(url);
+					debug('Connection to db established...');
+					const db = client.db(dbName);
+					const collection = db.collection(collectionName);
+					const { title } = req.body;
+					const filter = { title };
+					await collection.deleteOne(filter);
+					res.redirect('/list'); */
+			} catch (error) {
+				debug(error.stack);
+			}
+
+			client.close();
+		})();
+	});
 	recipesRouter.route('/:title').get((req, res) => {
 		const url = 'mongodb://localhost:27017';
 		const dbName = 'organicMarket';
@@ -94,28 +126,7 @@ function router(nav) {
 			}
 		})();
 	});
-	recipesRouter.route('/create').get((req, res) => {
-		const url = 'mongodb://localhost:27017';
-		const dbName = 'organicMarket';
 
-		(async function mongo() {
-			let client;
-			try {
-				/* client = await MongoClient.connect(url);
-				debug('Connect sucesfully');
-
-				const db = client.db(dbName);
-
-				const response = await db.collection('books').insertMany(books);
-				res.json(response); */
-				res.send('Inserting books');
-			} catch (error) {
-				debug(error.stack);
-			}
-
-			client.close();
-		})();
-	});
 	return recipesRouter;
 }
 
