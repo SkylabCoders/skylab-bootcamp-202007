4eb8dfb7abd9400b904cd965d8c85c170c1a85d4
alishpls <aleixsaar@gmail.com>
2020-08-22 11:00:44 +0200

    Fix auth routes

--- organic-market/group05/src/config/passport.js
+++ organic-market/group05/src/config/passport.js
@@ -1,17 +1,20 @@
 const passport = require('passport');
+
 require('./strategies/local.strategy');
 
 function passportConfig(app) {
-    app.use(passport.initialize());
-    app.use(passport.session());
-    //almacenar el usuario en la sesión 
-    passport.serializeUser((user, done) => {
-        done(null, user);
-    });
-    //y recuperar el usuario de la sesión
-    passport.deserializeUser((user, done) => {
-        done(null, user);
-    });
+	app.use(passport.initialize());
+	app.use(passport.session());
 
+	// almacenar el usuario en la sesión
+	passport.serializeUser((user, done) => {
+		done(null, user);
+	});
+
+	// y recuperar el usuario de la sesión
+	passport.deserializeUser((user, done) => {
+		done(null, user);
+	});
 }
+
 module.exports = passportConfig;
