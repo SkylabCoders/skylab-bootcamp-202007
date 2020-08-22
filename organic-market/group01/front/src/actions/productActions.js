a10ac878d2edbd329de53d9607fb33f4e457b220
Dani Alcala <daniamcode@gmail.com>
2020-08-22 19:14:48 +0200

    add post method to productlist

--- organic-market/group01/front/src/actions/productActions.js
+++ organic-market/group01/front/src/actions/productActions.js
@@ -1,14 +1,14 @@
-import productList from "../../src/product.mock";
-import dispatcher from "../appDispatcher";
-import actionTypes from "./actionTypes";
+import productList from '../../src/product.mock';
+import dispatcher from '../appDispatcher';
+import actionTypes from './actionTypes';
+import axios from 'axios'
+
 
 export function loadProducts() {
-  return new Promise((resolve) => {
-    resolve(productList);
-  }).then((product) => {
-    dispatcher.dispatch({
-      type: actionTypes.LOAD_PRODUCTS,
-      data: product,
-    });
-  });
+  axios.get('http://192.168.0.152:3000/market/productlist').then((product) => {
+		dispatcher.dispatch({
+			type: actionTypes.LOAD_PRODUCTS,
+			data: product
+		});
+	});
 }
--- organic-market/group01/front/src/components/ProductListItem.jsx
+++ organic-market/group01/front/src/components/ProductListItem.jsx
@@ -1,46 +1,64 @@
-import React,  {useState} from "react";
-import { Link } from "react-router-dom";
-import "./ProductListItem.css";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './ProductListItem.css';
+import productList from '../../src/product.mock';
 
+function ProductListItem({
+	title,
+	id,
+	type,
+	description,
+	price,
+	cover,
+	rating
+}) {
+	// const [isLogged, setIsLogged] = useState(authStore.isLogged());
+	// function addToCart(id) {
+	//   cartStore.addCartProduct(id);
+	//   addNumberCart();
+	// }
 
-function ProductListItem({ title, id, type,  description, price, cover, rating }) {
-  // const [isLogged, setIsLogged] = useState(authStore.isLogged());
-  // function addToCart(id) {
-  //   cartStore.addCartProduct(id);
-  //   addNumberCart();
-  // }
+	const product = productList;
 
-
-  return (
-    <div className="main-box">
-      <img className="box__image" src={cover} alt={title} />
-      <div className="box__contain">
-        <div className="box__text">
-          <h3 className="box__title">{title}</h3>
-          {/* <p className="box__author">{author}</p> */}
-          <p className="box__sinopsis">Description</p>
-          <p className="box__description">{description}</p>
-        </div>
-        <div className="box__button">
-          <div className="box__complet-price">
-            <p className="box__number-price">
-              {price}
-              <span>€</span>
-            </p>
-            <p className="box__vat">VAT INCLUDED</p>
-          </div>
-          <div className="box__column-button">
-            <p className="box__button-cart">
-              Add to cart
-            </p>
-            <p className="box__button-star">
-              Add to favorites
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="main-box">
+			<img className="box__image" src={cover} alt={title} />
+			<div className="box__contain">
+				<div className="box__text">
+					<h3 className="box__title">{title}</h3>
+					<p className="box__sinopsis">Description</p>
+					<p className="box__description">{description}</p>
+				</div>
+				<div className="box__button">
+					<div className="box__complet-price">
+						<p className="box__number-price">
+							{price}
+							<span>€</span>
+						</p>
+						<p className="box__vat">VAT INCLUDED</p>
+					</div>
+					<div className="box__column-button">
+						<form
+							action="http://192.168.0.152:3001/cart/add/"
+							method="POST"
+							name="productsForm"
+						>
+							<button
+								className="box__button-cart"
+								type="submit"
+								name="productId"
+								value="2"
+								title="add product"
+							>
+								Add to cart
+							</button>
+						</form>
+						<p className="box__button-star">Add to favorites</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default ProductListItem;
