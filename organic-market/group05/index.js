5781f3a5b5695a62779da78066f0c3f74b54beb1
alishpls <aleixsaar@gmail.com>
2020-08-23 18:42:40 +0200

    Add to cart from home and products

--- organic-market/group05/index.js
+++ organic-market/group05/index.js
@@ -62,32 +62,33 @@ app
 		})();
 	})
 	.post('/', (req, res) => {
-		const { addtocart } = req.body;
-		debug('-- addtocart value from POST method -->', addtocart);
-		const { _id } = req.user;
-		const url =
-			'mongodb+srv://admin:admin1234@cluster0.rpj2g.mongodb.net/organics?retryWrites=true&w=majority';
-		const dbName = 'organics';
-		const collectionName = 'carts';
-		let client;
-		(async function mongo() {
-			try {
-				client = await MongoClient.connect(url);
-				const db = client.db(dbName);
-				const collection = db.collection(collectionName);
-				const result = await collection.findOne({ userid: ObjectId(_id) });
-				debug('-- result -->', result);
-				const filter = { userid: ObjectId(_id) };
-				debug('-- filter -->', filter);
+		if (req.user) {
+			const { addtocart } = req.body;
+			debug('-- addtocart value from POST method -->', addtocart);
+			const { _id } = req.user;
+			const url =
+				'mongodb+srv://admin:admin1234@cluster0.rpj2g.mongodb.net/organics?retryWrites=true&w=majority';
+			const dbName = 'organics';
+			const collectionName = 'carts';
+			let client;
+			(async function mongo() {
+				try {
+					client = await MongoClient.connect(url);
+					const db = client.db(dbName);
+					const collection = db.collection(collectionName);
+					const result = await collection.findOne({ userid: ObjectId(_id) });
+					debug('-- result -->', result);
+					const filter = { userid: ObjectId(_id) };
+					debug('-- filter -->', filter);
 
-				// CART NOT EXISTS -> CREATE CART
-				const createCart = {
-					userid: ObjectId(_id),
-					status: 'open',
-					products: [{ productid: addtocart, qty: 1 }]
-				};
+					// CART NOT EXISTS -> CREATE CART
+					const createCart = {
+						userid: ObjectId(_id),
+						status: 'open',
+						products: [{ productid: addtocart, qty: 1 }]
+					};
 
-				/* const { qty } = result.products[0].qty;
+					/* const { qty } = result.products[0].qty;
 				debug('qty', qty);
 				const quantity = Number(qty + 1);
 				debug('quantity', quantity);
@@ -97,25 +98,28 @@ app
 				};
 				debug('-- increase qty -->', increaseQty); */
 
-				// CART EXISTS BUT PRODUCT NOT IN CART -> PUSH TO CART
-				const pushToCart = {
-					$push: { products: { productid: addtocart, qty: 1 } }
-				};
+					// CART EXISTS BUT PRODUCT NOT IN CART -> PUSH TO CART
+					const pushToCart = {
+						$push: { products: { productid: addtocart, qty: 1 } }
+					};
 
-				if (result) {
-					// CART EXISTS -> PUSH TO CART
-					debug('-- push to cart -->', pushToCart);
-					await collection.updateOne(filter, pushToCart);
-				} else {
-					// CART NOT EXISTS -> CREATE CART
-					debug('-- Go to create cart -->', createCart);
-					await collection.insertOne(createCart);
+					if (result) {
+						// CART EXISTS -> PUSH TO CART
+						debug('-- push to cart -->', pushToCart);
+						await collection.updateOne(filter, pushToCart);
+					} else {
+						// CART NOT EXISTS -> CREATE CART
+						debug('-- Go to create cart -->', createCart);
+						await collection.insertOne(createCart);
+					}
+					res.redirect('/');
+				} catch (error) {
+					debug(error.stack);
 				}
-				res.redirect('/');
-			} catch (error) {
-				debug(error.stack);
-			}
-		})();
+			})();
+		} else {
+			res.redirect('/auth/signin');
+		}
 	});
 
 const mongoRoutes = require('./src/routes/mongoRoutes');
@@ -130,11 +134,11 @@ const findRoutes = require('./src/routes/findRoutes')(nav);
 
 app.use('/find', findRoutes);
 
-const authRoutes = require('./src/routes/authroutes')(nav);
+const authRoutes = require('./src/routes/authRoutes')(nav);
 
 app.use('/auth', authRoutes);
 
-const cartRoutes = require('./src/routes/cartroutes')(nav);
+const cartRoutes = require('./src/routes/cartRoutes')(nav);
 
 app.use('/cart', cartRoutes);
 
