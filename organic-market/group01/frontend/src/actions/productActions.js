6439679fdb14f92f802d917a22b98757bae2b93a
Gilberto Cao <gilbe.cao@gmail.com>
2020-08-23 11:32:00 +0200

    hotfix split frontend and backend apps

--- organic-market/group01/frontend/src/actions/productActions.js
+++ organic-market/group01/frontend/src/actions/productActions.js
@@ -1,11 +1,9 @@
-import productList from '../../src/product.mock';
 import dispatcher from '../appDispatcher';
 import actionTypes from './actionTypes';
-import axios from 'axios'
-
+import axios from 'axios';
 
 export function loadProducts() {
-  axios.get('http://192.168.0.152:3000/market/productlist').then((product) => {
+	axios.get('http://192.168.0.152:3000/market/productlist').then((product) => {
 		dispatcher.dispatch({
 			type: actionTypes.LOAD_PRODUCTS,
 			data: product
