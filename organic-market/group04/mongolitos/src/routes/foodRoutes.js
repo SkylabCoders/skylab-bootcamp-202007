c9f94413126ee0bdbca20933e854120806a00a54
Vanesa Gómez <v.cucchiararo@gmail.com>
2020-08-23 16:57:56 +0200

    adminroutes

--- organic-market/group04/mongolitos/src/routes/foodRoutes.js
+++ organic-market/group04/mongolitos/src/routes/foodRoutes.js
@@ -15,29 +15,31 @@ function router(nav) {
 		 	}
 		}) */
 		.get((req, res) => {
-            const url = 'mongodb+srv://admin:1234Abcd!@cluster0.vdzqh.mongodb.net/mongoProducts?retryWrites=true&w=majority';
-            const dbName = 'mongoProducts';
-            const collectionName = 'products';
-            let client;
-            (async function mongo() {
-                try {
-                    client = await MongoClient.connect(url);
-                    const db = client.db(dbName);
-                    const collection = db.collection(collectionName);
-                    const products = await collection.find().toArray();
-                    res.render('foodList', { nav, products });
-                } catch (error) {
-                    debug(error.stack);
-                }
-                client.close();
-            })();
-        });
+			const url =
+				'mongodb+srv://admin:1234Abcd!@cluster0.vdzqh.mongodb.net/mongoProducts?retryWrites=true&w=majority';
+			const dbName = 'mongoProducts';
+			const collectionName = 'products';
+			let client;
+			(async function mongo() {
+				try {
+					client = await MongoClient.connect(url);
+					const db = client.db(dbName);
+					const collection = db.collection(collectionName);
+					const products = await collection.find().toArray();
+					res.render('foodList', { nav, products });
+				} catch (error) {
+					debug(error.stack);
+				}
+				client.close();
+			})();
+		});
 
 	foodRoutes
 		.route('/:productId')
 		.all((req, res, next) => {
 			const id = req.params.productId;
-			const url = 'mongodb+srv://admin:1234Abcd!@cluster0.vdzqh.mongodb.net/mongoProducts?retryWrites=true&w=majority';
+			const url =
+				'mongodb+srv://admin:1234Abcd!@cluster0.vdzqh.mongodb.net/mongoProducts?retryWrites=true&w=majority';
 			const dbName = 'mongoProducts';
 			const collectionName = 'products';
 			let client;
@@ -66,7 +68,8 @@ function router(nav) {
 			});
 		})
 		.post((req, res) => {
-			const url = 'mongodb+srv://admin:1234Abcd!@cluster0.vdzqh.mongodb.net/mongoProducts?retryWrites=true&w=majority';
+			const url =
+				'mongodb+srv://admin:1234Abcd!@cluster0.vdzqh.mongodb.net/mongoProducts?retryWrites=true&w=majority';
 			const dbName = 'mongoProducts';
 			const userCollection = 'users';
 			const productCollection = 'products';
@@ -99,14 +102,15 @@ function router(nav) {
 				}
 
 				client.close();
-			}())
+			})();
 		});
 
-		foodRoutes
+	foodRoutes
 		.route('/search')
 		.all((req, res, next) => {
 			const id = req.params.productId;
-			const url = 'mongodb+srv://admin:1234Abcd!@cluster0.vdzqh.mongodb.net/mongoProducts?retryWrites=true&w=majority';
+			const url =
+				'mongodb+srv://admin:1234Abcd!@cluster0.vdzqh.mongodb.net/mongoProducts?retryWrites=true&w=majority';
 			const dbName = 'mongoProducts';
 			const collectionName = 'products';
 			let client;
@@ -129,16 +133,13 @@ function router(nav) {
 		})
 
 		.get((req, res) => {
-			if(req.query.search) {
+			if (req.query.search) {
 				// const [ search ] = req.query.search;
-				debug('====>',req.query.search)
+				debug('====>', req.query.search);
 			}
-				
-		})		
-		
-		return foodRoutes;
+		});
+
+	return foodRoutes;
 }
 
 module.exports = router;
-
-
