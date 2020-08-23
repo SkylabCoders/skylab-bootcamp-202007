f06ffc33e30f846989c73d77dcaf2a61a91aeec7
Dani Alonso <danialonso85@hotmail.es>
2020-08-23 17:34:46 +0200

    Add admin creating list item

--- organic-market/group06/src/routes/adminRoutes.js
+++ organic-market/group06/src/routes/adminRoutes.js
@@ -8,8 +8,58 @@ const MONGO = require('../../public/mongoConstants');
 
 const adminRoute = express.Router();
 
-function router() {
-	adminRoute.route('/addProduct').get((req, res) => {});
+function router(nav) {
+	adminRoute
+		.route('/addProduct')
+
+		.all((req, res, next) => {
+			debug(req.user.admin);
+			if (req.user.admin === true) {
+				next();
+			} else {
+				res.redirect('/user/list');
+			}
+		})
+		.get((req, res) => {
+			res.render('addProduct', { nav });
+		})
+		.post((req, res) => {
+			debug(req.body);
+			// Obtenir info necesaria del req.body
+			const { title, type, description, price, rating } = req.body;
+
+			// Fem que guardi a la ddbb
+			//  1-Crear la conexio (client)
+			//  2- Obtenir la DB
+			//  3- Obtenir la collection
+			//  4- fer la consulta
+			let client = null;
+
+			(async function mongo() {
+				try {
+					debug('before.....');
+					client = await MongoClient.connect(MONGO.url);
+
+					debug('afeter.........');
+					const db = client.db(MONGO.dbName);
+					const collection = db.collection(MONGO.itemsCollection);
+
+					const response = await collection.insertOne({
+						title,
+						type,
+						description,
+						height: 0,
+						width: 0,
+						price,
+						rating
+					});
+				} catch (error) {
+					debug(error.stack);
+				}
+				client.close();
+			})();
+			res.redirect('/admin/addProduct');
+		});
 
 	return adminRoute;
 }
