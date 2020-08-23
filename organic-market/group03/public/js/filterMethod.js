2ebcc4c1c96e98d481de77d7657877d7736a94af
Pablo <pablo_hortelano@hotmail.comm>
2020-08-23 20:07:17 +0200

    Filter working

--- organic-market/group03/public/js/filterMethod.js
+++ organic-market/group03/public/js/filterMethod.js
@@ -1,15 +1,25 @@
-function changeArray() {
-	let acceptedword = /pear/;
+let input = document.querySelector('.selectRecipes');
+
+function changeArray(e) {
+	let value = e.target.value;
+	var acceptedword = new RegExp(value);
+
 	const recipesContainer = document.querySelector('.heroes');
 	const lisRecipes = recipesContainer.querySelectorAll('li');
 	const lisRecipesArray = Array.from(lisRecipes);
 	const lisRecipesArrayFiltered = lisRecipesArray.filter(
 		(elem) => acceptedword.test(elem.innerText.toLowerCase()) === true
 	);
-	console.log(lisRecipesArrayFiltered);
-	for (let liRecipe of lisRecipes) {
-		/* console.log(liRecipe.innerHTML); */
+
+	const newLiContainer = document.createElement('ul');
+	recipesContainer.innerHTML = '';
+	for (let liRecipe of lisRecipesArrayFiltered) {
+		const newLi = document.createElement('li');
+		const liContent = liRecipe.innerHTML;
+		const definiiveLi = (newLi.innerHTML = liContent);
+		recipesContainer.appendChild(newLi);
 	}
 }
 
-document.addEventListener('click', changeArray);
+input.addEventListener('input', changeArray);
+input.addEventListener('input', changeArray);
