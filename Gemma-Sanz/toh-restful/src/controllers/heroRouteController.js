1483ddcd1fe032b4d4aa2f2843339112a788adc7
gemmas95 <gemma1030@gmail.com>
2020-08-24 18:59:49 +0200

    added heroRouteController

---
+++ Gemma-Sanz/toh-restful/src/controllers/heroRouteController.js
@@ -0,0 +1,22 @@
+const Hero = require('../models/heroModel');
+
+const get = (req, res) => {
+	const query = {};
+	if (req.query.id) {
+		query.id = req.query.id;
+	}
+	Hero.find(query, (error, heroes) => {
+		if (error) {
+			res.send(error);
+		}
+		res.json(heroes);
+	});
+};
+
+const post = (req, res) => {
+	const hero = new Hero(req.body);
+	hero.save();
+	res.status(201).json(hero);
+};
+
+module.exports = { get, post };
