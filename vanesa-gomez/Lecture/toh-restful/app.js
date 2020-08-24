4fa8b415e754122511a9591d68a8d595ada245e6
Vanesa Gómez <v.cucchiararo@gmail.com>
2020-08-24 20:43:52 +0200

    create userroute...

--- vanesa-gomez/Lecture/toh-restful/app.js
+++ vanesa-gomez/Lecture/toh-restful/app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 const Hero = require('./src/models/heroModel');
+const User = require('./src/models/userModel');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -16,8 +17,10 @@ app.get('/', (req, res) => {
 	res.send('My server works...');
 });
 
-const heroRouter = require('./src/routes/heroRouter');
+const heroRouter = require('./src/routes/heroRouter')(Hero);
+const userRouter = require('./src/routes/userRouter')(User);
 
-app.use('/heroes', heroRouter(Hero));
+app.use('/heroes', heroRouter);
+app.use('/users', userRouter);
 
 app.listen(port, () => debug(`Running on port ${port}`));
--- vanesa-gomez/Lecture/toh-restful/src/routes/heroRouter.js
+++ vanesa-gomez/Lecture/toh-restful/src/routes/heroRouter.js
@@ -1,36 +1,37 @@
 const express = require('express');
 
+const heroesRouteController = require('../controllers/heroesRouteController');
+const heroRouteController = require('../controllers/heroRouteController');
+
 const heroRouter = express.Router();
 
 function routes(Hero) {
 	heroRouter
 		.route('/')
-		.post((req, res) => {
-			const hero = new Hero(req.body);
-			hero.save();
-			res.status(201).json(hero);
-		})
-		.get((req, res) => {
-			const query = {};
-			if (req.query.id) {
-				query.id = req.query.id;
-			}
-			Hero.find(query, (error, heroes) => {
-				if (error) {
-					return res.send(error);
-				}
-				res.json(heroes); // mismo nombre que el exports de heroesJson
-			});
-		});
+		.post(heroesRouteController.post)
+		.get(heroesRouteController.get);
 
-	heroRouter.route('/:heroId').get((req, res) => {
+	heroRouter.use('/:heroId', (req, res, next) => {
 		Hero.findById(req.params.heroId, (error, hero) => {
 			if (error) {
-				return res.send(error);
+				res.send(error);
+			} // Si viene _id
+			if (hero) {
+				req.hero = hero;
+				next();
+			} else {
+				res.sendStatus(404);
 			}
-			res.json(hero);
 		});
 	});
+	heroRouter
+		.route('/:heroId')
+		.get(heroRouteController.get)
+		.put(heroRouteController.put)
+		.patch(heroRouteController.patch)
+		.delete(heroRouteController.deleter);
+
 	return heroRouter;
 }
+
 module.exports = routes;
