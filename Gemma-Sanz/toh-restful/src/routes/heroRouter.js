62629e9d108c4e915e5607a803f03c5032059e8e
gemmas95 <gemma1030@gmail.com>
2020-08-24 18:29:32 +0200

    heroes and put, get, post, delete methods done

---
+++ Gemma-Sanz/toh-restful/src/routes/heroRouter.js
@@ -0,0 +1,111 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-param-reassign */
+
+const express = require('express');
+
+const heroRouter = express.Router();
+
+function routes(Hero) {
+	heroRouter
+		.route('/')
+		.post((req, res) => {
+			const hero = new Hero(req.body);
+			hero.save();
+			res.status(201).json(hero);
+		})
+		.get((req, res) => {
+			const query = {};
+			if (req.query.id) {
+				query.id = req.query.id;
+			}
+			Hero.find(query, (error, heroes) => {
+				if (error) {
+					res.send(error);
+				}
+				res.json(heroes);
+			});
+		});
+	// esto es un all, antes de hacer nada hacemos esto
+	heroRouter.use('/:heroId', (req, res, next) => {
+		Hero.findById(req.params.heroId, (error, hero) => {
+			if (error) {
+				res.send(error);
+			}
+			if (hero) {
+				req.hero = hero;
+				next();
+			} else {
+				res.sendStatus(404);
+			}
+		});
+	});
+
+	heroRouter
+		.route('/:heroId')
+		// la ruta será esta, que el nombre tiene que ser igual al de req.params.____
+		.put((req, res) => {
+			// Necesitamos:
+			// Obtener el héroe
+			const { hero } = req;
+			if (hero) {
+				// Si el heroe existe modificamos la propiedad nombre con el que nos viene por el body
+				// eslint-disable-next-line no-param-reassign
+				hero.name = req.body.name;
+				hero.save((err) => {
+					if (err) {
+						res.send(err);
+					}
+					res.json(hero);
+				});
+			}
+			// Obtener los nuevos valores
+			// Guardar los valores actualizados
+
+			// req trae el hero, hay una propiedad .hero
+		})
+		// el put actualiza un elemento ya existente en la db, el patch hace lo mismo pero creando uno nuevo, borra uno y crea otro, el _id canviaria
+		.patch((req, res) => {
+			const { hero } = req;
+			if (hero) {
+				Object.entries(req.body).forEach((item) => {
+					const key = item[0];
+					const value = item[1];
+					// eslint-disable-next-line no-param-reassign
+					hero[key] = value;
+				});
+				hero.save((err) => {
+					if (err) {
+						res.send(err);
+					}
+					res.json(hero);
+				});
+			}
+		})
+		// si viene _id lo elimino del objeto
+		/* Buscamos el objeto
+			si viene el _id, lo elimino del objeto
+			por cada propiedad del body, actualizo el modelo
+
+			guardo el modelo
+
+			respondo con un json */
+		.delete((req, res) => {
+			const { hero } = req;
+
+			if (hero) {
+				hero.remove((err) => {
+					if (err) {
+						res.send(err);
+					}
+					res.sendStatus(204);
+				});
+			}
+		})
+		.get((req, res) => {
+			return res.json(req.hero);
+		});
+
+	return heroRouter;
+}
+
+module.exports = routes;
