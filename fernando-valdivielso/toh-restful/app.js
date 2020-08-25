537598cbfca7fa042646c44d7cf2bb64fbf93cea
Fernando <fvaldivielso@outlook.com>
2020-08-25 12:17:21 +0200

    typescript

--- fernando-valdivielso/toh-restful/app.js
+++ fernando-valdivielso/toh-restful/app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 const Hero = require('./src/models/heroModel');
+// const Company = require('./src/models/companyModel')
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
