328b43378e33f365c04260cdc71dfbb18032dd83
gemmas95 <gemma1030@gmail.com>
2020-08-25 17:21:23 +0200

    added instambuuuuuuuul

--- Gemma-Sanz/toh-restful/src/controllers/heroesRouteController.js
+++ Gemma-Sanz/toh-restful/src/controllers/heroesRouteController.js
@@ -2,6 +2,10 @@
 function heroesController(Hero) {
 	function post(req, res) {
 		const hero = new Hero(req.body);
+		if (!req.body.name) {
+			res.status(400);
+			res.send(`Name is required!`);
+		}
 		hero.save();
 		res.status(201);
 		res.json(hero);
--- Gemma-Sanz/toh-restful/src/test/heroesRouteController.Spec.js
+++ Gemma-Sanz/toh-restful/src/test/heroesRouteController.Spec.js
@@ -18,14 +18,32 @@ describe('Hero Controller', () => {
 			const req = {
 				body: {
 					// Aquí sólo nos importa que req.body nos traiga el body, es lo único required!! Lo podemos comprovar en el postman
-					name: 'Bombasto'
 				}
 			};
 			const res = {
 				// Si hacemos un return de res.json, sólo nos interesa json y también el status porque forma parte de la respuesta
 				json: sinon.spy(),
-				status: sinon.spy()
+				status: sinon.spy(),
+				send: sinon.spy()
 			};
+			// Tenemos que ver de dónde viene el méodo, dónde lo invocan, aqu es a heroRouter
+			const controller = heroesController(Hero);
+			controller.post(req, res);
+			// Cuando res.status ha sido invocado con 201 tiene que ser true
+			res.status
+				.calledWith(400)
+				.should.equal(
+					true,
+					`Bad status, name is not required in this scenario.`
+				);
+			res.send
+				.calledWith(`Name is required!`)
+				.should.equal(true, `Message is not correct`);
+			// 2o argumento del marched le transmite al desarrollador un mensaje cuando falla el test
+
+			// para comprovar que el test falla podemos quitar el código de dentro del body:{}, t calledWith(400) en lugar de 201
+			// Hay un error de programación, y es que si el calledWith(201) se deja así pero quitamos el body y lo dejamos:{} pasaria igual, canviamos el código, lo adaptamos con un if(!req.body.name)
+			// En el unitario espiamos los argumentos, que haya sido llamado con este argumento
 		});
 	});
 });
