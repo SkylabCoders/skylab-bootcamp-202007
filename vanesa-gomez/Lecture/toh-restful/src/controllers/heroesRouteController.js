f8aa198ee0f6fe761f0dea24b51e5bbff95392ee
Vanesa Gómez <v.cucchiararo@gmail.com>
2020-08-25 22:35:28 +0200

    test mocha

--- vanesa-gomez/Lecture/toh-restful/src/controllers/heroesRouteController.js
+++ vanesa-gomez/Lecture/toh-restful/src/controllers/heroesRouteController.js
@@ -1,21 +1,27 @@
-const Hero = require('../models/heroModel');
-
-const post = (req, res) => {
-	const hero = new Hero(req.body);
-	hero.save();
-	res.status(201).json(hero);
-};
-const get = (req, res) => {
-	const query = {};
-	if (req.query.id) {
-		query.id = req.query.id;
-	}
-	Hero.find(query, (error, heroes) => {
-		if (error) {
-			res.send(error);
+function heroesController(Hero) {
+	function post(req, res) {
+		const hero = new Hero(req.body);
+		if (!req.body.name) {
+			res.status(400);
+			res.send('Name is required!');
 		}
-		res.json(heroes); // mismo nombre que el exports de heroesJson
-	});
-};
+		hero.save();
+		res.status(201);
+		return res.json(hero);
+	}
+	function get(req, res) {
+		const query = {}; // cuando no viene 1 query, cuando viene una query si coincide o no, y si el json viene
+		if (req.query.id) {
+			query.id = req.query.id;
+		}
+		Hero.find(query, (error, heroes) => {
+			if (error) {
+				res.send(error);
+			}
+			res.json(heroes); // mismo nombre que el exports de heroesJson
+		});
+	}
+	return { post, get };
+}
 
-module.exports = { post, get };
+module.exports = heroesController;
