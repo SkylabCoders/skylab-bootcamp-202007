255fd4630775314371c9568dd20eb070de7dd799
Gerard Ramon <gerardramonp@gmail.com>
2020-08-27 09:26:18 +0200

    finish hero tests

--- gerard-ramon/Diary/RestfulAPI/TOH-Restful/src/controllers/heroRouteController.js
+++ gerard-ramon/Diary/RestfulAPI/TOH-Restful/src/controllers/heroRouteController.js
@@ -9,44 +9,54 @@ const get = (req, res) => {
 
 const put = (req, res) => {
 	const { hero } = req;
-	hero.name = req.body.name;
-	hero.save((error) => {
-		if (error) {
-			res.send(error);
-		}
-		res.json(hero);
-	});
+	if (hero && req.body.name) {
+		hero.name = req.body.name;
+		hero.save((error) => {
+			if (error) {
+				res.send(error);
+			}
+			res.json(hero);
+		});
+	}
 };
 
 const patch = (req, res) => {
 	const { hero } = req;
 
-	if (req.body._id) {
-		delete req.body._id;
-	}
-
-	Object.entries(req.body).forEach((item) => {
-		const key = item[0];
-		const value = item[1];
-		hero[key] = value;
-	});
-	hero.save((error) => {
-		if (error) {
-			res.send(error);
+	if (hero && req.body) {
+		if (req.body._id) {
+			delete req.body._id;
 		}
-		res.json(hero);
-	});
+
+		Object.entries(req.body).forEach((item) => {
+			const key = item[0];
+			const value = item[1];
+			hero[key] = value;
+		});
+		hero.save((error) => {
+			if (error) {
+				res.send(error);
+			}
+			res.json(hero);
+		});
+	} else {
+		res.send('Request must include a hero and a body');
+	}
 };
 
 const deleter = (req, res) => {
 	const { hero } = req;
 
-	hero.remove((error) => {
-		if (error) {
-			res.send(error);
-		}
-		res.sendStatus(204);
-	});
+	if (hero) {
+		hero.remove((error) => {
+			if (error) {
+				res.send(error);
+			}
+			res.sendStatus(204);
+		});
+	} else {
+		res.send('Request must include a hero');
+	}
 };
 
 module.exports = { get, put, patch, deleter };
--- gerard-ramon/Diary/RestfulAPI/TOH-Restful/src/test/heroRouteController.spec.js
+++ gerard-ramon/Diary/RestfulAPI/TOH-Restful/src/test/heroRouteController.spec.js
@@ -4,6 +4,10 @@ const { expect } = require('chai');
 const heroController = require('../controllers/heroRouteController');
 
 describe('Hero Controller', () => {
+	afterEach(() => {
+		sinon.restore();
+	});
+
 	it('[GET] Should call json whith a req.hero ', () => {
 		let req = {
 			hero: 'bombasto'
@@ -37,5 +41,159 @@ describe('Hero Controller', () => {
 
 		expect(jsonSpy.called).to.be.false;
 		expect(sendSpy.calledWith('Bad parameters')).to.be.true;
+	});
+
+	it('[PUT] Should call hero.save with a req.hero & a body.name', () => {
+		let req = {
+			hero: {
+				save: () => {},
+				name: null
+			},
+			body: {
+				name: 'Santi'
+			}
+		};
+		const saveSpy = sinon.spy(req.hero, 'save');
+
+		let res = {};
+
+		heroController.put(req, res);
+
+		expect(saveSpy.called).to.be.true;
+	});
+
+	it('[PUT] Should NOT call hero.save without a body name', () => {
+		let req = {
+			hero: {
+				save: () => {},
+				name: null
+			},
+			body: {
+				name: null
+			}
+		};
+		const saveSpy = sinon.spy(req.hero, 'save');
+
+		let res = {};
+
+		heroController.put(req, res);
+
+		expect(saveSpy.called).to.be.false;
+	});
+
+	it('[PUT] Should NOT hero.save without a req.body.name', () => {
+		let req = {
+			hero: {
+				save: () => {},
+				name: null
+			},
+			body: {
+				name: null
+			}
+		};
+
+		let res = {
+			send: () => {}
+		};
+
+		let sendSpy = sinon.spy(res, 'send');
+
+		heroController.put(req, res);
+
+		expect(sendSpy.called).to.be.false;
+	});
+
+	it('[PATCH] Should call hero.save & _id', () => {
+		let req = {
+			hero: {
+				save: () => {}
+			},
+			body: {
+				_id: 13,
+				name: null
+			}
+		};
+
+		let res = {
+			send: () => {}
+		};
+
+		const saveSpy = sinon.spy(req.hero, 'save');
+
+		heroController.patch(req, res);
+
+		expect(saveSpy.called).to.be.true;
+	});
+
+	it('[PATCH] Should call hero.save & no _id', () => {
+		let req = {
+			hero: {
+				save: () => {}
+			},
+			body: {
+				_id: null,
+				name: null
+			}
+		};
+
+		let res = {
+			send: () => {}
+		};
+
+		const saveSpy = sinon.spy(req.hero, 'save');
+
+		heroController.patch(req, res);
+
+		expect(saveSpy.called).to.be.true;
+	});
+
+	it('[PATCH] Should NOT call hero.save with no req.body in request', () => {
+		let req = {
+			hero: {
+				save: () => {}
+			},
+			body: null
+		};
+
+		let res = {
+			send: () => {}
+		};
+
+		const sendSpy = sinon.spy(res, 'send');
+
+		heroController.patch(req, res);
+
+		expect(sendSpy.calledWith('Request must include a hero and a body')).to.be
+			.true;
+	});
+
+	it('[DELETE] Should call hero.remove', () => {
+		let req = {
+			hero: {
+				remove: () => {}
+			}
+		};
+
+		let res = {};
+
+		const removeSpy = sinon.spy(req.hero, 'remove');
+
+		heroController.deleter(req, res);
+
+		expect(removeSpy.called).to.be.true;
+	});
+
+	it('[DELETE] Should call res.send with no hero in req', () => {
+		let req = {};
+
+		let res = {
+			send: () => {}
+		};
+
+		const sendSpy = sinon.spy(res, 'send');
+
+		heroController.deleter(req, res);
+
+		expect(sendSpy.calledWith('Request must include a hero')).to.be.true;
 	});
 });
