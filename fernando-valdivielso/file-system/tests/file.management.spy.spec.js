dce2c785ba13adc7596fb3411e7b2f8d0d1ebdd7
Fernando <fvaldivielso@outlook.com>
2020-08-27 15:15:11 +0200

    testing.... no me entero

--- fernando-valdivielso/file-system/tests/file.management.spy.spec.js
+++ fernando-valdivielso/file-system/tests/file.management.spy.spec.js
@@ -1,11 +1,39 @@
-const { expect } = require('chai')
+const { expect } = require('chai');
+const sinon = require('sinon');
+const fs = require('fs');
+const proxyquire = require('proxyquire');
+const fileManagement = require('../file.management');
 
-describe('File Management', () => {
-    it('should creat a new file', () => {
-        expect(true).to.be.true;
+
+describe('File Management SPY', () => {
+    afterEach(() => {
+        sinon.restore();    // restaura todo el sandbox, donde se ejecutan los tests
+    })
+    it.skip('should create a new file', () => {
+        // configuracion del escenario
+        const writeSpy = sinon.spy(fs, 'writeFileSync');
+        const fileManagement = proxyquire('../file.management', { fs });
+
+
+        // invocar las funciones
+        fileManagement.createFile('test.txt');
+
+        // afirmaciones / asserts
+        expect(writeSpy.called).to.be.true;
+        expect(writeSpy.calledWith('./data/test.txt')).to.be.true;
     });
 
-    it('should not create a file when there is no filename', () => {
-        expect(true).to.be.true;
+    it.skip('should not create a file when there is no filename', () => {
+        const writeSpy = sinon.spy(fs, 'writeFileSync');
+        const fileManagement = proxyquire('../file.management', { fs });
+
+        try {
+            fileManagement.createFile();
+
+        } catch (error) {
+            expect(writeSpy.notcalled).to.be.true;
+        }
+
+
     });
 });
---
+++ fernando-valdivielso/file-system/tests/file.management.stub.spec.js
@@ -0,0 +1,67 @@
+const sinon = require('sinon');
+const proxyquire = require('proxyquire');
+const fs = require('fs');
+const { expect } = require('chai');
+
+describe('File Mangament STUB', () => {
+    afterEach(() => {
+        sinon.restore();
+    });
+
+    it('should create a file', () => {
+
+        // set up
+        const writeStub = sinon.stub(fs, 'writeFileSync');
+        writeStub.throws(new Error('filename is required!'))     // simulamos este comportamiento -> obligamos a lanzar el error
+        const fileManagement = proxyquire('../file.management', { fs });
+
+        // invoke
+        // fileManagement.createFile('test.txt');
+
+        // asserts
+        // expect(writeStub.callCount).to.equal(1);
+        // expect(fileManagement.createFile('test.txt')).to.throw();
+        expect(() => fileManagement.createFile()).to.throw();        // cazamos el error?
+
+    });
+
+    it('should create a file named test1.txt when text.txt already exists', () => {
+        const writeStub = sinon.stub(fs, 'writeFileSync');
+        const fileManagement = proxyquire('../file.management', { fs });
+
+        writeStub.withArgs('./data/test.txt').throws(new Error());
+
+
+        fileManagement.createFileSafe('test.txt')
+
+        expect(writeStub.calledWith('./data/test1.txt')).to.be.true;
+    });
+
+    it('should getAllfiles and return a list of files', () => {
+        const readdirStub = sinon.stub(fs, 'readdir');
+        const fileManagement = proxyquire('../file.management', { fs });
+
+        fileManagement.getAllFiles((error, data) => {
+            expect(data).to.equal([test.txt])
+        });
+
+    });
+
+    it('should return a list of files from getAllFilesPromise', () => {
+        const readdirStub = sinon.stub(fs, 'readdir');
+
+        const util = {
+            promisify: sinon.stub().returns(readdirStub)
+        }
+
+        const fileManagement = proxyquire('../file.management', { fs, util });
+        readdirStub.resolves(['test.txt']);
+
+        fileManagement
+            .getAllFilesPromise()
+            .then((files) => expect(files).to.eql(['test.txt']));
+
+    });
+
+
+});
