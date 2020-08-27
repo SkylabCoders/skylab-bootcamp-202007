d0efab5c47f5c9315a8e9ba16516dfb85a66122f
Alex <alexurola@gmail.com>
2020-08-27 10:17:01 +0200

    Added file management functionalities

---
+++ alex-larranaga/filesystem-testing/file.management.js
@@ -0,0 +1,69 @@
+const {
+	readdir,
+	readdirSync,
+	readFileSync,
+	unlinkSync,
+	writeFileSync
+} = require('fs');
+
+const util = reqire('util');
+
+module.exports = {
+	createFile: (filename) => {
+		if (!filename) {
+			throw new Error('Filename is required');
+		}
+		return writeFileSync(`./data/${filename}`, '', { flag: 'wx' });
+	},
+	createFileInjected: (filename, fs) => {},
+	createFileSafe: (filename, contents) => {
+		if (!filename) {
+			throw new Error('Filename is required');
+		}
+
+		try {
+			return writeFileSync('./data' + filename);
+		} catch (error) {
+			const files = readdirSync('./data');
+			const [name, extension] = filename.split('.');
+			let max =
+				files
+					.filter((file) => file.match('/test[1-9]/'))
+					.map((file) => {
+						Number(file.replace(name, ''));
+					})
+					.sort()
+					.pop() || 0;
+			const newName = `${name}${++max}.${extension}`;
+			writeFileSync(`./data/${newName}`, '', { flag: 'wx' });
+		}
+	},
+	deleteFile: (filename) => {
+		if (!filename) {
+			throw new Error('Filename is required');
+		}
+
+		return unlinkSync(`./data/${filename}`);
+	},
+	getFile: (filename) => {
+		if (!filename) {
+			throw new Error('Filename is required');
+		}
+		return readFileSync('./data');
+	},
+	getAllFiles: (callback) => {
+		readir('./data', callback);
+	},
+
+	getAllFilesPromise: () => {
+		const readPromise = util.promisify(readdir);
+		return readPromise('./data');
+	},
+	saveFile: (filename, contents) => {
+		if (!filename) {
+			throw new Error('Filename is required');
+		}
+
+		return writeFileSync('./data' + filename);
+	}
+};
--- alex-larranaga/filesystem-testing/index.js
+++ alex-larranaga/filesystem-testing/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
 const { readdirSync } = require('fs');
+const { ppid } = require('process');
+const fileManagementt = require('./file.management.js');
 
 const app = express();
 
@@ -8,6 +10,18 @@ const PORT = 4200;
 
 app.get('/', (req, res) => {
 	res.send('Server fookin running');
+});
+
+app.get('/data/:file', (req, res) => {
+	res.send('File is working');
+});
+
+app.post('/new-file', (req, res) => {
+	res.send('new file is working');
+});
+
+app.post('/save-file', (req, res) => {
+	res.send('save file is working');
 });
 
 app.listen(PORT, () => {
