2969e5a1c0eabd2a3973c4850517c8b6ca384464
Anna Codina <anna.co91@gmail.com>
2020-08-27 16:43:55 +0200

    new test with fake

--- anna-codina/test-mocha/tests/file.management.fake.spec.js
+++ anna-codina/test-mocha/tests/file.management.fake.spec.js
@@ -20,7 +20,7 @@ describe('File Management Fake', () => {
 		expect(writeFake.calledOnce).to.be.true;
 		expect(writeFake.calledWith(`./data/${fileName}`, '')).to.be.true;
 	});
-	it('throw an esception when a file already exists', () => {
+	it('should throw an esception when a file already exists', () => {
 		// los fakes no podemos redefinir (es inmutable) asi que cuando los creamos tenemos que
 		// definir el comportamiento a la vez. Pero OJO no podemos hacer .fake().comportamiento()
 		// no concatenamos con la funcion sino con la PROPIEDAD fake
@@ -32,5 +32,14 @@ describe('File Management Fake', () => {
 		expect(() => {
 			fileManagement.createFile(fileName);
 		}).to.throw();
+	});
+	it('async with yeld: should getAllfils and return a list of files', () => {
+		const readDirFake = sinon.fake.yields(null, [fileName]);
+		sinon.replace(fs, 'readdir', readDirFake);
+		const fileManagement = proxyquire('../file.management', { fs });
+
+		fileManagement.getAllFiles((error, data) => {
+			expect(data).to.eql([fileName]);
+		});
 	});
 });
