ab7b11fd437d74b9d3f500fb99df521f5c71966a
Marti Olasz <martiolasz@gmail.com>
2020-08-28 09:56:37 +0200

    change sendstatus to status

--- marti-olasz/Node/TohApi/src/controllers/hero/heroCreate.js
+++ marti-olasz/Node/TohApi/src/controllers/hero/heroCreate.js
@@ -3,11 +3,11 @@ function createHero(req, res, Hero) {
 		const hero = new Hero(req.body);
 		if (hero.id && hero.name) {
 			hero.save(() => {
-				res.sendStatus(200);
+				res.status(200);
 				res.json(hero);
 			});
-		} else res.sendStatus(404);
-	} else res.sendStatus(404);
+		} else res.status(404);
+	} else res.status(404);
 }
 
 module.exports = createHero;
