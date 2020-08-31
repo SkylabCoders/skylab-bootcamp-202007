551830400e8b72b86356cbf128dd12d733446c4b
fcesc-code <bpfran@gmail.com>
2020-08-31 09:16:19 +0200

    update config files

---
+++ fcesc/tender/database/DB_PARAMETERS.js
@@ -0,0 +1,9 @@
+const DB_PARAMETERS = {
+  projectsPerPage: 5,
+  budgetsPerPage: 5,
+  positionsPerPage: 25,
+  showClosedProjects: false,
+  showClosedBudgets: true
+}
+
+module.exports = DB_PARAMETERS;
