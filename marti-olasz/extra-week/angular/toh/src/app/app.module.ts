56b5779635797a4c5c6c3eabf9b02a034946d4fd
Marti Olasz <martiolasz@gmail.com>
2020-09-23 11:17:06 +0200

    Angular project created

---
+++ marti-olasz/extra-week/angular/toh/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
